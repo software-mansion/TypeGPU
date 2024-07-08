eaf5fca276a931f53d6a4bfe33b9afe98498451a
Konrad Reczko <konradreczko@Konrads-MacBook-Pro.local>
2024-07-08 15:13:10 +0200

    added simple tests for macros

---
+++ packages/wigsill/tests/macro.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest';
+import { repeat, wgsl } from 'wigsill';
+
+describe('repeat', () => {
+  it('repeats a string', () => {
+    const result = repeat('a', 3);
+    expect(result).toEqual(wgsl`${['a', 'a', 'a']}`);
+  });
+
+  it('repeats a string with a function', () => {
+    const result = repeat((idx) => `a${idx}`, 3);
+    expect(result).toEqual(wgsl`${['a0', 'a1', 'a2']}`);
+  });
+});
