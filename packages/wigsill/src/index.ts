2d966534788f0cda3f3e84c4a4b82661f5c6966e
Iwo Plaza <iwoplaza@gmail.com>
2024-07-08 12:11:47 +0200

    Added `typed-binary` as a dependency of wigsill-examples.

--- packages/wigsill/src/index.ts
+++ packages/wigsill/src/index.ts
@@ -1,6 +1,7 @@
 export * from './errors';
 export * from './types';
 export { default as wgsl } from './wgsl';
+export { AsCallable, ICallable } from './callable';
 export { MemoryArena, makeArena } from './memoryArena';
 export { default as WGSLRuntime } from './wgslRuntime';
 export { default as ProgramBuilder, type Program } from './programBuilder';
