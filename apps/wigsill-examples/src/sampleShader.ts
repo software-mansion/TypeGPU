3d42cbdc174517de1f8456187b0b49c7d3921d2e
Konrad Reczko <konradreczko@Konrads-MacBook-Pro.local>
2024-07-08 14:22:56 +0200

    cleanup

--- apps/wigsill-examples/src/sampleShader.ts
+++ apps/wigsill-examples/src/sampleShader.ts
@@ -1,7 +1,8 @@
-import { wgsl } from 'wigsill';
+import { wgsl, repeat } from 'wigsill';
 
 export const sampleShader = wgsl`
 fn add(a: f32, b: f32) -> f32 {
+  ${repeat('a + b', 5)}
   return a + ${wgsl.constant('123 + 5')};
 }
 `;
