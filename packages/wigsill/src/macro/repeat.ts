bb55010e5b9785caba9102f468e1bbbbe52f0b53
reczkok <66403540+reczkok@users.noreply.github.com>
2024-07-09 21:37:24 +0200

    Swap function parameters for better readability.

--- packages/wigsill/src/macro/repeat.ts
+++ packages/wigsill/src/macro/repeat.ts
@@ -2,8 +2,8 @@ import { WGSLCode, code } from '../wgslCode';
 import { WGSLSegment } from './../types';
 
 export function repeat(
-  snippet: string | WGSLSegment | ((idx: number) => string | WGSLSegment),
   count: number,
+  snippet: string | WGSLSegment | ((idx: number) => string | WGSLSegment),
 ): WGSLCode {
   if (typeof snippet === 'function') {
     return code`${Array.from({ length: count }, (_, idx) => snippet(idx))}`;
