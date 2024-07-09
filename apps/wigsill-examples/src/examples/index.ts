e768e32f480f24bd26722fe34d134f348feccecd
mhawryluk <marcinhawrylukxx@gmail.com>
2024-07-09 12:01:03 +0200

    Add camera thresholding example

--- apps/wigsill-examples/src/examples/index.ts
+++ apps/wigsill-examples/src/examples/index.ts
@@ -1,8 +1,13 @@
+import { CameraThresholdingExample } from './CameraThresholdingExample';
 import { GradientTilesExample } from './GradientTilesExample';
 
 export const examples = {
   'gradient-tiles': {
     label: 'Gradient tiles',
     component: GradientTilesExample,
+  },
+  'camera-thresholding': {
+    label: 'Camera thresholding',
+    component: CameraThresholdingExample,
   },
 };
