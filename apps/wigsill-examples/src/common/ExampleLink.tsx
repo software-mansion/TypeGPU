295908d33febfea8eac4e4b2a308b8f96365dbca
Iwo Plaza <iwoplaza@gmail.com>
2024-07-09 11:05:34 +0200

    Examples can be made as React components, and have exposed parameters through dat.gui (#12)

---
+++ apps/wigsill-examples/src/common/ExampleLink.tsx
@@ -0,0 +1,39 @@
+import cs from 'classnames';
+import { MouseEvent } from 'react';
+import { RESET } from 'jotai/utils';
+import { useAtom } from 'jotai/react';
+
+import { currentExampleAtom } from '../router';
+import useEvent from './useEvent';
+
+export function ExampleLink(props: {
+  exampleKey: string | undefined;
+  children?: string;
+}) {
+  const { exampleKey, children } = props;
+
+  const [currentExample, setCurrentExample] = useAtom(currentExampleAtom);
+
+  const handleClick = useEvent((e: MouseEvent) => {
+    e.preventDefault();
+    setCurrentExample(exampleKey ?? RESET);
+  });
+
+  const active = currentExample === exampleKey;
+
+  return (
+    <a
+      // Even though we prevent the default behavior of this link
+      // it is good to have this set semantically.
+      href={`#example=${exampleKey}`}
+      onClick={handleClick}
+      className={cs(
+        'block px-2 py-1 border-l rounded text-left transition-all',
+        active
+          ? 'border-gray-500 text-black'
+          : 'border-transparent text-gray-600',
+      )}>
+      {children}
+    </a>
+  );
+}
---
+++ apps/wigsill-examples/src/examples/index.ts
@@ -0,0 +1,8 @@
+import { GradientTilesExample } from './GradientTilesExample';
+
+export const examples = {
+  'gradient-tiles': {
+    label: 'Gradient tiles',
+    component: GradientTilesExample,
+  },
+};
