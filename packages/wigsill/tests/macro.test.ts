cae7b5f842408eebcf1c9294337228296e1128ac
reczkok <66403540+reczkok@users.noreply.github.com>
2024-07-09 21:28:39 +0200

    Make the test description more precise
    Co-authored-by: Iwo Plaza <iwoplaza@gmail.com>

--- packages/wigsill/tests/macro.test.ts
+++ packages/wigsill/tests/macro.test.ts
@@ -7,7 +7,7 @@ describe('repeat', () => {
     expect(result).toEqual(wgsl`${['a', 'a', 'a']}`);
   });
 
-  it('repeats a string with a function', () => {
+  it('repeats a computed string', () => {
     const result = repeat((idx) => `a${idx}`, 3);
     expect(result).toEqual(wgsl`${['a0', 'a1', 'a2']}`);
   });
