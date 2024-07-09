b0e7d22424669d1f0a23487027bccf78e4e7853a
reczkok <66403540+reczkok@users.noreply.github.com>
2024-07-09 21:38:42 +0200

    Adjust the order of arguments in macro/repeat tests

--- packages/wigsill/tests/macro.test.ts
+++ packages/wigsill/tests/macro.test.ts
@@ -3,12 +3,12 @@ import { repeat, wgsl } from 'wigsill';
 
 describe('repeat', () => {
   it('repeats a string', () => {
-    const result = repeat('a', 3);
+    const result = repeat(3, 'a');
     expect(result).toEqual(wgsl`${['a', 'a', 'a']}`);
   });
 
   it('repeats a computed string', () => {
-    const result = repeat((idx) => `a${idx}`, 3);
+    const result = repeat(3, (idx) => `a${idx}`);
     expect(result).toEqual(wgsl`${['a0', 'a1', 'a2']}`);
   });
 });
