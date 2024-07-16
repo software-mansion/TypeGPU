c4cf7a42ade1ec9f029789a0304c36484f19a4c6
Iwo Plaza <iwoplaza@gmail.com>
2024-07-16 14:13:00 +0200

    More tests for wgsl.fn

--- packages/wigsill/tests/function.test.ts
+++ packages/wigsill/tests/function.test.ts
@@ -1,31 +1,103 @@
 import { parse } from '@wigsill/parser';
 import { describe, expect, it } from 'vitest';
-import { StrictNameRegistry, wgsl } from '../src';
+import { StrictNameRegistry, WGSLSegment, wgsl } from '../src';
 import { ResolutionCtxImpl } from '../src/programBuilder';
 
-describe('wgsl.fn', () => {
-  it('should represent an empty function', () => {
-    const ctx = new ResolutionCtxImpl({ names: new StrictNameRegistry() });
+function parseWGSL(segment: WGSLSegment) {
+  const ctx = new ResolutionCtxImpl({ names: new StrictNameRegistry() });
 
-    const empty = wgsl.fn()`() -> {
+  const resolved = ctx.resolve(segment);
+
+  const resolvedWithDependencies =
+    ctx.dependencies.map((d) => ctx.resolve(d)).join('\n') + '\n' + resolved;
+
+  return parse(resolvedWithDependencies);
+}
+
+describe('wgsl.fn', () => {
+  it('should inject function declaration of called function', () => {
+    const emptyFn = wgsl.fn()`() {
       // do nothing
     }`.alias('empty');
 
-    const shader = wgsl`
-    fn main() {
-      // ${empty}();
-    }`;
+    const actual = parseWGSL(wgsl`
+      fn main() {
+        ${emptyFn}();
+      }
+    `);
 
-    expect(parse(ctx.resolve(shader))).toEqual(
-      parse(`
-fn empty() {
-  // still does nothing
-}
+    const expected = parse(`
+      fn empty() {}
 
-fn main() {
-  // empty();
-}
-`),
-    );
+      fn main() {
+        empty();
+      }
+    `);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should inject function declaration only once', () => {
+    const emptyFn = wgsl.fn()`() {
+      // do nothing
+    }`.alias('empty');
+
+    const actual = parseWGSL(wgsl`
+      fn main() {
+        ${emptyFn}();
+        ${emptyFn}();
+      }
+    `);
+
+    const expected = parse(`
+      fn empty() {}
+
+      fn main() {
+        empty();
+        empty();
+      }
+    `);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should inject function declaration only once (calls are nested)', () => {
+    const emptyFn = wgsl.fn()`() {
+      // do nothing
+    }`.alias('empty');
+
+    const nestedAFn = wgsl.fn()`() {
+      ${emptyFn}();
+    }`.alias('nested_a');
+
+    const nestedBFn = wgsl.fn()`() {
+      ${emptyFn}();
+    }`.alias('nested_b');
+
+    const actual = parseWGSL(wgsl`
+      fn main() {
+        ${nestedAFn}();
+        ${nestedBFn}();
+      }
+    `);
+
+    const expected = parse(`
+      fn empty() {}
+
+      fn nested_a() {
+        empty();
+      }
+
+      fn nested_b() {
+        empty();
+      }
+
+      fn main() {
+        nested_a();
+        nested_b();
+      }
+    `);
+
+    expect(actual).toEqual(expected);
   });
 });
