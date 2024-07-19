ace79a541f21d1f5e50f90559cab3c45ef95738e
Iwo Plaza <iwoplaza@gmail.com>
2024-07-19 15:00:00 +0200

    Started to write momentum fluid sim

---
+++ apps/wigsill-examples/src/examples/momentum-fluid-simulation.ts
@@ -0,0 +1,18 @@
+/*
+{
+  "title": "Momentum Fluid Simulation"
+}
+*/
+
+// import { addElement } from '@wigsill/example-toolkit';
+// import { createRuntime, wgsl } from 'wigsill';
+
+// const canvas = await addElement('canvas', { width: 128, height: 128 });
+
+// const runtime = await createRuntime();
+
+// const renderPipeline = runtime.makeRenderPipeline({
+//   label: 'Render Pipeline',
+//   primitive: { topology: 'triangle-strip' },
+//   vertex: {},
+// });
