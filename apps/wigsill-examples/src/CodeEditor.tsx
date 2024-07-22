2d06352f098dfd12a21851cab11b3b47465bf8e6
Iwo Plaza <iwoplaza@gmail.com>
2024-07-22 16:17:50 +0200

    Copying over wigsill types using vite

--- apps/wigsill-examples/src/CodeEditor.tsx
+++ apps/wigsill-examples/src/CodeEditor.tsx
@@ -1,10 +1,10 @@
 import Editor, { type Monaco } from '@monaco-editor/react';
 import webgpuTypes from '@webgpu/types/dist/index.d.ts?raw';
 import typedBinary from 'typed-binary/dist/index.d.ts?raw';
+import wigsill from 'wigsill/dist/index.d.ts?raw';
 import useEvent from './common/useEvent';
 import { tsCompilerOptions } from './embeddedTypeScript';
 import toolkitTypes from './types/example-toolkit.d.ts?raw';
-import wigsill from './types/wigsill-types.d.ts?raw';
 
 function handleEditorWillMount(monaco: Monaco) {
   const tsDefaults = monaco?.languages.typescript.typescriptDefaults;
