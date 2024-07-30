553f354590defb68f636563c8530ec6e84aecd3e
reczkok <reczkok@gmail.com>
2024-07-30 14:51:43 +0200

    fixed textures and texture views

--- packages/wigsill/src/types.ts
+++ packages/wigsill/src/types.ts
@@ -1,4 +1,5 @@
 import type { ISchema } from 'typed-binary';
+import type { F32, I32, U32 } from './data';
 import type { WgslIdentifier } from './wgslIdentifier';
 
 export type Wgsl = string | number | WgslResolvable | symbol;
@@ -204,6 +205,7 @@ export interface WgslData<TInner> extends ISchema<TInner>, WgslResolvable {
 }
 
 export type AnyWgslData = WgslData<unknown>;
+export type AnyWgslPrimitive = U32 | I32 | F32;
 
 export interface WgslPointer<
   TScope extends 'function',
