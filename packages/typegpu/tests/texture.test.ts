cd33b21cb27e55e83471f7df29bcb6eb3a89cf8c
reczkok <reczkok@gmail.com>
2024-08-01 12:20:55 +0200

    testing tests and fixing tests

--- packages/typegpu/tests/texture.test.ts
+++ packages/typegpu/tests/texture.test.ts
@@ -1,19 +1,27 @@
-// import { wgsl } from 'typegpu';
+import { wgsl } from 'typegpu';
+import { u32 } from 'typegpu/data';
 import { describe, expect, it } from 'vitest';
 
-// describe('texture', () => {
-//   it('creates a texture with no usage', () => {
-//     const texture = wgsl.texture({
-//       size: [1, 1],
-//       format: 'rgba8unorm',
-//     });
-//     // should be null
-//     expect(texture.$asTypedTexture()).toBe(null);
-//   });
-// });
+global.GPUTextureUsage = {
+  COPY_SRC: 0x01,
+  COPY_DST: 0x02,
+  TEXTURE_BINDING: 0x04,
+  STORAGE_BINDING: 0x08,
+  RENDER_ATTACHMENT: 0x10,
+};
 
-describe('temp', () => {
-  it('temp', () => {
-    expect(1).toBe(1);
+describe('texture', () => {
+  it('creates a texture with no usage', () => {
+    const texture = wgsl.texture({
+      size: [1, 1],
+      format: 'rgba8unorm',
+    });
+    // should be null
+    expect(
+      texture.asSampled({
+        dataType: u32,
+        type: 'texture_2d',
+      }),
+    ).toBeNull();
   });
 });
