8dff16810885ddf7608472041a3362319b0c29f0
reczkok <reczkok@gmail.com>
2024-08-05 13:06:15 +0200

    we were already supporting textures and buffers so just added global_variable and some tests

--- packages/typegpu-wgsl-parser/tests/variableDecl.spec.ts
+++ packages/typegpu-wgsl-parser/tests/variableDecl.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import type { VariableDecl } from '../src/grammar';
+import type { GlobalVariableDecl, VariableDecl } from '../src/grammar';
 import { parse } from '../src/index';
 
 describe('variable_decl', () => {
@@ -35,93 +35,168 @@ describe('variable_decl', () => {
   });
 });
 
-// describe('global_variable_decl', () => {
-//   it('parses a buffer', () => {
-//     expect(parse('@group(0) @binding(0) var<storage, read> buf: u32;')).toEqual(
-//       {
-//         type: 'global_variable_decl',
-//         attributes: [
-//           {
-//             type: 'attribute',
-//             ident: 'group',
-//             args: [],
-//           },
-//           {
-//             type: 'attribute',
-//             ident: 'binding',
-//             args: [],
-//           },
-//         ],
-//         variable_decl: {
-//           type: 'variable_decl',
-//           ident: 'buf',
-//           typespec: {
-//             type: 'template_elaborated_ident',
-//             ident: 'u32',
-//             template_list: null,
-//           },
-//           expr: null,
-//           template_list: [
-//             {
-//               type: 'template_elaborated_ident',
-//               ident: 'storage',
-//               template_list: null,
-//             },
-//             {
-//               type: 'template_elaborated_ident',
-//               ident: 'read',
-//               template_list: null,
-//             },
-//           ],
-//         },
-//       } satisfies GlobalVariableDecl,
-//     );
-//   });
+describe('global_variable_decl', () => {
+  it('parses a buffer', () => {
+    expect(parse('@group(0) @binding(0) var<storage, read> buf: u32;')).toEqual(
+      {
+        declarations: [
+          {
+            type: 'global_variable_decl',
+            attributes: [
+              {
+                type: 'attribute',
+                ident: 'group',
+                args: [
+                  {
+                    type: 'int_literal',
+                    value: '0',
+                  },
+                ],
+              },
+              {
+                type: 'attribute',
+                ident: 'binding',
+                args: [
+                  {
+                    type: 'int_literal',
+                    value: '0',
+                  },
+                ],
+              },
+            ],
+            variable_decl: {
+              type: 'variable_decl',
+              ident: 'buf',
+              typespec: {
+                type: 'template_elaborated_ident',
+                ident: 'u32',
+                template_list: null,
+              },
+              expr: null,
+              template_list: [
+                {
+                  type: 'template_elaborated_ident',
+                  ident: 'storage',
+                  template_list: null,
+                },
+                {
+                  type: 'template_elaborated_ident',
+                  ident: 'read',
+                  template_list: null,
+                },
+              ],
+            },
+          } satisfies GlobalVariableDecl,
+        ],
+        type: 'translation_unit',
+      },
+    );
+  });
 
-// it('parses a texture', () => {
-//   expect(parse('@group(0) @binding(0) var tex: texture_2d<f32>;')).toEqual({
-//     type: 'variable_decl',
-//     ident: 'tex',
-//     typespec: {
-//       type: 'template_elaborated_ident',
-//       ident: 'texture_2d',
-//       template_list: [
-//         {
-//           type: 'template_elaborated_ident',
-//           ident: 'f32',
-//           template_list: null,
-//         },
-//       ],
-//     },
-//     expr: null,
-//     template_list: null,
-//   } satisfies VariableDecl);
+  it('parses a texture', () => {
+    expect(parse('@group(0) @binding(0) var tex: texture_2d<f32>;')).toEqual({
+      declarations: [
+        {
+          type: 'global_variable_decl',
+          attributes: [
+            {
+              type: 'attribute',
+              ident: 'group',
+              args: [
+                {
+                  type: 'int_literal',
+                  value: '0',
+                },
+              ],
+            },
+            {
+              type: 'attribute',
+              ident: 'binding',
+              args: [
+                {
+                  type: 'int_literal',
+                  value: '0',
+                },
+              ],
+            },
+          ],
+          variable_decl: {
+            type: 'variable_decl',
+            ident: 'tex',
+            typespec: {
+              type: 'template_elaborated_ident',
+              ident: 'texture_2d',
+              template_list: [
+                {
+                  type: 'template_elaborated_ident',
+                  ident: 'f32',
+                  template_list: null,
+                },
+              ],
+            },
+            expr: null,
+            template_list: null,
+          },
+        } satisfies GlobalVariableDecl,
+      ],
+      type: 'translation_unit',
+    });
 
-//   expect(
-//     parse(
-//       '@group(0) @binding(0) var tex: texture_storage_2d<rgba8unorm, write>;',
-//     ),
-//   ).toEqual({
-//     type: 'variable_decl',
-//     ident: 'tex',
-//     typespec: {
-//       type: 'template_elaborated_ident',
-//       ident: 'texture_storage_2d',
-//       template_list: [
-//         {
-//           type: 'template_elaborated_ident',
-//           ident: 'rgba8unorm',
-//           template_list: null,
-//         },
-//         {
-//           type: 'template_elaborated_ident',
-//           ident: 'write',
-//           template_list: null,
-//         },
-//       ],
-//     },
-//     expr: null,
-//     template_list: null,
-//   } satisfies VariableDecl);
-// });
-// });
+    expect(
+      parse(
+        '@group(0) @binding(0) var tex: texture_storage_2d<rgba8unorm, write>;',
+      ),
+    ).toEqual({
+      declarations: [
+        {
+          type: 'global_variable_decl',
+          attributes: [
+            {
+              type: 'attribute',
+              ident: 'group',
+              args: [
+                {
+                  type: 'int_literal',
+                  value: '0',
+                },
+              ],
+            },
+            {
+              type: 'attribute',
+              ident: 'binding',
+              args: [
+                {
+                  type: 'int_literal',
+                  value: '0',
+                },
+              ],
+            },
+          ],
+          variable_decl: {
+            type: 'variable_decl',
+            ident: 'tex',
+            typespec: {
+              type: 'template_elaborated_ident',
+              ident: 'texture_storage_2d',
+              template_list: [
+                {
+                  type: 'template_elaborated_ident',
+                  ident: 'rgba8unorm',
+                  template_list: null,
+                },
+                {
+                  type: 'template_elaborated_ident',
+                  ident: 'write',
+                  template_list: null,
+                },
+              ],
+            },
+            expr: null,
+            template_list: null,
+          },
+        } satisfies GlobalVariableDecl,
+      ],
+      type: 'translation_unit',
+    });
+  });
+});
