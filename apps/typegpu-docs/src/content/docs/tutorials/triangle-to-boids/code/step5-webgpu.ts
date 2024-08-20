cee485b59bd5b98a64b17bfd921bc0d62687cd5b
reczkok <reczkok@gmail.com>
2024-08-20 12:28:59 +0200

    new folder structure and TryInPlaygroundButton

---
+++ apps/typegpu-docs/src/content/docs/tutorials/triangle-to-boids/code/step5-webgpu.ts
@@ -0,0 +1,234 @@
+import { addButton, addElement, onFrame } from '@typegpu/example-toolkit';
+
+const adapter = await navigator.gpu?.requestAdapter();
+const device = await adapter?.requestDevice();
+
+if (!device) {
+  throw new Error('Failed to acquire a device');
+}
+
+const canvas = await addElement('canvas', { aspectRatio: 1 });
+const context = canvas.getContext('webgpu') as GPUCanvasContext;
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+context.configure({
+  device,
+  format: presentationFormat,
+  alphaMode: 'premultiplied',
+});
+
+addButton('Randomize', randomizeTriangles);
+
+const triangleSize = 0.2;
+const triangleVertexData = new Float32Array([
+  0.0,
+  triangleSize,
+  -triangleSize,
+  -triangleSize,
+  triangleSize,
+  -triangleSize,
+]);
+
+const triangleAmount = 10;
+const trianglePos = device.createBuffer({
+  size: triangleAmount * 3 * 4,
+  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+});
+
+const triangleVertexBuffer = device.createBuffer({
+  size: triangleVertexData.byteLength,
+  usage: GPUBufferUsage.VERTEX,
+  mappedAtCreation: true,
+});
+new Float32Array(triangleVertexBuffer.getMappedRange()).set(triangleVertexData);
+triangleVertexBuffer.unmap();
+
+function randomizeTriangles() {
+  if (!device) {
+    return;
+  }
+  const data = new Float32Array(triangleAmount * 3);
+  for (let i = 0; i < triangleAmount; i++) {
+    data[i * 3] = Math.random() * 2 - 1;
+    data[i * 3 + 1] = Math.random() * 2 - 1;
+    data[i * 3 + 2] = Math.random() * Math.PI * 2;
+  }
+  device.queue.writeBuffer(trianglePos, 0, data);
+}
+
+const wgslCode = `
+  fn rotate(v: vec2f, angle: f32) -> vec2f {
+    let pos = vec2(
+      (v.x * cos(angle)) - (v.y * sin(angle)),
+      (v.x * sin(angle)) + (v.y * cos(angle))
+    );
+    return pos;
+  };
+
+  struct TriangleData {
+    x : f32,
+    y : f32,
+    rotation : f32,
+  };
+
+  struct VertexOutput {
+    @builtin(position) position : vec4f,
+    @location(1) fragUV : vec2f,
+  };
+
+  @binding(0) @group(0) var<storage, read> trianglePos : array<TriangleData>;
+
+  @vertex
+  fn mainVert(@builtin(instance_index) ii: u32 ,@location(0) v: vec2f) -> VertexOutput {
+    let instanceInfo = trianglePos[ii];
+
+    let rotated = rotate(v, instanceInfo.rotation);
+    let offset = vec2(instanceInfo.x, instanceInfo.y);
+
+    let pos = vec4(rotated + offset, 0.0, 1.0);
+    let fragUV = (rotated + vec2f(${triangleSize}, ${triangleSize})) / vec2f(${triangleSize} * 2.0);
+    return VertexOutput(pos, fragUV);
+  }
+
+  @fragment
+  fn mainFrag(@location(1) fragUV : vec2f) -> @location(0) vec4f {
+    let color1 = vec3(196.0 / 255.0, 100.0 / 255.0, 255.0 / 255.0);
+    let color2 = vec3(29.0 / 255.0, 114.0 / 255.0, 240.0 / 255.0);
+
+    let dist = length(fragUV - vec2(0.5, 0.5));
+
+    let color = mix(color1, color2, dist);
+
+    return vec4(color, 1.0);
+  }
+`;
+
+const wgslCodeCompute = `
+  struct TriangleData {
+    x : f32,
+    y : f32,
+    rotation : f32,
+  };
+
+  @binding(0) @group(0) var<storage, read_write> trianglePos : array<TriangleData>;
+
+  @compute @workgroup_size(1)
+  fn mainCompute(@builtin(global_invocation_id) gid: vec3u) {
+    let index = gid.x;
+    var instanceInfo = trianglePos[index];
+    let triangleSize = ${triangleSize};
+
+    if (instanceInfo.x > 1.0 + triangleSize) {
+      instanceInfo.x = -1.0 - triangleSize;
+    }
+    if (instanceInfo.y > 1.0 + triangleSize) {
+      instanceInfo.y = -1.0 - triangleSize;
+    }
+
+    instanceInfo.rotation += 0.01;
+    instanceInfo.x += 0.01;
+    instanceInfo.y += 0.01;
+
+    trianglePos[index] = instanceInfo;
+  }
+`;
+
+const module = device.createShaderModule({
+  code: wgslCode,
+});
+const moduleCompute = device.createShaderModule({
+  code: wgslCodeCompute,
+});
+
+const pipeline = device.createRenderPipeline({
+  layout: 'auto',
+  vertex: {
+    module: module,
+    buffers: [
+      {
+        arrayStride: 2 * 4,
+        attributes: [
+          {
+            shaderLocation: 0,
+            offset: 0,
+            format: 'float32x2',
+          },
+        ],
+      },
+    ],
+  },
+  fragment: {
+    module: module,
+    targets: [
+      {
+        format: presentationFormat,
+      },
+    ],
+  },
+  primitive: {
+    topology: 'triangle-list',
+  },
+});
+
+const computePipeline = device.createComputePipeline({
+  layout: 'auto',
+  compute: {
+    module: moduleCompute,
+  },
+});
+
+const bindGroup = device.createBindGroup({
+  layout: pipeline.getBindGroupLayout(0),
+  entries: [
+    {
+      binding: 0,
+      resource: {
+        buffer: trianglePos,
+      },
+    },
+  ],
+});
+
+const bindGroupCompute = device.createBindGroup({
+  layout: computePipeline.getBindGroupLayout(0),
+  entries: [
+    {
+      binding: 0,
+      resource: {
+        buffer: trianglePos,
+      },
+    },
+  ],
+});
+
+randomizeTriangles();
+onFrame(() => {
+  const commandEncoder = device.createCommandEncoder();
+  const textureView = context.getCurrentTexture().createView();
+
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    colorAttachments: [
+      {
+        view: textureView,
+        clearValue: [0, 0, 0, 0],
+        loadOp: 'clear',
+        storeOp: 'store',
+      },
+    ],
+  };
+
+  const computePass = commandEncoder.beginComputePass();
+  computePass.setPipeline(computePipeline);
+  computePass.setBindGroup(0, bindGroupCompute);
+  computePass.dispatchWorkgroups(triangleAmount);
+  computePass.end();
+
+  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+  passEncoder.setPipeline(pipeline);
+  passEncoder.setVertexBuffer(0, triangleVertexBuffer);
+  passEncoder.setBindGroup(0, bindGroup);
+  passEncoder.draw(3, triangleAmount);
+  passEncoder.end();
+
+  device.queue.submit([commandEncoder.finish()]);
+});
---
+++ apps/typegpu-docs/src/content/docs/tutorials/triangle-to-boids/code/step9-typegpu.ts
@@ -0,0 +1,332 @@
+import {
+  addButton,
+  addElement,
+  addParameter,
+  onFrame,
+} from '@typegpu/example-toolkit';
+import { type WgslBufferUsage, builtin, createRuntime, wgsl } from 'typegpu';
+import { arrayOf, struct, vec2f, f32 } from 'typegpu/data';
+
+const runtime = await createRuntime();
+const device = runtime.device;
+
+const canvas = await addElement('canvas', { aspectRatio: 1 });
+const context = canvas.getContext('webgpu') as GPUCanvasContext;
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+context.configure({
+  device,
+  format: presentationFormat,
+  alphaMode: 'premultiplied',
+});
+
+addButton('Randomize', randomizeTriangles);
+
+const parametesBuffer = wgsl
+  .buffer(
+    struct({
+      separationDistance: f32,
+      separationStrength: f32,
+      alignmentDistance: f32,
+      alignmentStrength: f32,
+      cohesionDistance: f32,
+      cohesionStrength: f32,
+    }),
+    {
+      separationDistance: 0.03,
+      separationStrength: 0.001,
+      alignmentDistance: 0.3,
+      alignmentStrength: 0.01,
+      cohesionDistance: 0.3,
+      cohesionStrength: 0.001,
+    },
+  )
+  .$allowReadonly();
+
+const triangleSize = 0.04;
+const triangleVertex = wgsl
+  .buffer(arrayOf(vec2f, 3), [
+    [0.0, triangleSize],
+    [-triangleSize / 2, -triangleSize / 2],
+    [triangleSize / 2, -triangleSize / 2],
+  ])
+  .$allowVertex('vertex');
+
+const triangleAmount = 500;
+const trianglePosData = arrayOf(
+  struct({
+    position: vec2f,
+    velocity: vec2f,
+  }),
+  triangleAmount,
+);
+type TrianglePosData = typeof trianglePosData;
+
+const trianglePosBuffers = Array.from({ length: 2 }, () => {
+  return wgsl.buffer(trianglePosData).$allowUniform().$allowMutable();
+});
+
+const pairs = [
+  [trianglePosBuffers[0].asUniform(), trianglePosBuffers[1].asMutable()],
+  [trianglePosBuffers[1].asUniform(), trianglePosBuffers[0].asMutable()],
+];
+
+const readSlot = wgsl.slot<WgslBufferUsage<TrianglePosData, 'uniform'>>();
+const writeSlot = wgsl.slot<WgslBufferUsage<TrianglePosData, 'mutable'>>();
+
+function randomizeTriangles() {
+  const positions = [];
+  for (let i = 0; i < triangleAmount; i++) {
+    const position = [Math.random() * 2 - 1, Math.random() * 2 - 1] as [
+      number,
+      number,
+    ];
+    const velocity = [
+      Math.random() * 0.1 - 0.05,
+      Math.random() * 0.1 - 0.05,
+    ] as [number, number];
+    positions.push({ position, velocity });
+  }
+  runtime.writeBuffer(trianglePosBuffers[0], positions);
+  runtime.writeBuffer(trianglePosBuffers[1], positions);
+}
+
+const rotate = wgsl.fn`(v: vec2f, angle: f32) -> vec2f {
+  let pos = vec2(
+    (v.x * cos(angle)) - (v.y * sin(angle)),
+    (v.x * sin(angle)) + (v.y * cos(angle))
+  );
+  return pos;
+}`;
+
+const getRotationFromVelocity = wgsl.fn`(velocity: vec2f) -> f32 {
+  return -atan2(velocity.x, velocity.y);
+}`;
+
+const renderPipelines = [0, 1].map((idx) =>
+  runtime.makeRenderPipeline({
+    vertex: {
+      code: wgsl`
+      let instanceInfo = ${pairs[idx][0]}[${builtin.instanceIndex}];
+      let rotated = ${rotate}(
+        ${triangleVertex.asVertex()},
+        ${getRotationFromVelocity}(instanceInfo.velocity),
+      );
+
+      let offset = instanceInfo.position;
+
+      let pos = vec4f(rotated + offset, 0.0, 1.0);
+      let fragUV = (rotated + vec2f(${triangleSize}, ${triangleSize})) / vec2f(${triangleSize} * 2.0);
+    `,
+      output: {
+        [builtin.position]: 'pos',
+        fragUV: vec2f,
+      },
+    },
+    fragment: {
+      code: wgsl`
+      let color1 = vec3(196.0 / 255.0, 100.0 / 255.0, 255.0 / 255.0);
+      let color2 = vec3(29.0 / 255.0, 114.0 / 255.0, 240.0 / 255.0);
+
+      let dist = length(fragUV - vec2(0.5, 0.5));
+
+      let color = mix(color1, color2, dist);
+
+      return vec4(color, 1.0);
+    `,
+      target: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  }),
+);
+
+const computePipelines = [0, 1].map((idx) =>
+  runtime.makeComputePipeline({
+    code: wgsl`
+    let index = ${builtin.globalInvocationId}.x;
+    var instanceInfo = ${readSlot}[index];
+    let params = ${parametesBuffer.asReadonly()};
+
+    var separation = vec2(0.0, 0.0);
+    var alignment = vec2(0.0, 0.0);
+    var alignmentCount = 0u;
+    var cohesion = vec2(0.0, 0.0);
+    var cohesionCount = 0u;
+    for (var i = 0u; i < ${triangleAmount}; i = i + 1) {
+      if (i == index) {
+        continue;
+      }
+      var other = ${readSlot}[i];
+      var dist = distance(instanceInfo.position, other.position);
+      if (dist < params.separationDistance) {
+        separation += instanceInfo.position - other.position;
+      }
+      if (dist < params.alignmentDistance) {
+        alignment += other.velocity;
+        alignmentCount++;
+      }
+      if (dist < params.cohesionDistance) {
+        cohesion += other.position;
+        cohesionCount++;
+      }
+    };
+
+    if (alignmentCount > 0u) {
+      alignment = alignment / f32(alignmentCount);
+    }
+
+    if (cohesionCount > 0u) {
+      cohesion = (cohesion / f32(cohesionCount)) - instanceInfo.position;
+    }
+
+    instanceInfo.velocity += (separation * params.separationStrength) + (alignment * params.alignmentStrength) + (cohesion * params.cohesionStrength);
+    instanceInfo.velocity = normalize(instanceInfo.velocity) * clamp(length(instanceInfo.velocity), 0.0, 0.01);
+
+    let triangleSize = ${triangleSize};
+    if (instanceInfo.position[0] > 1.0 + triangleSize) {
+      instanceInfo.position[0] = -1.0 - triangleSize;
+    }
+    if (instanceInfo.position[1] > 1.0 + triangleSize) {
+      instanceInfo.position[1] = -1.0 - triangleSize;
+    }
+    if (instanceInfo.position[0] < -1.0 - triangleSize) {
+      instanceInfo.position[0] = 1.0 + triangleSize;
+    }
+    if (instanceInfo.position[1] < -1.0 - triangleSize) {
+      instanceInfo.position[1] = 1.0 + triangleSize;
+    }
+
+    instanceInfo.position += instanceInfo.velocity;
+
+    ${writeSlot}[index] = instanceInfo;
+  `
+      .with(readSlot, pairs[idx][0])
+      .with(writeSlot, pairs[idx][1]),
+  }),
+);
+
+randomizeTriangles();
+let even = false;
+onFrame(() => {
+  even = !even;
+  computePipelines[even ? 0 : 1].execute({
+    workgroups: [triangleAmount],
+  });
+  renderPipelines[even ? 1 : 0].execute({
+    colorAttachments: [
+      {
+        view: context.getCurrentTexture().createView(),
+        clearValue: [0, 0, 0, 0],
+        loadOp: 'clear',
+        storeOp: 'store',
+      },
+    ],
+    vertexCount: 3,
+    instanceCount: triangleAmount,
+  });
+
+  runtime.flush();
+});
+
+const options = {
+  separationDistance: 0.05,
+  separationStrength: 0.001,
+  alignmentDistance: 0.3,
+  alignmentStrength: 0.01,
+  cohesionDistance: 0.3,
+  cohesionStrength: 0.001,
+};
+
+function applyOptions() {
+  runtime.writeBuffer(parametesBuffer, options);
+}
+
+addParameter(
+  'Separation Dist',
+  {
+    initial: options.separationDistance,
+    min: 0.0,
+    max: 0.5,
+    step: 0.001,
+  },
+  (v) => {
+    options.separationDistance = v;
+    applyOptions();
+  },
+);
+
+addParameter(
+  'Separation Str',
+  {
+    initial: options.separationStrength,
+    min: 0.0,
+    max: 0.1,
+    step: 0.001,
+  },
+  (v) => {
+    options.separationStrength = v;
+    applyOptions();
+  },
+);
+
+addParameter(
+  'Align Dist',
+  {
+    initial: options.alignmentDistance,
+    min: 0.0,
+    max: 0.5,
+    step: 0.001,
+  },
+  (v) => {
+    options.alignmentDistance = v;
+    applyOptions();
+  },
+);
+
+addParameter(
+  'Align Str',
+  {
+    initial: options.alignmentStrength,
+    min: 0.0,
+    max: 0.1,
+    step: 0.001,
+  },
+  (v) => {
+    options.alignmentStrength = v;
+    applyOptions();
+  },
+);
+
+addParameter(
+  'Cohesion Dist',
+  {
+    initial: options.cohesionDistance,
+    min: 0.0,
+    max: 0.5,
+    step: 0.001,
+  },
+  (v) => {
+    options.cohesionDistance = v;
+    applyOptions();
+  },
+);
+
+addParameter(
+  'Cohesion Str',
+  {
+    initial: options.cohesionStrength,
+    min: 0.0,
+    max: 0.1,
+    step: 0.001,
+  },
+  (v) => {
+    options.cohesionStrength = v;
+    applyOptions();
+  },
+);
