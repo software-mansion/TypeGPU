425afd364a067af7fd642b0298162426a9c66929
Iwo Plaza <iwoplaza@gmail.com>
2024-08-21 16:32:19 +0200

    Tweaked the landing page to fit new requirements. (#188)

--- apps/typegpu-docs/tailwind.config.mjs
+++ apps/typegpu-docs/tailwind.config.mjs
@@ -9,6 +9,11 @@ export default {
         sans: ['Aeonik', ...defaultTheme.fontFamily.sans],
       },
       colors: {
+        tameplum: {
+          50: '#EFEFF9',
+          600: '#757387',
+          800: '#515061',
+        },
         grayscale: {
           100: '#000000',
           80: '#333333',
@@ -17,7 +22,7 @@ export default {
           0: '#FFF',
         },
         special: {
-          stroke: '#00000029',
+          stroke: '#2a0a6629',
         },
         gradient: {
           purple: '#C464FF',
