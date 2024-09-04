ee3d3defba2177ed2552bfcaebd607b7dd327460
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2024-09-04 09:58:24 +0200

    Commit review suggestions
    Co-authored-by: reczkok <66403540+reczkok@users.noreply.github.com>

--- packages/typegpu/tests/std/fract.test.ts
+++ packages/typegpu/tests/std/fract.test.ts
@@ -3,8 +3,8 @@ import { std } from '../../src/std';
 
 describe('fract', () => {
   it('computes fractional part of a number', () => {
-    expect(std.fract(2)).toEqual(0);
+    expect(std.fract(2)).toBeCloseTo(0);
     expect(std.fract(2.2)).toBeCloseTo(0.2);
-    expect(std.fract(-0.5)).toEqual(0.5);
+    expect(std.fract(-0.5)).toBeCloseTo(0.5);
   });
 });
