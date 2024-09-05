913f7f3ea0d621e0b2c6dc1009f28d83fa1a1be2
reczkok <66403540+reczkok@users.noreply.github.com>
2024-09-05 12:40:17 +0200

    Add a way to destroy Tgpu buffers (#263)
    * Initial implementation
    
    * Add info about buffer status
    
    * Updated error msg as suggested
    
    Co-authored-by: Iwo Plaza <iwoplaza@gmail.com>

--- packages/typegpu/src/wgslBuffer.ts
+++ packages/typegpu/src/wgslBuffer.ts
@@ -48,8 +48,10 @@ export interface TgpuBuffer<TData extends AnyTgpuData>
   $allowVertex(stepMode: 'vertex' | 'instance'): this & AllowVertex;
   $addFlags(flags: GPUBufferUsageFlags): this;
   $device(device: GPUDevice): this & Unmanaged;
+  destroy(): void;
 
   _usages: AllowedUsages<TData>;
+  readonly destroyed: boolean;
   readonly label: string | undefined;
 }
 
@@ -79,6 +81,7 @@ class TgpuBufferImpl<TData extends AnyTgpuData> implements TgpuBuffer<TData> {
     GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC;
   private _device: GPUDevice | null = null;
   private _buffer: GPUBuffer | null = null;
+  private _destroyed = false;
 
   _usages: AllowedUsages<TData> = {
     uniform: null,
@@ -117,6 +120,9 @@ class TgpuBufferImpl<TData extends AnyTgpuData> implements TgpuBuffer<TData> {
         'To use this property, make the buffer unmanaged by passing a GPUDevice to $device',
       );
     }
+    if (this._destroyed) {
+      throw new Error('This buffer has been destroyed');
+    }
     if (!this._buffer) {
       this._buffer = this._device.createBuffer({
         size: this.dataType.size,
@@ -135,10 +141,14 @@ class TgpuBufferImpl<TData extends AnyTgpuData> implements TgpuBuffer<TData> {
   get device() {
     if (!this._device) {
       throw new Error(
-        'This buffer is managed by TypeGPU and cannot be used directly',
+        'This buffer has not been assigned a device. Use .$device(device) to assign a device',
       );
     }
     return this._device;
+  }
+
+  get destroyed() {
+    return this._destroyed;
   }
 
   $name(label: string) {
@@ -214,6 +224,14 @@ class TgpuBufferImpl<TData extends AnyTgpuData> implements TgpuBuffer<TData> {
   $device(device: GPUDevice) {
     this._device = device;
     return this;
+  }
+
+  destroy() {
+    if (this._destroyed) {
+      return;
+    }
+    this._destroyed = true;
+    this._buffer?.destroy();
   }
 
   toString(): string {
