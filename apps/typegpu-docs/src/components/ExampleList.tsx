b21a868e298701094d1b2af1369d8003dd3f33ce
mhawryluk <marcinhawrylukxx@gmail.com>
2024-09-13 11:37:32 +0200

    Remove playground from examples page

--- apps/typegpu-docs/src/components/ExampleList.tsx
+++ apps/typegpu-docs/src/components/ExampleList.tsx
@@ -1,7 +1,4 @@
-import {
-  PLAYGROUND_KEY,
-  examplesByCategory,
-} from '../utils/examples/exampleContent';
+import { examplesByCategory } from '../utils/examples/exampleContent';
 import { exampleCategories } from '../utils/examples/types';
 import { ExampleLink } from './ExampleLink';
 
@@ -9,10 +6,10 @@ function ExampleList() {
   return (
     <>
       <nav className="flex flex-col flex-1 gap-7 py-4 overflow-y-auto min-w-64">
-        <ExampleLink key={PLAYGROUND_KEY} exampleKey={PLAYGROUND_KEY}>
+        {/* <ExampleLink key={PLAYGROUND_KEY} exampleKey={PLAYGROUND_KEY}>
           Playground
         </ExampleLink>
-        <hr />
+        <hr /> */}
         {exampleCategories.map((category) =>
           (examplesByCategory[category.key] ?? []).map((example) => (
             <ExampleLink key={example.key} exampleKey={example.key}>
