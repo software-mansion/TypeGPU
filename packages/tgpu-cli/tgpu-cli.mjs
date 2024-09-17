57bd5bfc3ab8762c055bdd0ace17c69823f6811d
reczkok <66403540+reczkok@users.noreply.github.com>
2024-09-17 13:50:13 +0200

    POC of a companion CLI for TypeGPU (#336)
    Co-authored-by: Iwo Plaza <iwoplaza@gmail.com>

---
+++ packages/tgpu-cli/tgpu-cli.mjs
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+import { readFileSync } from 'node:fs';
+import { exit } from 'node:process';
+import arg from 'arg';
+import color from './colors.mjs';
+import placeholderGenerate from './gen.mjs';
+
+const args = arg({
+  '--version': Boolean,
+  '--help': Boolean,
+  '--input': String,
+  '--output': String,
+
+  '-v': '--version',
+  '-h': '--help',
+  '-i': '--input',
+  '-o': '--output',
+});
+
+const COMMANDS = {
+  gen: {
+    execute: async () => {
+      const input = args['--input'];
+      const output = args['--output'];
+
+      if (!input || !output) {
+        console.error(
+          `${color.Red}Error: Missing required arguments: ${color.Yellow}--input${color.Reset} and ${color.Yellow}--output${color.Reset}`,
+        );
+        exit(1);
+      }
+
+      try {
+        await placeholderGenerate(input, output);
+      } catch (error) {
+        console.error(`${color.Red}Error: ${error.message}${color.Reset}`);
+        exit(1);
+      }
+    },
+  },
+};
+
+function printHelp() {
+  console.log(`${color.Cyan}\
+----------------------------
+
+  TypeGPU CLI
+
+----------------------------
+${color.Reset}
+
+${color.Bold}Commands:${color.Reset}
+  tgpu-cli gen   Generate a ts file from a wgsl file.
+`);
+}
+
+function printVersion() {
+  try {
+    const packageJson = JSON.parse(
+      readFileSync(new URL('./package.json', import.meta.url)),
+    );
+    console.log(
+      `${color.Green}TypeGPU CLI version ${packageJson.version}${color.Reset}`,
+    );
+  } catch (error) {
+    console.error(
+      `${color.Red}Error reading version: ${error.message}${color.Reset}`,
+    );
+    exit(1);
+  }
+}
+
+if (args['--help']) {
+  printHelp();
+  exit(0);
+}
+
+if (args['--version']) {
+  printVersion();
+  exit(0);
+}
+
+/** @type {keyof typeof COMMANDS} */
+const command = args._[0]; // first positional argument
+if (!command) {
+  printHelp();
+  exit(1);
+}
+
+if (!(command in COMMANDS)) {
+  console.error(`\nUnknown command: ${color.Yellow}${command}${color.Reset}\n`);
+  exit(1);
+}
+
+await COMMANDS[command].execute();
