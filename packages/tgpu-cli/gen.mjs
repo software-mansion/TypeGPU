f6a6559e15569feb5d9958944bc0bd46deef8809
mhawryluk <marcinhawrylukxx@gmail.com>
2024-09-23 13:29:31 +0200

    Add array support

--- packages/tgpu-cli/gen.mjs
+++ packages/tgpu-cli/gen.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 
 import fs from 'node:fs/promises';
-import { StructInfo, TemplateInfo, WgslReflect } from 'wgsl_reflect';
+import { ArrayInfo, StructInfo, TemplateInfo, WgslReflect } from 'wgsl_reflect';
 
 const cwd = new URL(`file:${process.cwd()}/`);
+
+const LENGTH_VAR = 'arrayLength';
 
 /**
  * @param { string } input
@@ -12,6 +14,7 @@ const cwd = new URL(`file:${process.cwd()}/`);
 async function generate(input, output) {
   const inputPath = new URL(input, cwd);
   const outputPath = new URL(output, cwd);
+  const isTs = output.endsWith('.ts');
 
   const inputContents = await fs.readFile(inputPath, 'utf8');
   const reflect = new WgslReflect(inputContents);
@@ -20,7 +23,7 @@ async function generate(input, output) {
 
 import * as d from 'typegpu/data';
 
-${generateStructs(reflect.structs)}
+${generateStructs(reflect.structs, isTs)}
 ${generateAliases(reflect.aliases)}
 `;
 
@@ -29,45 +32,75 @@ ${generateAliases(reflect.aliases)}
 
 /**
  * @param { import('wgsl_reflect').StructInfo[] } structs
+ * @param { boolean } isTs
  */
-const generateStructs = (structs) => `/* structs */
-${structs
-  .map(
-    (struct) => `const ${struct.name} = d.struct({
-  ${struct.members.map((member) => generateStructMember(member)).join('\n  ')}
-});`,
-  )
-  .join('\n\n')}  
+function generateStructs(structs, isTs) {
+  return `/* structs */
+${structs.map((struct) => generateStruct(struct, isTs)).join('\n\n')}  
 `;
+}
+
+/**
+ * @param { import('wgsl_reflect').StructInfo } struct
+ * @param { boolean } isTs
+ */
+function generateStruct(struct, isTs) {
+  return `const ${struct.name} =${hasVarLengthMember(struct) ? ` (${LENGTH_VAR}${isTs ? ': number' : ''}) =>` : ''} d.struct({
+  ${struct.members.map((member) => generateStructMember(member)).join('\n  ')}
+});`;
+}
+
+/**
+ * @param { import('wgsl_reflect').StructInfo } struct
+ */
+function hasVarLengthMember(struct) {
+  const member = struct.members[struct.members.length - 1].type;
+  return (
+    (member instanceof StructInfo && hasVarLengthMember(member)) ||
+    (member instanceof ArrayInfo && member.size === 0)
+  );
+}
 
 /**
  * @param { import('wgsl_reflect').AliasInfo[] } aliases
  */
-const generateAliases = (aliases) => `/* aliases */
+function generateAliases(aliases) {
+  return `/* aliases */
 ${aliases
-  .map(
-    (alias) =>
-      `const ${alias.name} = ${alias.type instanceof StructInfo ? '' : 'd.'}${replaceWithAlias(alias.type)};`,
-  )
+  .map((alias) => `const ${alias.name} = ${generateType(alias.type)};`)
   .join('\n')}`;
+}
 
 /**
  * @param { import('wgsl_reflect').MemberInfo } member
  */
 function generateStructMember(member) {
-  if (member.type.size === 0) {
-    throw new Error(`Invalid data type with size 0: ${member.type.name}`);
+  return `${member.name}: ${generateType(member.type)},`;
+}
+
+/**
+ * @param { import('wgsl_reflect').TypeInfo } type
+ */
+function generateType(type) {
+  if (!(type instanceof ArrayInfo) && type.size === 0) {
+    throw new Error(`Invalid data type with size 0: ${type.name}`);
   }
 
-  let type = `${member.type instanceof StructInfo ? '' : 'd.'}${replaceWithAlias(member.type)}`;
-
-  for (const attribute of member.attributes ?? []) {
-    if (['align', 'size'].includes(attribute.name)) {
-      type = `d.${attribute.name}(${type}, ${attribute.value})`;
-    }
+  if (type instanceof ArrayInfo) {
+    return `d.arrayOf(${generateType(type.format)}, ${type.count > 0 ? type.count : LENGTH_VAR})`;
   }
 
-  return `${member.name}: ${type},`;
+  const tgpuType = `${type instanceof StructInfo ? '' : 'd.'}${replaceWithAlias(type)}`;
+
+  return (
+    type.attributes?.reduce(
+      (acc, attribute) =>
+        ['align', 'size'].includes(attribute.name)
+          ? `d.${attribute.name}(${acc}, ${attribute.value})`
+          : acc,
+      tgpuType,
+    ) ?? tgpuType
+  );
 }
 
 /**
