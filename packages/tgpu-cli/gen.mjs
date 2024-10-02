f9de5606d0f20d3707c69b15f3ae49655552f1bc
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2024-10-02 11:59:23 +0200

    BindGroupLayouts generator in tgpu-cli (#405)

--- packages/tgpu-cli/gen.mjs
+++ packages/tgpu-cli/gen.mjs
@@ -26,13 +26,17 @@ async function main(input, output) {
  */
 export function generate(wgsl, toTs = true) {
   const reflect = new WgslReflect(wgsl);
+
   return `/* generated via tgpu-cli by TypeGPU */
 
+import tgpu from 'typegpu';
 import * as d from 'typegpu/data';
 
 ${generateStructs(reflect.structs, toTs)}
 
 ${generateAliases(reflect.aliases)}
+
+${generateBindGroupLayouts(reflect.getBindGroups())}
 `.trim();
 }
 
@@ -52,7 +56,11 @@ ${structs.map((struct) => generateStruct(struct, toTs)).join('\n\n')}`
  * @param { boolean } toTs
  */
 function generateStruct(struct, toTs) {
-  return `const ${struct.name} =${hasVarLengthMember(struct) ? ` (${LENGTH_VAR}${toTs ? ': number' : ''}) =>` : ''} d.struct({
+  return `export const ${struct.name} = ${
+    hasVarLengthMember(struct)
+      ? `(${LENGTH_VAR}${toTs ? ': number' : ''}) => `
+      : ''
+  }d.struct({
   ${struct.members.map((member) => generateStructMember(member)).join('\n  ')}
 });`;
 }
@@ -72,7 +80,7 @@ function generateAliases(aliases) {
   return aliases.length > 0
     ? `/* aliases */
 ${aliases
-  .map((alias) => `const ${alias.name} = ${generateType(alias.type)};`)
+  .map((alias) => `export const ${alias.name} = ${generateType(alias.type)};`)
   .join('\n')}`
     : '';
 }
@@ -86,9 +94,10 @@ function generateStructMember(member) {
 
 /**
  * @param { import('wgsl_reflect').TypeInfo } type_
+ * @param { boolean } checkNonZeroLength
  */
-function generateType(type_) {
-  if (!(type_ instanceof ArrayInfo) && type_.size === 0) {
+function generateType(type_, checkNonZeroLength = true) {
+  if (checkNonZeroLength && type_.size === 0 && !type_.isArray) {
     throw new Error(`Invalid data type with size 0: ${type_.name}`);
   }
 
@@ -134,6 +143,160 @@ function replaceWithAlias(type) {
   return type instanceof TemplateInfo
     ? typeToAlias(type.name, type.format?.name ?? '')
     : type.name;
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo[][] } bindGroups
+ */
+function generateBindGroupLayouts(bindGroups) {
+  return bindGroups.length > 0
+    ? `/* bindGroupLayouts */
+${bindGroups
+  .flatMap(
+    (group, index) => `\
+export const layout${index} = tgpu.bindGroupLayout({
+  ${generateGroupLayout(group)}
+}).$forceIndex(${index});`,
+  )
+  .join('\n\n')}`
+    : '';
+}
+
+const RESOURCE_GENERATORS = [
+  generateUniformVariable,
+  generateStorageVariable,
+  generateTextureVariable,
+  generateSamplerVariable,
+  generateStorageTextureVariable,
+];
+
+const ACCESS_TYPES = {
+  read: 'readonly',
+  write: 'writeonly',
+  read_write: 'mutable',
+};
+
+const SAMPLE_TYPES = {
+  u32: 'uint',
+  i32: 'sint',
+  f32: 'float',
+};
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo[] } group
+ */
+function generateGroupLayout(group) {
+  return Array.from(group)
+    .map((variable, index) =>
+      variable
+        ? `${variable.name}: ${generateVariable(variable)},`
+        : `_${index}: null, // skipping binding ${index}`,
+    )
+    .join('\n  ');
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function generateVariable(variable) {
+  return RESOURCE_GENERATORS[variable.resourceType](variable);
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function generateUniformVariable(variable) {
+  return `{
+    uniform: ${generateType(variable.type)},
+  }`;
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function generateStorageVariable(variable) {
+  return `{
+    storage: ${generateType(variable.type, false)},${
+      variable.access ? `\n    access: '${ACCESS_TYPES[variable.access]}',` : ''
+    }
+  }`;
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function getViewDimension(variable) {
+  const type_ = variable.type.name;
+  const dimension = type_.includes('_3d')
+    ? '3d'
+    : type_.includes('_1d')
+      ? '1d'
+      : type_.includes('_cube')
+        ? 'cube'
+        : null;
+
+  return dimension
+    ? `${dimension}${type_.includes('_array') ? '-array' : ''}`
+    : null;
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function generateStorageTextureVariable(variable) {
+  const viewDimension = getViewDimension(variable);
+  const access =
+    variable.type instanceof TemplateInfo ? variable.type.access : null;
+
+  return `{
+    storageTexture: '${variable.format?.name}',${
+      access ? `\n    access: '${ACCESS_TYPES[access]}',` : ''
+    }${viewDimension ? `\n    viewDimension: '${viewDimension}',` : ''}
+  }`;
+}
+
+const SAMPLER_TYPES = {
+  sampler: 'filtering',
+  sampler_comparison: 'comparison',
+};
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function generateSamplerVariable(variable) {
+  return `{
+    sampler: '${SAMPLER_TYPES[variable.type.name]}',
+  }`;
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function generateTextureVariable(variable) {
+  const type_ = variable.type.name;
+
+  if (type_ === 'texture_external') {
+    return generateExternalTextureVariable(variable);
+  }
+
+  const format = variable.format?.name;
+  const viewDimension = getViewDimension(variable);
+  const multisampled = type_.includes('_multisampled');
+
+  return `{
+    texture: '${type_.includes('_depth') ? 'depth' : SAMPLE_TYPES[format]}',${
+      viewDimension ? `\n    viewDimension: '${viewDimension}',` : ''
+    }${multisampled ? '\n    multisampled: true,' : ''}
+  }`;
+}
+
+/**
+ * @param { import('wgsl_reflect').VariableInfo } variable
+ */
+function generateExternalTextureVariable(variable) {
+  return `{
+    externalTexture: {},
+  }`;
 }
 
 export default main;
