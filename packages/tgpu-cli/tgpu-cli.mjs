7e64838c41f3fff622dc60b132e6289257769f7d
mhawryluk <marcinhawrylukxx@gmail.com>
2024-10-04 14:20:07 +0200

    tab--

--- packages/tgpu-cli/tgpu-cli.mjs
+++ packages/tgpu-cli/tgpu-cli.mjs
@@ -96,12 +96,12 @@ ${color.Reset}
 
 ${color.Bold}Commands:${color.Reset}
   ${color.Cyan}tgpu-cli gen ${color.Reset} Generate a js/ts file from a wgsl file.
-    -c, --commonjs\t\t generate a CommonJS style file
-    -h, --help\t\t\t list all commands and their options
-    -i, --input\t\t\t a single input name or a glob pattern to generate js/ts from all matching files
-    -j, --js\t\t\t generate a JavaScript file, instead of TypeScript
-    -o, --output\t\t an output name for generated file; can be used only if a single file is being generated
-    -v, --version\t\t print tgpu-cli version
+    -c, --commonjs\t generate a CommonJS style file
+    -h, --help\t\t list all commands and their options
+    -i, --input\t\t a single input name or a glob pattern to generate js/ts from all matching files
+    -j, --js\t\t generate a JavaScript file, instead of TypeScript
+    -o, --output\t an output name for generated file; can be used only if a single file is being generated
+    -v, --version\t print tgpu-cli version
 `);
 }
 
