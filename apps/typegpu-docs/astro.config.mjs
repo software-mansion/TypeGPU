f440f563c75ad4da07c88ad8b502c9060a8d31b2
Iwo Plaza <iwoplaza@gmail.com>
2024-10-07 16:10:53 +0200

    Cleaning up the docs a bit (#420)

--- apps/typegpu-docs/astro.config.mjs
+++ apps/typegpu-docs/astro.config.mjs
@@ -6,6 +6,10 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import importRawRedirectPlugin from './vite-import-raw-redirect-plugin';
 
+const stripFalsy = (items) => items.filter((item) => !!item);
+
+const DEV = import.meta.env.DEV;
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.swmansion.com',
@@ -40,7 +44,7 @@ export default defineConfig({
       social: {
         github: 'https://github.com/software-mansion/TypeGPU',
       },
-      sidebar: [
+      sidebar: stripFalsy([
         {
           label: '⭐ Live Examples',
           link: 'examples',
@@ -50,11 +54,15 @@ export default defineConfig({
         },
         {
           label: 'Guides',
-          items: [
+          items: stripFalsy([
             // Each item here is one entry in the navigation menu.
             {
               label: 'Getting Started',
               slug: 'guides/getting-started',
+            },
+            DEV && {
+              label: 'Roots',
+              slug: 'guides/roots',
             },
             {
               label: 'Typed Buffers',
@@ -63,6 +71,10 @@ export default defineConfig({
             {
               label: 'Defining Data Types',
               slug: 'guides/defining-data-types',
+            },
+            DEV && {
+              label: 'Generating JS from WGSL',
+              slug: 'guides/generating-js-from-wgsl',
             },
             // {
             //   label: 'Basic Principles',
@@ -76,13 +88,23 @@ export default defineConfig({
             //   label: 'Parametrized Functions',
             //   slug: 'guides/parametrized-functions',
             // },
+          ]),
+        },
+        DEV && {
+          label: 'Tutorials',
+          items: [
+            {
+              label:
+                'From a Triangle to Simulating Boids: Step-by-step Tutorial',
+              slug: 'tutorials/triangle-to-boids',
+            },
           ],
         },
         {
           label: '🙋 FAQ',
           slug: 'faq',
         },
-      ],
+      ]),
     }),
     tailwind(),
     react(),
