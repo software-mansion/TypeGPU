a3e3a14d5e164216d84cc75312b489b135fe8491
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2024-10-10 16:04:05 +0200

    CommonJS support in tgpu-cli and better output path compiler (#424)

--- packages/tgpu-cli/gen.mjs
+++ packages/tgpu-cli/gen.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 
 import fs from 'node:fs/promises';
+import path from 'node:path';
+import { exit } from 'node:process';
 import { ArrayInfo, StructInfo, TemplateInfo, WgslReflect } from 'wgsl_reflect';
 
 const cwd = new URL(`file:${process.cwd()}/`);
@@ -8,57 +10,104 @@ const cwd = new URL(`file:${process.cwd()}/`);
 const LENGTH_VAR = 'arrayLength';
 
 /**
- * @param { string } input
- * @param { string } output
+ * @typedef {object} Options
+ * @prop {string} inputPath
+ * @prop {string} outputPath
+ * @prop {boolean} toTs
+ * @prop {'commonjs' | 'esmodule'} moduleSyntax
+ * @prop {'keep' | 'overwrite'} [existingFileStrategy]
+ * @prop {string[]} [exportsList]
  */
-async function main(input, output) {
-  const inputPath = new URL(input, cwd);
-  const outputPath = new URL(output, cwd);
-  const toTs = output.endsWith('.ts');
+
+/**
+ * @param {Options} options
+ */
+async function main(options) {
+  const inputPath = new URL(options.inputPath, cwd);
+  const outputPath = new URL(options.outputPath, cwd);
   const inputContents = await fs.readFile(inputPath, 'utf8');
 
-  await fs.writeFile(outputPath, generate(inputContents, toTs));
+  if (options.existingFileStrategy !== 'overwrite') {
+    const fileExists = await fs
+      .access(options.outputPath)
+      .then(() => true)
+      .catch(() => false);
+
+    if (fileExists) {
+      if (options.existingFileStrategy === undefined) {
+        console.error(
+          `Error: File ${options.outputPath} already exists. Use --overwrite option to replace existing files or --keep to skip them.`,
+        );
+
+        exit(1);
+      }
+
+      if (options.existingFileStrategy === 'keep') {
+        console.log(
+          `Skipping ${options.inputPath}, file ${options.outputPath} already exists.`,
+        );
+        return;
+      }
+    }
+  }
+
+  const generated = generate(inputContents, options);
+  await fs.mkdir(path.dirname(options.outputPath), { recursive: true });
+  await fs.writeFile(outputPath, generated);
 }
 
 /**
- * @param { string } wgsl
- * @param { boolean } toTs
+ * @param {string} wgsl
+ * @param {Options} options
  */
-export function generate(wgsl, toTs = true) {
+export function generate(
+  wgsl,
+  options = {
+    inputPath: '',
+    outputPath: '',
+    toTs: true,
+    moduleSyntax: 'esmodule',
+  },
+) {
   const reflect = new WgslReflect(wgsl);
 
   return `/* generated via tgpu-cli by TypeGPU */
 
+${
+  options.moduleSyntax === 'commonjs'
+    ? `\
+const tgpu = require('typegpu').default;
+const d = require('typegpu/data');`
+    : `\
 import tgpu from 'typegpu';
-import * as d from 'typegpu/data';
-
-${generateStructs(reflect.structs, toTs)}
-
-${generateAliases(reflect.aliases)}
-
-${generateBindGroupLayouts(reflect.getBindGroups())}
-`.trim();
+import * as d from 'typegpu/data';`
+}
+${generateStructs(reflect.structs, options)}
+${generateAliases(reflect.aliases, options)}
+${generateBindGroupLayouts(reflect.getBindGroups(), options)}
+${generateExports(options)}
+`;
 }
 
 /**
- * @param { import('wgsl_reflect').StructInfo[] } structs
- * @param { boolean } toTs
+ * @param {import('wgsl_reflect').StructInfo[]} structs
+ * @param {Options} options
  */
-function generateStructs(structs, toTs) {
+function generateStructs(structs, options) {
   return structs.length > 0
-    ? `/* structs */
-${structs.map((struct) => generateStruct(struct, toTs)).join('\n\n')}`
+    ? `\n/* structs */
+${structs.map((struct) => generateStruct(struct, options)).join('\n\n')}`
     : '';
 }
 
 /**
- * @param { import('wgsl_reflect').StructInfo } struct
- * @param { boolean } toTs
+ * @param {import('wgsl_reflect').StructInfo} struct
+ * @param {Options} options
  */
-function generateStruct(struct, toTs) {
-  return `export const ${struct.name} = ${
+function generateStruct(struct, options) {
+  return `${declareConst(struct.name, options)} = ${
     hasVarLengthMember(struct)
-      ? `(${LENGTH_VAR}${toTs ? ': number' : ''}) => `
+      ? `(${LENGTH_VAR}${options.toTs ? ': number' : ''}) => `
       : ''
   }d.struct({
   ${struct.members.map((member) => generateStructMember(member)).join('\n  ')}
@@ -66,7 +115,7 @@ function generateStruct(struct, toTs) {
 }
 
 /**
- * @param { import('wgsl_reflect').StructInfo } struct
+ * @param {import('wgsl_reflect').StructInfo} struct
  */
 function hasVarLengthMember(struct) {
   const member = struct.members[struct.members.length - 1].type;
@@ -74,27 +123,31 @@ function hasVarLengthMember(struct) {
 }
 
 /**
- * @param { import('wgsl_reflect').AliasInfo[] } aliases
+ * @param {import('wgsl_reflect').AliasInfo[]} aliases
+ * @param {Options} options
  */
-function generateAliases(aliases) {
+function generateAliases(aliases, options) {
   return aliases.length > 0
-    ? `/* aliases */
+    ? `\n/* aliases */
 ${aliases
-  .map((alias) => `export const ${alias.name} = ${generateType(alias.type)};`)
+  .map(
+    (alias) =>
+      `${declareConst(alias.name, options)} = ${generateType(alias.type)};`,
+  )
   .join('\n')}`
     : '';
 }
 
 /**
- * @param { import('wgsl_reflect').MemberInfo } member
+ * @param {import('wgsl_reflect').MemberInfo} member
  */
 function generateStructMember(member) {
   return `${member.name}: ${generateType(member.type)},`;
 }
 
 /**
- * @param { import('wgsl_reflect').TypeInfo } type_
- * @param { boolean } checkNonZeroLength
+ * @param {import('wgsl_reflect').TypeInfo} type_
+ * @param {boolean} checkNonZeroLength
  */
 function generateType(type_, checkNonZeroLength = true) {
   if (checkNonZeroLength && type_.size === 0 && !type_.isArray) {
@@ -124,8 +177,8 @@ function generateType(type_, checkNonZeroLength = true) {
 }
 
 /**
- * @param { string } type
- * @param { string } format
+ * @param {string} type
+ * @param {string} format
  */
 function typeToAlias(type, format) {
   if (
@@ -137,7 +190,7 @@ function typeToAlias(type, format) {
 }
 
 /**
- * @param { import('wgsl_reflect').TypeInfo } type
+ * @param {import('wgsl_reflect').TypeInfo} type
  */
 function replaceWithAlias(type) {
   return type instanceof TemplateInfo
@@ -146,15 +199,16 @@ function replaceWithAlias(type) {
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo[][] } bindGroups
+ * @param {import('wgsl_reflect').VariableInfo[][]} bindGroups
+ * @param {Options} options
  */
-function generateBindGroupLayouts(bindGroups) {
+function generateBindGroupLayouts(bindGroups, options) {
   return bindGroups.length > 0
-    ? `/* bindGroupLayouts */
+    ? `\n/* bindGroupLayouts */
 ${bindGroups
   .flatMap(
     (group, index) => `\
-export const layout${index} = tgpu.bindGroupLayout({
+${declareConst(`layout${index}`, options)} = tgpu.bindGroupLayout({
   ${generateGroupLayout(group)}
 }).$forceIndex(${index});`,
   )
@@ -183,7 +237,7 @@ const SAMPLE_TYPES = {
 };
 
 /**
- * @param { import('wgsl_reflect').VariableInfo[] } group
+ * @param {import('wgsl_reflect').VariableInfo[]} group
  */
 function generateGroupLayout(group) {
   return Array.from(group)
@@ -196,14 +250,14 @@ function generateGroupLayout(group) {
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function generateVariable(variable) {
   return RESOURCE_GENERATORS[variable.resourceType](variable);
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function generateUniformVariable(variable) {
   return `{
@@ -212,7 +266,7 @@ function generateUniformVariable(variable) {
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function generateStorageVariable(variable) {
   return `{
@@ -223,7 +277,7 @@ function generateStorageVariable(variable) {
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function getViewDimension(variable) {
   const type_ = variable.type.name;
@@ -245,7 +299,7 @@ function getViewDimension(variable) {
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function generateStorageTextureVariable(variable) {
   const viewDimension = getViewDimension(variable);
@@ -265,7 +319,7 @@ const SAMPLER_TYPES = {
 };
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function generateSamplerVariable(variable) {
   return `{
@@ -274,7 +328,7 @@ function generateSamplerVariable(variable) {
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function generateTextureVariable(variable) {
   const type_ = variable.type.name;
@@ -295,12 +349,38 @@ function generateTextureVariable(variable) {
 }
 
 /**
- * @param { import('wgsl_reflect').VariableInfo } variable
+ * @param {import('wgsl_reflect').VariableInfo} variable
  */
 function generateExternalTextureVariable(variable) {
   return `{
     externalTexture: {},
   }`;
+}
+
+/**
+ * @param {string} ident
+ * @param {Options} options
+ */
+function declareConst(ident, options) {
+  if (options.moduleSyntax === 'commonjs') {
+    if (options.exportsList === undefined) {
+      options.exportsList = [ident];
+    } else {
+      options.exportsList.push(ident);
+    }
+
+    return `const ${ident}`;
+  }
+  return `export const ${ident}`;
+}
+
+/**
+ * @param {Options} options
+ */
+function generateExports(options) {
+  return options.moduleSyntax === 'commonjs'
+    ? `\nmodule.exports = {${(options.exportsList ?? []).join(', ')}};`
+    : '';
 }
 
 export default main;
---
+++ packages/tgpu-cli/tests/outputPathCompiler.test.ts
@@ -0,0 +1,149 @@
+import { describe, expect, it } from 'vitest';
+import { createOutputPathCompiler } from '../outputPathCompiler.mjs';
+
+const test = ({
+  inputPattern,
+  outputPattern,
+  fileName,
+  expected,
+}: {
+  inputPattern?: string;
+  outputPattern: string;
+  fileName: string;
+  expected: string;
+}) =>
+  expect(
+    createOutputPathCompiler(
+      inputPattern ?? '**/*.wgsl',
+      outputPattern,
+    )(fileName),
+  ).toEqual(expected);
+
+describe('createOutputPathCompiler', () => {
+  it('generates an output path that is the same as output pattern when no placeholders are used', () => {
+    test({
+      outputPattern: 'hello.ts',
+      fileName: 'hello.wgsl',
+      expected: 'hello.ts',
+    });
+  });
+
+  it('generates output paths in the same directories as the source files when no directory separator is present in the output pattern', () => {
+    test({
+      outputPattern: '*.ts',
+      fileName: 'hello.wgsl',
+      expected: 'hello.ts',
+    });
+
+    test({
+      outputPattern: '*.js',
+      fileName: 'hello.wgsl',
+      expected: 'hello.js',
+    });
+
+    test({
+      outputPattern: '*.ts',
+      fileName: 'test/hello.wgsl',
+      expected: 'test/hello.ts',
+    });
+
+    test({
+      outputPattern: '*.ts',
+      fileName: 'test/test2/hello.wgsl',
+      expected: 'test/test2/hello.ts',
+    });
+  });
+
+  it('generates output paths, flattening all outputs to a single specified directory when no ** placeholder is used', () => {
+    test({
+      outputPattern: 'generated/*.ts',
+      fileName: 'hello.wgsl',
+      expected: 'generated/hello.ts',
+    });
+
+    test({
+      outputPattern: 'generated/generated2/*.ts',
+      fileName: 'hello.wgsl',
+      expected: 'generated/generated2/hello.ts',
+    });
+
+    test({
+      outputPattern: 'generated/*.ts',
+      fileName: 'test/hello.wgsl',
+      expected: 'generated/hello.ts',
+    });
+
+    test({
+      outputPattern: 'generated/*.ts',
+      fileName: 'test/test2/hello.wgsl',
+      expected: 'generated/hello.ts',
+    });
+  });
+
+  it('generates output paths keeping the directory structure when ** placeholder is used', () => {
+    test({
+      outputPattern: 'generated/**/*.ts',
+      fileName: 'hello.wgsl',
+      expected: 'generated/hello.ts',
+    });
+
+    test({
+      outputPattern: 'generated/**/*.ts',
+      fileName: 'test/hello.wgsl',
+      expected: 'generated/test/hello.ts',
+    });
+
+    test({
+      outputPattern: 'generated/**/*.ts',
+      fileName: 'test/test2/hello.wgsl',
+      expected: 'generated/test/test2/hello.ts',
+    });
+
+    test({
+      outputPattern: 'generated/**/files/*.ts',
+      fileName: 'test/test2/hello.wgsl',
+      expected: 'generated/test/test2/files/hello.ts',
+    });
+
+    test({
+      outputPattern: 'generated/**/*_1.ts',
+      fileName: 'test/test2/hello.wgsl',
+      expected: 'generated/test/test2/hello_1.ts',
+    });
+
+    test({
+      inputPattern: 'test/**/*.wgsl',
+      outputPattern: 'generated/**/*.ts',
+      fileName: 'test/test2/hello.wgsl',
+      expected: 'generated/test2/hello.ts',
+    });
+
+    test({
+      inputPattern: 'test/**/test3/*.wgsl',
+      outputPattern: 'generated/**/*.ts',
+      fileName: 'test/test2/test3/hello.wgsl',
+      expected: 'generated/test2/hello.ts',
+    });
+
+    test({
+      inputPattern: 'test/**/test3/*.wgsl',
+      outputPattern: 'generated/**/gen/*.ts',
+      fileName: 'test/test1/test2/test3/hello.wgsl',
+      expected: 'generated/test1/test2/gen/hello.ts',
+    });
+
+    test({
+      inputPattern: '**/wgsl/*.wgsl',
+      outputPattern: '**/ts/*.ts',
+      fileName: 'test/wgsl/hello.wgsl',
+      expected: 'test/ts/hello.ts',
+    });
+
+    test({
+      inputPattern: '**/test3/*.wgsl',
+      outputPattern: '**/gen/gen2/*.ts',
+      fileName: 'test/test1/test2/test3/hello.wgsl',
+      expected: 'test/test1/test2/gen/gen2/hello.ts',
+    });
+  });
+});
