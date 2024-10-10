74ab342a0c7722fbe4c462b82522f35df2bdd8d7
Iwo Plaza <iwoplaza@gmail.com>
2024-10-10 11:49:29 +0200

    Global font reset for inputs and buttons.

--- apps/typegpu-docs/src/components/design/Button.tsx
+++ apps/typegpu-docs/src/components/design/Button.tsx
@@ -13,7 +13,7 @@ export const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
   return (
     <button
       className={cs(
-        'inline-flex justify-center items-center font-sans box-border focus:ring-2 focus:ring-gradient-blue rounded-[6.25rem] text-sm px-5 py-2.5',
+        'inline-flex justify-center items-center box-border focus:ring-2 focus:ring-gradient-blue rounded-[6.25rem] text-sm px-5 py-2.5',
         accent
           ? 'bg-gradient-to-br from-gradient-purple to-gradient-blue text-white hover:from-gradient-purple-dark hover:to-gradient-blue-dark'
           : 'bg-white border-tameplum-100 border-2 hover:bg-tameplum-20',
