5f8b0ae6ac16b6a7684a73b8529b8cba31849e24
reczkok <reczkok@gmail.com>
2024-10-18 12:18:54 +0200

    initial

---
+++ apps/typegpu-docs/src/content/examples/algorithms/mnist-inference.ts
@@ -0,0 +1,225 @@
+/*
+{
+  "title": "Mnist Inference",
+  "category": "algorithms",
+  "tags": ["experimental"]
+}
+*/
+
+import { arrayOf, f32 } from 'typegpu/data';
+import tgpu from 'typegpu/experimental';
+
+if (!navigator.gpu) {
+  throw new Error('WebGPU is not supported by this browser.');
+}
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+  throw new Error('Could not find a compatible GPU.');
+}
+const device = await adapter.requestDevice();
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+const data = new Uint8Array(28 * 28);
+
+const draw = () => {
+  const size = 28;
+  const scale = canvas.width / size;
+  context.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas before drawing
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const value = data[i * size + j];
+      if (value > 0) {
+        // Only draw filled cells (as black)
+        context.fillStyle = 'black';
+        context.fillRect(j * scale, i * scale, scale, scale);
+      }
+    }
+  }
+};
+
+let isDrawing = false;
+canvas.addEventListener('mousedown', () => {
+  isDrawing = true;
+});
+
+canvas.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener('mousemove', (event) => {
+  if (!isDrawing) {
+    return;
+  }
+  const cellSize = canvas.width / 28;
+  const x = Math.floor((event.offsetX * window.devicePixelRatio) / cellSize);
+  const y = Math.floor((event.offsetY * window.devicePixelRatio) / cellSize);
+  if (x >= 0 && x < 28 && y >= 0 && y < 28) {
+    // Ensure coordinates are within bounds
+    data[y * 28 + x] = 255;
+    draw();
+  }
+});
+
+// weights = np.array(weights) # (784, 48)
+// biases = np.array(biases) # (48,)
+// weights2 = np.array(weights2) # (48, 10)
+// biases2 = np.array(biases2) # (10,)
+// data = {
+//         "weights": {
+//             "dense": {
+//                 "weights": weights.tolist(),
+//                 "biases": biases.tolist()
+//             },
+//             "dense_1": {
+//                 "weights": weights2.tolist(),
+//                 "biases": biases2.tolist()
+//             }
+//         }
+//     }
+
+// load json file
+const response = await fetch('/TypeGPU/model.json');
+const model = await response.json();
+
+const weights = model.weights.dense.weights.flat();
+const biases = model.weights.dense.biases;
+const weights2 = model.weights.dense_1.weights.flat();
+const biases2 = model.weights.dense_1.biases;
+
+console.log(weights);
+console.log(biases);
+console.log(weights2);
+console.log(biases2);
+
+const root = await tgpu.init();
+
+const input = tgpu
+  .createBuffer(arrayOf(f32, 784), [...data])
+  .$usage(tgpu.Storage);
+const layer1 = tgpu.createBuffer(arrayOf(f32, 48)).$usage(tgpu.Storage);
+const output = tgpu.createBuffer(arrayOf(f32, 10)).$usage(tgpu.Storage);
+
+const weightsBuffer = tgpu
+  .createBuffer(arrayOf(f32, 784 * 48), weights)
+  .$usage(tgpu.Storage);
+const biasesBuffer = tgpu
+  .createBuffer(arrayOf(f32, 48), biases)
+  .$usage(tgpu.Storage);
+const weights2Buffer = tgpu
+  .createBuffer(arrayOf(f32, 48 * 10), weights2)
+  .$usage(tgpu.Storage);
+const biases2Buffer = tgpu
+  .createBuffer(arrayOf(f32, 10), biases2)
+  .$usage(tgpu.Storage);
+
+const layerShader = `
+  @binding(0) @group(0) var<storage, read> input: array<f32>;
+  @binding(1) @group(0) var<storage, read_write> output: array<f32>;
+
+  @binding(0) @group(1) var<storage, read> weights: array<f32>;
+  @binding(1) @group(1) var<storage, read> biases: array<f32>;
+
+  @compute @workgroup_size(1)
+  fn main(@builtin(global_invocation_id) gid: vec3u) {
+    let inputSize = arrayLength( &input );
+    let outputSize = arrayLength( &output );
+    let weightsSize = arrayLength( &weights );
+    let biasesSize = arrayLength( &biases );
+
+    let i = gix;
+    // we can assume the number of invocations won't exceed the size of the output array
+
+    let weightsOffset = i * inputSize;
+    var sum = 0.0;
+
+    for (var j = 0; j < inputSize; j = j + 1) {
+      sum = sum + input[j] * weights[weightsOffset + j];
+    }
+
+    sum = sum + biases[i];
+
+    output[i] = sum;
+  }
+`;
+
+const inputOutput = tgpu.bindGroupLayout({
+  input: {
+    storage: (length: number) => {
+      arrayOf(f32, length);
+    },
+    access: 'readonly',
+  },
+  output: {
+    storage: (length: number) => {
+      arrayOf(f32, length);
+    },
+    access: 'mutable',
+  },
+});
+
+const weightsBiases = tgpu.bindGroupLayout({
+  weights: {
+    storage: (length: number) => {
+      arrayOf(f32, length);
+    },
+    access: 'readonly',
+  },
+  biases: {
+    storage: (length: number) => {
+      arrayOf(f32, length);
+    },
+    access: 'readonly',
+  },
+});
+
+const pipelineLayout = device.createPipelineLayout({
+  bindGroupLayouts: [root.unwrap(inputOutput), root.unwrap(weightsBiases)],
+});
+
+const pipeline = device.createComputePipeline({
+  layout: pipelineLayout,
+  compute: {
+    module: device.createShaderModule({
+      code: layerShader,
+    }),
+  },
+});
+
+const encoder = device.createCommandEncoder();
+
+const pass = encoder.beginComputePass();
+pass.setPipeline(pipeline);
+pass.setBindGroup(0, root.unwrap(inputOutput.populate({ input, layer1 })));
+pass.setBindGroup(
+  1,
+  root.unwrap(
+    weightsBiases.populate({ weights: weightsBuffer, biases: biasesBuffer }),
+  ),
+);
+pass.dispatchWorkgroups(48);
+pass.end();
+device.queue.submit([encoder.finish()]);
+
+const pass2 = encoder.beginComputePass();
+pass2.setPipeline(pipeline);
+pass2.setBindGroup(
+  0,
+  root.unwrap(inputOutput.populate({ input: layer1, output })),
+);
+pass2.setBindGroup(
+  1,
+  root.unwrap(
+    weightsBiases.populate({ weights: weights2Buffer, biases: biases2Buffer }),
+  ),
+);
+pass2.dispatchWorkgroups(10);
+pass2.end();
+device.queue.submit([encoder.finish()]);
+
+const outputData = output.read();
+outputData.then((data) => {
+  const max = Math.max(...data);
+  const index = data.indexOf(max);
+  console.log('Prediction:', index);
+});
