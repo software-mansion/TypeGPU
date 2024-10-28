d4b9c6896140b7428c4b1bd44c0c500df3360ca1
reczkok <reczkok@gmail.com>
2024-10-28 16:09:51 +0100

    don't ask

--- packages/typegpu/tests/wgpuMatrixIntegration.test.ts
+++ packages/typegpu/tests/wgpuMatrixIntegration.test.ts
@@ -34,7 +34,6 @@ describe('mat3x3f', () => {
     expect(m.mat3.equals(mat, mat)).toBe(true);
 
     expect([...mat.elements()]).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-    console.log(mat);
     m.mat3.identity(mat);
     expect([...mat.elements()]).toEqual([1, 0, 0, 0, 1, 0, 0, 0, 1]);
   });
