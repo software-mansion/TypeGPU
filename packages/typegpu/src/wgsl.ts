d311b9bca7d09084fcef93db433d252654422d9a
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2024-12-17 17:44:46 +0100

    Remove wgsl.code and .fn (#659)

--- packages/typegpu/src/wgsl.ts
+++ packages/typegpu/src/wgsl.ts
@@ -1,13 +1,7 @@
-import { code } from './tgpuCode';
 import { constant } from './tgpuConstant';
-import { declare } from './tgpuDeclare';
-import { fn } from './tgpuFunction';
 import { variable } from './tgpuVariable';
 
-export default Object.assign(code, {
-  code,
-  fn,
+export default {
   constant,
-  declare,
   var: variable,
-});
+};
