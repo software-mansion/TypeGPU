0ad9f26288ccf2539f155ada407029ba13cf5122
reczkok <reczkok@gmail.com>
2025-01-24 10:22:51 +0100

    more loose data resolution tests

--- packages/typegpu/tests/resolve.test.ts
+++ packages/typegpu/tests/resolve.test.ts
@@ -201,6 +201,83 @@ describe('tgpu resolve', () => {
     );
   });
 
+  it('should resolve an unstruct with a disarray to its corresponding struct', () => {
+    const vertexInfo = d.unstruct({
+      color: d.snorm8x4,
+      colorHDR: d.unorm10_10_10_2,
+      position2d: d.float16x2,
+      extra: d.disarrayOf(d.snorm8x4, 16),
+    });
+
+    const resolved = tgpu.resolve({
+      template: 'fn foo() { var v: vertexInfo; }',
+      externals: { vertexInfo },
+      names: 'strict',
+    });
+
+    expect(parse(resolved)).toEqual(
+      parse(`
+        struct vertexInfo {
+          color: vec4f,
+          colorHDR: vec4f,
+          position2d: vec2f,
+          extra: array<vec4f, 16>,
+        }
+        fn foo() { var v: vertexInfo; }
+      `),
+    );
+  });
+
+  it('should resolve an unstruct with a complex nested structure', () => {
+    const vertexInfo = d.unstruct({
+      color: d.snorm8x4,
+      colorHDR: d.unorm10_10_10_2,
+      position2d: d.float16x2,
+      extra: d
+        .unstruct({
+          a: d.snorm8,
+          b: d.snorm8x4,
+          c: d.float16x2,
+        })
+        .$name('extra'),
+      more: d.disarrayOf(
+        d.unstruct({ a: d.snorm8, b: d.snorm8x4 }).$name('more'),
+        16,
+      ),
+    });
+
+    const resolved = tgpu.resolve({
+      template: 'fn foo() { var v: vertexInfo; }',
+      externals: { vertexInfo },
+      names: 'strict',
+    });
+
+    expect(parse(resolved)).toEqual(
+      parse(`
+        struct extra {
+          a: f32,
+          b: vec4f,
+          c: vec2f,
+        }
+
+        struct more {
+          a: f32,
+          b: vec4f,
+        }
+
+        struct vertexInfo {
+          color: vec4f,
+          colorHDR: vec4f,
+          position2d: vec2f,
+          extra: extra,
+          more: array<more, 16>,
+        }
+
+        fn foo() { var v: vertexInfo; }
+      `),
+    );
+  });
+
   it('should resolve object externals and replace their usages in template', () => {
     const getColor = tgpu['~unstable']
       .fn([], d.vec3f)
