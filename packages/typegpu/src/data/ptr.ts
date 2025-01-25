ee8359955940904a949a8bf14a8add1bec679717
Iwo Plaza <iwoplaza@gmail.com>
2025-01-25 23:36:39 +0100

    Infer type properly

--- packages/typegpu/src/data/ptr.ts
+++ packages/typegpu/src/data/ptr.ts
@@ -2,7 +2,7 @@ import type { AnyData } from './dataTypes';
 import type { Exotic } from './exotic';
 import type { PtrFn } from './wgslTypes';
 
-export function ptrFn<T extends AnyData>(inner: AnyData): PtrFn<Exotic<T>> {
+export function ptrFn<T extends AnyData>(inner: T): PtrFn<Exotic<T>> {
   return {
     type: 'ptrFn',
     inner: inner as Exotic<T>,
