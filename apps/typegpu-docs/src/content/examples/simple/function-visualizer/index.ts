889d60ce8d420fdbe4753b75da44d98ea2940a93
Aleksander Katan <aleksander.katan@swmansion.com>
2025-02-11 11:31:04 +0100

    Add function point evaluation

--- apps/typegpu-docs/src/content/examples/simple/function-visualizer/index.ts
+++ apps/typegpu-docs/src/content/examples/simple/function-visualizer/index.ts
@@ -1,6 +1,11 @@
 import tgpu from 'typegpu';
 
-// const fRaw = 'x + 1';
+const input = {
+  functions: {
+    f: 'sin(x*10)/2',
+  },
+  interpolationPoins: 27,
+};
 
 const root = await tgpu.init();
 const device = root.device;
@@ -15,17 +20,72 @@ context.configure({
   alphaMode: 'premultiplied',
 });
 
-const shaderCode = /* wgsl */ `
+const computeShaderCode = /* wgsl */ `
+fn temp(x: f32) -> f32 {
+  return ${input.functions.f};
+}
+
+@group(0) @binding(0) var<storage, read_write> lineVertices: array<vec2f>;
+
+@compute @workgroup_size(1) fn computePoints(
+  @builtin(global_invocation_id) id: vec3u
+) {
+  let point = (-1.0f + 2.0f/f32(${input.interpolationPoins} - 1) * f32(id.x));
+  let value = temp(point);
+  lineVertices[id.x] = vec2f(point, value);
+}
+`;
+
+const computeShaderModule = device.createShaderModule({
+  label: 'Compute shader module',
+  code: computeShaderCode,
+});
+
+const computePipeline = device.createComputePipeline({
+  label: 'point computation pipeline',
+  layout: 'auto',
+  compute: {
+    module: computeShaderModule,
+  },
+});
+
+const lineVerticesBuffer = device.createBuffer({
+  label: 'line vertices buffer',
+  size: input.interpolationPoins * 4 * 2,
+  usage: GPUBufferUsage.STORAGE,
+});
+
+const bindGroup = device.createBindGroup({
+  label: 'bindGroup for work buffer',
+  layout: computePipeline.getBindGroupLayout(0),
+  entries: [{ binding: 0, resource: { buffer: lineVerticesBuffer } }],
+});
+
+const encoder = device.createCommandEncoder({
+  label: 'doubling encoder',
+});
+const pass = encoder.beginComputePass({
+  label: 'doubling compute pass',
+});
+pass.setPipeline(computePipeline);
+pass.setBindGroup(0, bindGroup);
+pass.dispatchWorkgroups(input.interpolationPoins);
+pass.end();
+
+const commandBuffer = encoder.finish();
+device.queue.submit([commandBuffer]);
+
+// buffor do którego piszemy pozycje w compute
+// w vs obliczamy pozycje wierzchołków
+
+const vertexFraxmentShaderCode = /* wgsl */ `
+@group(0) @binding(0) var<storage, read> lineVertices: array<vec2f>;
+
 @vertex fn vs(
   @builtin(vertex_index) vertexIndex : u32
 ) -> @builtin(position) vec4f {
-  let pos = array(
-    vec2f( 0.0,  0.5),  // top center
-    vec2f(-0.5, -0.5),  // bottom left
-    vec2f( 0.5, -0.5)   // bottom right
-  );
 
-  return vec4f(pos[vertexIndex], 0.0, 1.0);
+  return vec4f(lineVertices[vertexIndex], 0.0, 1.0);
 }
 
 @fragment fn fs() -> @location(0) vec4f {
@@ -33,83 +93,66 @@ const shaderCode = /* wgsl */ `
 }
 `;
 
-const shaderModule = device.createShaderModule({ code: shaderCode });
+const vertexFraxmentShaderModule = device.createShaderModule({
+  label: 'Render module',
+  code: vertexFraxmentShaderCode,
+});
 
-const pipeline = device.createRenderPipeline({
-  layout: device.createPipelineLayout({
-    bindGroupLayouts: [],
-  }),
+const renderPipeline = device.createRenderPipeline({
+  label: 'Render pipeline',
+  layout: 'auto',
   vertex: {
-    module: shaderModule,
+    module: vertexFraxmentShaderModule,
   },
   fragment: {
-    module: shaderModule,
-    targets: [
-      {
-        format: presentationFormat,
-      },
-    ],
+    module: vertexFraxmentShaderModule,
+    targets: [{ format: presentationFormat }],
   },
   primitive: {
-    topology: 'triangle-strip',
+    topology: 'line-strip',
   },
 });
 
-function draw(spanXValue: number, spanYValue: number) {
-  const textureView = context.getCurrentTexture().createView();
-  const renderPassDescriptor: GPURenderPassDescriptor = {
-    colorAttachments: [
-      {
-        view: textureView,
-        clearValue: [0, 0, 0, 0],
-        loadOp: 'clear',
-        storeOp: 'store',
-      },
-    ],
-  };
+const renderBindGroup = device.createBindGroup({
+  label: 'bindGroup for work buffer',
+  layout: renderPipeline.getBindGroupLayout(0),
+  entries: [{ binding: 0, resource: { buffer: lineVerticesBuffer } }],
+});
 
-  const commandEncoder = device.createCommandEncoder();
-  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-  passEncoder.setPipeline(pipeline);
-  passEncoder.draw(3);
-  passEncoder.end();
+const renderPassDescriptor = {
+  label: 'Render pass',
+  colorAttachments: [
+    {
+      view: undefined as unknown as GPUTextureView,
+      clearValue: [0.3, 0.3, 0.3, 1] as const,
+      loadOp: 'clear' as const,
+      storeOp: 'store' as const,
+    },
+  ],
+};
 
-  device.queue.submit([commandEncoder.finish()]);
+function render() {
+  // Get the current texture from the canvas context and
+  // set it as the texture to render to.
+  renderPassDescriptor.colorAttachments[0].view = context
+    .getCurrentTexture()
+    .createView();
+
+  // make a command encoder to start encoding commands
+  const encoder = device.createCommandEncoder({ label: 'our encoder ' });
+
+  // make a render pass encoder to encode render specific commands
+  const pass = encoder.beginRenderPass(renderPassDescriptor);
+  pass.setPipeline(renderPipeline);
+  pass.setBindGroup(0, renderBindGroup);
+  pass.draw(input.interpolationPoins); // call our vertex shader 3 times.
+  pass.end();
+
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
 }
 
-let spanX = 10;
-let spanY = 10;
-
-// deferring to wait for canvas to init
-setTimeout(() => {
-  draw(spanX, spanY);
-}, 100);
-
-// #region Example controls and cleanup
-
-export const controls = {
-  'x span ↔️': {
-    initial: spanY,
-    min: 0,
-    max: 20,
-    step: 1,
-    onSliderChange: (newValue: number) => {
-      spanX = newValue;
-      draw(spanX, spanY);
-    },
-  },
-
-  'y span ↕️': {
-    initial: spanY,
-    min: 0,
-    max: 20,
-    step: 1,
-    onSliderChange: (newValue: number) => {
-      spanY = newValue;
-      draw(spanX, spanY);
-    },
-  },
-};
+render();
 
 export function onCleanup() {
   root.destroy();
