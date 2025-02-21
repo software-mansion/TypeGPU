b3aa9a5d4af0b530e179001c43968c59d54f3df7
Aleksander Katan <aleksander.katan@swmansion.com>
2025-02-21 14:50:46 +0100

    Remove unused functions

--- apps/typegpu-docs/src/pages/benchmark/suites.ts
+++ apps/typegpu-docs/src/pages/benchmark/suites.ts
@@ -5,6 +5,17 @@ import type { BenchParameterSet } from './parameter-set';
 import { dummySuite } from './test_suites/dummy';
 import { atomWithUrl } from './atom-with-url';
 
+export type TestIdentifier = `${string}_${string}`;
+
+export function identifierOf(
+  suiteName: string,
+  testName: string,
+): TestIdentifier {
+  return `${suiteName.replaceAll(' ', '-')}_${testName.replaceAll(' ', '-')}`;
+}
+
+export const selectedTestsAtom = atomWithUrl<TestIdentifier[]>('t', []);
+
 export type Suite = () => {
   suiteSetup(
     params: BenchParameterSet,
@@ -18,39 +29,3 @@ export const suites: Record<string, Suite> = {
   Dummy: dummySuite,
   'Mass transfer': massTransferSuite,
 };
-
-export type TestIdentifier = `${string}_${string}`;
-
-export function testIdentifierOf(
-  suiteName: string,
-  testName: string,
-): TestIdentifier {
-  return `${suiteName.replaceAll(' ', '-')}_${testName.replaceAll(' ', '-')}`;
-}
-
-export function namesFromIdentifier(identifier: TestIdentifier): {
-  suiteName: string;
-  testName: string;
-} {
-  const split = identifier.split('_');
-  if (split.length !== 2) {
-    throw new Error(`Invalid test identifier ${identifier}.`);
-  }
-  return {
-    suiteName: split[0].replaceAll('-', ' '),
-    testName: split[1].replaceAll('-', ' '),
-  };
-}
-
-export function testIdentifiers(): Set<TestIdentifier> {
-  const result = new Set<TestIdentifier>();
-  for (const suiteName in suites) {
-    const suite = suites[suiteName]();
-    for (const testName in suite) {
-      result.add(testIdentifierOf(suiteName, testName));
-    }
-  }
-  return result;
-}
-
-export const selectedTestsAtom = atomWithUrl<TestIdentifier[]>('t', []);
