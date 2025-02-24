62f7f7f8ca4cd2c47cca2bf4f413a16ef6f00aa5
Aleksander Katan <aleksander.katan@swmansion.com>
2025-02-24 15:52:45 +0100

    Add useMemo to CheckboxTree

--- apps/typegpu-docs/src/pages/benchmark/components/checkbox-tree.tsx
+++ apps/typegpu-docs/src/pages/benchmark/components/checkbox-tree.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from 'jotai/react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import {
   selectedTestsAtom,
   identifierOf,
@@ -7,51 +7,33 @@ import {
   type TestIdentifier,
 } from '../suites.js';
 
-function TestCheckbox(props: { suiteName: string; testName: string }) {
-  const { suiteName, testName } = props;
-  const identifier = identifierOf(suiteName, testName);
-  const [selected, setSelected] = useAtom(selectedTestsAtom);
-
-  return (
-    <div>
-      <input
-        type="checkbox"
-        id="option"
-        className="ml-6 text-sm"
-        checked={selected.includes(identifier)}
-        onChange={() =>
-          selected.includes(identifier)
-            ? setSelected(selected.filter((item) => item !== identifier))
-            : setSelected([...selected, identifier])
-        }
-      />
-      {testName}
-    </div>
-  );
-}
-
 type CheckboxState = 'checked' | 'unchecked' | 'indeterminate';
 
 export function SuiteCheckbox(props: { suiteName: string; suite: Suite }) {
-  const [opened, setOpened] = useState(false);
   const { suiteName, suite } = props;
   const [selected, setSelected] = useAtom(selectedTestsAtom);
 
-  const childrenIdentifiers: TestIdentifier[] = [];
-  for (const testName in suite.tests) {
-    childrenIdentifiers.push(identifierOf(suiteName, testName));
-  }
+  const childrenIdentifiers: TestIdentifier[] = useMemo(
+    () =>
+      Object.keys(suite.tests).map((testName) =>
+        identifierOf(suiteName, testName),
+      ),
+    [suite, suiteName],
+  );
 
-  const selectedChildren = selected.filter((item) =>
-    childrenIdentifiers.includes(item),
-  ).length;
-  const totalChildren = Object.keys(suite.tests).length;
+  const selectedChildrenCount = useMemo(
+    () => selected.filter((item) => childrenIdentifiers.includes(item)).length,
+    [selected, childrenIdentifiers],
+  );
+  const totalChildrenCount = Object.keys(suite.tests).length;
   const status: CheckboxState =
-    selectedChildren === totalChildren
+    selectedChildrenCount === totalChildrenCount
       ? 'checked'
-      : selectedChildren === 0
+      : selectedChildrenCount === 0
         ? 'unchecked'
         : 'indeterminate';
+
+  const [opened, setOpened] = useState(status === 'indeterminate');
 
   const cRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
@@ -87,6 +69,29 @@ export function SuiteCheckbox(props: { suiteName: string; suite: Suite }) {
         Object.keys(suite.tests).map((key) => (
           <TestCheckbox suiteName={suiteName} testName={key} key={key} />
         ))}
+    </div>
+  );
+}
+
+function TestCheckbox(props: { suiteName: string; testName: string }) {
+  const { suiteName, testName } = props;
+  const identifier = identifierOf(suiteName, testName);
+  const [selected, setSelected] = useAtom(selectedTestsAtom);
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        id="option"
+        className="ml-6 text-sm"
+        checked={selected.includes(identifier)}
+        onChange={() =>
+          selected.includes(identifier)
+            ? setSelected(selected.filter((item) => item !== identifier))
+            : setSelected([...selected, identifier])
+        }
+      />
+      {testName}
     </div>
   );
 }
