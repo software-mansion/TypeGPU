e9827f98ca1e95700f4893519e19b7faaf4bed69
Iwo Plaza <iwoplaza@gmail.com>
2025-02-24 13:18:02 +0100

    fix: Better type hints for inferred struct (& unstruct) schemas

--- apps/typegpu-docs/ec.config.mjs
+++ apps/typegpu-docs/ec.config.mjs
@@ -1,6 +1,14 @@
 import ecTwoSlash from 'expressive-code-twoslash';
+import ts from 'typescript';
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
-  plugins: [ecTwoSlash()],
+  plugins: [
+    ecTwoSlash({
+      twoslashOptions: {
+        strict: true,
+        compilerOptions: { moduleResolution: ts.ModuleResolutionKind.Bundler },
+      },
+    }),
+  ],
 };
