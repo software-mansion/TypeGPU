5e29d98f60c7a797bd2411711446d66a8f375f1b
Aleksander Katan <aleksander.katan@swmansion.com>
2025-03-17 17:31:07 +0100

    Adjust stackblitz canvas size

--- apps/typegpu-docs/src/components/stackblitz/stackBlitzIndex.ts
+++ apps/typegpu-docs/src/components/stackblitz/stackBlitzIndex.ts
@@ -5,8 +5,11 @@ const body = document.querySelector('body') as HTMLBodyElement;
 body.style.display = 'flex';
 body.style.flexDirection = 'column';
 body.style.alignItems = 'center';
-body.style.height = '75vh';
+body.style.height = '100vh';
 body.style.gap = '1.5rem';
+body.style.margin = '0';
+body.style.boxSizing = 'border-box';
+body.style.padding = '1rem';
 
 // Resize canvases
 for (const canvas of document.querySelectorAll('canvas')) {
@@ -22,19 +25,24 @@ for (const canvas of document.querySelectorAll('canvas')) {
   frame.appendChild(canvas);
   container.appendChild(frame);
 
-  const aspectRatio = canvas.dataset.aspectRatio ?? '1';
-
   container.style.display = 'flex';
   container.style.flex = '1';
   container.style.justifyContent = 'center';
-  container.style.alignItems = 'center';
+  container.style.alignItems = 'top';
   container.style.width = '100%';
 
   container.style.containerType = 'size';
 
   frame.style.position = 'relative';
-  frame.style.aspectRatio = aspectRatio;
-  frame.style.height = `min(calc(min(100cqw, 100cqh)/(${aspectRatio})), min(100cqw, 100cqh))`;
+
+  if (canvas.dataset.fitToContainer !== undefined) {
+    frame.style.width = '100%';
+    frame.style.height = '100%';
+  } else {
+    const aspectRatio = canvas.dataset.aspectRatio ?? '1';
+    frame.style.aspectRatio = aspectRatio;
+    frame.style.height = `min(calc(min(100cqw, 100cqh)/(${aspectRatio})), min(100cqw, 100cqh))`;
+  }
 
   canvas.style.position = 'absolute';
   canvas.style.width = '100%';
