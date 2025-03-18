e56671162a11cb02def7e6c29412a13efacd5034
Iwo Plaza <iwoplaza@gmail.com>
2025-03-18 01:53:26 +0100

    Dug up the noise package

--- packages/typegpu-noise/tsup.config.ts
+++ packages/typegpu-noise/tsup.config.ts
@@ -10,14 +10,11 @@ export default defineConfig({
   entry,
   outDir: 'dist',
   format: ['cjs', 'esm'],
-  tsconfig: './tsconfig.json',
   target: 'es2017',
   splitting: true,
   sourcemap: true,
   minify: !inDevMode,
-  // When in dev mode, we first build then watch, so we do not want the `watch` to
-  // clean the out directory.
-  clean: !inDevMode,
+  clean: true,
   dts: true,
   define: {
     'process.env.NODE_ENV': JSON.stringify(
