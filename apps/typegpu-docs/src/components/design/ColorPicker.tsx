5c11f2b5a6abd1b53fb143895de9d0f6c10bc463
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2025-05-06 09:51:30 +0200

    docs: ColorPicker example controls (#1214)

---
+++ apps/typegpu-docs/src/components/design/ColorPicker.tsx
@@ -0,0 +1,35 @@
+function hexToRgb(hex: string) {
+  return [
+    Number.parseInt(hex.slice(1, 3), 16) / 255,
+    Number.parseInt(hex.slice(3, 5), 16) / 255,
+    Number.parseInt(hex.slice(5, 7), 16) / 255,
+  ] as const;
+}
+
+function componentToHex(c: number) {
+  const hex = (c * 255).toString(16);
+  return hex.length === 1 ? `0${hex}` : hex;
+}
+
+function rgbToHex(rgb: readonly [number, number, number]) {
+  return `#${rgb.map(componentToHex).join('')}`;
+}
+
+type Props = {
+  /**
+   * RGB 0-1
+   */
+  value: readonly [number, number, number];
+  onChange: (value: readonly [number, number, number]) => void;
+};
+
+export function ColorPicker({ value, onChange }: Props) {
+  return (
+    <input
+      value={rgbToHex(value)}
+      className="w-full h-10 p-1 rounded bg-tameplum-50"
+      type="color"
+      onChange={(e) => onChange(hexToRgb(e.target.value))}
+    />
+  );
+}
