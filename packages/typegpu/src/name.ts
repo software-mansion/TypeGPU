96e38ed46fba6796d7fbbdad8f7bacf89a8f6cbf
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2025-05-12 10:20:26 +0200

    Apply suggestions from code review
    Co-authored-by: Iwo Plaza <iwoplaza@gmail.com>

--- packages/typegpu/src/name.ts
+++ packages/typegpu/src/name.ts
@@ -4,29 +4,23 @@ interface MetaData {
   name: string | undefined;
 }
 
-declare global {
-  var __TYPEGPU_META__: WeakMap<object, MetaData>;
+interface GlobalWithMeta {
+  __TYPEGPU_META__: WeakMap<object, MetaData>;
 }
 
 function isObject(value: unknown): value is object {
   return !!value && (typeof value === 'object' || typeof value === 'function');
 }
 
-function isForwarded(value: object): value is { [$getNameForward]: object } {
-  if (!($getNameForward in value)) {
-    return false;
-  }
-  return isObject(value[$getNameForward]);
+function isForwarded(value: object): value is { [$getNameForward]: unknown } {
+  return !!value?.[$getNameForward];
 }
 
 export function getName(definition: unknown): string | undefined {
-  if (!isObject(definition)) {
-    return undefined;
-  }
   if (isForwarded(definition)) {
     return getName(definition[$getNameForward]);
   }
-  return globalThis.__TYPEGPU_META__?.get(definition)?.name;
+  return (globalThis as unknown as GlobalWithMeta).__TYPEGPU_META__?.get(definition)?.name;
 }
 
 export function setName(definition: object, name: string): void {
@@ -39,7 +33,7 @@ export function setName(definition: object, name: string): void {
  * Can be assigned a name. Not to be confused with
  * being able to HAVE a name.
  * The `$name` function should use `setName` to rename the object itself,
- * or rename the object `$labelForward` symbol points to instead if applicable.
+ * or rename the object `$getNameForward` symbol points to instead if applicable.
  */
 export interface TgpuNamable {
   $name(label: string): this;
