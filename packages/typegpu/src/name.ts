d6f6db747ec2cbcf11ff4f2b875280db4fded127
Aleksander Katan <aleksander.katan@swmansion.com>
2025-05-12 10:48:16 +0200

    Fix types

--- packages/typegpu/src/name.ts
+++ packages/typegpu/src/name.ts
@@ -8,24 +8,25 @@ interface GlobalWithMeta {
   __TYPEGPU_META__: WeakMap<object, MetaData>;
 }
 
-function isObject(value: unknown): value is object {
-  return !!value && (typeof value === 'object' || typeof value === 'function');
-}
-
-function isForwarded(value: object): value is { [$getNameForward]: unknown } {
-  return !!value?.[$getNameForward];
+function isForwarded(value: unknown): value is { [$getNameForward]: unknown } {
+  return !!value &&
+    (typeof value === 'object' || typeof value === 'function') &&
+    $getNameForward in value;
 }
 
 export function getName(definition: unknown): string | undefined {
   if (isForwarded(definition)) {
     return getName(definition[$getNameForward]);
   }
-  return (globalThis as unknown as GlobalWithMeta).__TYPEGPU_META__?.get(definition)?.name;
+  return (globalThis as unknown as GlobalWithMeta).__TYPEGPU_META__?.get(
+    // biome-ignore lint/suspicious/noExplicitAny: it's fine, if it's not an object, the get will return undefined
+    definition as any,
+  )?.name;
 }
 
 export function setName(definition: object, name: string): void {
-  globalThis.__TYPEGPU_META__ ??= new WeakMap();
-  const map = globalThis.__TYPEGPU_META__;
+  (globalThis as unknown as GlobalWithMeta).__TYPEGPU_META__ ??= new WeakMap();
+  const map = (globalThis as unknown as GlobalWithMeta).__TYPEGPU_META__;
   map.set(definition, { ...map.get(definition), name });
 }
 
