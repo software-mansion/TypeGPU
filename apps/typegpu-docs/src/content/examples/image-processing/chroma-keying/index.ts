e0eb5f5e38666c7db6e00ce44ab585d89880ab50
mhawryluk <marcinhawrylukxx@gmail.com>
2025-05-13 10:23:56 +0200

    docs: Make Chroma Keying example more responsive

--- apps/typegpu-docs/src/content/examples/image-processing/chroma-keying/index.ts
+++ apps/typegpu-docs/src/content/examples/image-processing/chroma-keying/index.ts
@@ -175,6 +175,15 @@ const renderPassDescriptor: GPURenderPassDescriptor = {
   ],
 };
 
+video.addEventListener('resize', () => {
+  const aspectRatio = video.videoWidth / video.videoHeight;
+  video.style.height = `${video.clientWidth / aspectRatio}px`;
+  if (canvas.parentElement) {
+    canvas.parentElement.style.aspectRatio = `${aspectRatio}`;
+    canvas.parentElement.style.height = `min(100cqh, calc(100cqw/(${aspectRatio})))`;
+  }
+});
+
 async function drawFrame() {
   const frame = await getFrame();
   if (!frame) {
@@ -216,26 +225,15 @@ function run() {
   }
 }
 
-// #region UI
-
-const table = document.querySelector('.rgb') as HTMLDivElement;
-
-video.addEventListener('click', (event) => {
-  const { offsetX: x, offsetY: y } = event;
-
-  // Sampling the video frame
-  samplingContext.drawImage(video, 0, 0, width, height);
-  const [r, g, b] = samplingContext.getImageData(x, y, 1, 1).data;
-
-  table.innerText = `R: ${r} G: ${g} B: ${b}`;
-  colorBuffer.write(d.vec3f(r / 255, g / 255, b / 255));
-});
-
-// #endregion
-
 // #region Example controls & Cleanup
 
 export const controls = {
+  color: {
+    onColorChange: (value: readonly [number, number, number]) => {
+      colorBuffer.write(d.vec3f(...value));
+    },
+    initial: [0, 1, 0] as const,
+  },
   threshold: {
     initial: 0.1,
     min: 0,
