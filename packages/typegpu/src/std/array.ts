09554507ff1ba85807539750c2cde99b28a981a7
lursz <lukoszr@gmail.com>
2025-05-19 14:29:26 +0200

    std.arrayLength now checks for fixed size

--- packages/typegpu/src/std/array.ts
+++ packages/typegpu/src/std/array.ts
@@ -1,12 +1,21 @@
-import { u32 } from '../data/numeric.ts';
+import { abstractInt, u32 } from '../data/numeric.ts';
 import { ptrFn } from '../data/ptr.ts';
 import type { AnyWgslData } from '../data/wgslTypes.ts';
+import { isWgslArray } from '../data/wgslTypes.ts';
 import { createDualImpl } from '../shared/generators.ts';
 
 export const arrayLength = createDualImpl(
   // CPU implementation
   (a: unknown[]) => a.length,
   // GPU implementation
-  (a) => ({ value: `arrayLength(${a.value})`, dataType: u32 }),
+  (a) => {
+    if (isWgslArray(a.dataType) && a.dataType.elementCount > 0) {
+      return {
+        value: String(a.dataType.elementCount),
+        dataType: abstractInt,
+      };
+    }
+    return { value: `arrayLength(${a.value})`, dataType: u32 };
+  },
   (a) => [ptrFn(a.dataType as AnyWgslData)],
 );
