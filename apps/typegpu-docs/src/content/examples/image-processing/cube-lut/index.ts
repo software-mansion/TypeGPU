3b038133a3bcf738bb0550e4dc4f23beebce47c9
bielok <138621639+bielok@users.noreply.github.com>
2025-05-28 15:26:35 -0300

    Added image tuning options

--- apps/typegpu-docs/src/content/examples/image-processing/cube-lut/index.ts
+++ apps/typegpu-docs/src/content/examples/image-processing/cube-lut/index.ts
@@ -6,26 +6,25 @@ type ParsedLUT = {
   type: string,
   size: number,
   domain: number[][],
-  data: number[][],
+  data: Float16Array | undefined,
 };
-
-let lut: ParsedLUT;
-/* = {
-  title: "No LUT",
-  type: "3D",
-  size: 2,
-  domain: [[0,0,0], [1,1,1]],
-  data: [[1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1]]
-};*/
-
 
 const LUTDesc = d.struct({
   lutSize: d.f32,
   lutMin: d.vec3f,
   lutMax: d.vec3f,
+  enabled: d.f32,
 });
-let lutTexture: TgpuTexture;
 
+const Adjustments = d.struct({
+  exposure: d.f32,
+  contrast: d.f32,
+  highlights: d.f32,
+  shadows: d.f32,
+  saturation: d.f32,
+});
+
+let lutTexture: TgpuTexture;
 
 const shaderCode = /* wgsl */ `
 @vertex
@@ -61,13 +60,50 @@ fn main_frag(@location(0) uv: vec2f) -> @location(0) vec4f {
   let dmin = lut.lutMin;
   let dmax = lut.lutMax;
 
+  let inputLuminance = dot(color, vec3f(0.299, 0.587, 0.114));
+
   let normColor = clamp((color - dmin) / (dmax - dmin), vec3f(0.0), vec3f(1.0));
-  let result = textureSampleLevel(lutTexture, lutSampler, normColor, 0.0).rgb;
+  var lutColor = textureSampleLevel(lutTexture, lutSampler, normColor, 0.0).rgb;
+  if (lut.enabled == 0) {
+    lutColor = color;
+  }
 
-  // Convert linear RGB to sRGB
-  let srgbColor = pow(result, vec3f(1.0 / 2.2));
+  let normalizedLutColor = clamp(lutColor, vec3f(0.0), vec3f(1.0));
 
-  return vec4f(srgbColor, 1.0);
+  let exposureColor = normalizedLutColor * pow(2.0, adjustments.exposure);
+  let exposureLuminance = inputLuminance * pow(2.0, adjustments.exposure);
+  let clampedExposureColor = clamp(exposureColor, vec3f(0.0), vec3f(2.0));
+  let clampedExposureLuminance = clamp(exposureLuminance, 0.0, 2.0);
+
+  let contrastColor = (clampedExposureColor - vec3f(0.5)) * adjustments.contrast + vec3f(0.5);
+  let contrastLuminance = (clampedExposureLuminance - 0.5) * adjustments.contrast + 0.5;
+
+  let luminance = dot(contrastColor, vec3f(0.299, 0.587, 0.114));
+
+  let highlightWeight = smoothstep(0.5, 1.0, luminance);
+
+  let adjustment = adjustments.highlights - 1.0;
+  let scaledAdjustment = select(adjustment * 0.25, adjustment, adjustments.highlights >= 1.0);
+  let highlightFactor = 1.0 + scaledAdjustment * 0.5 * luminance;
+
+  // let highlightFactor = 1.0 + (adjustments.highlights - 1.0) * 0.5 * (luminance);
+  let highlightAdjust = contrastColor * highlightFactor;
+  let highlightColor = mix(contrastColor, clamp(highlightAdjust, vec3f(0.0), vec3f(1.0)), highlightWeight);
+  let highlightLuminanceAdjust = contrastLuminance * highlightFactor;
+  let highlightLuminance = mix(contrastLuminance, clamp(highlightLuminanceAdjust, 0.0, 1.0), highlightWeight);
+
+  let shadowWeight = 1.0 - smoothstep(0.0, 0.3, luminance);
+  let shadowAdjust = pow(highlightColor, vec3f(1.0 / adjustments.shadows));
+  let toneColor = mix(highlightColor, shadowAdjust, shadowWeight);
+  let shadowLuminanceAdjust = pow(highlightLuminance, 1.0 / adjustments.shadows);
+  let toneLuminance = mix(highlightLuminance, shadowLuminanceAdjust, shadowWeight);
+
+  let finalToneColor = clamp(toneColor, vec3f(0.0), vec3f(1.0));
+
+  let grayscaleColor = vec3f(toneLuminance);
+  let finalColor = mix(grayscaleColor, finalToneColor, adjustments.saturation);
+
+  return vec4f(finalColor, 1.0);
 }
 `;
 
@@ -87,6 +123,7 @@ context.configure({
 
 
 const lutDescBuffer = root.createBuffer(LUTDesc).$usage('uniform');
+const adjustmentsBuffer = root.createBuffer(Adjustments).$usage('uniform');
 
 //////////////////////////
 
@@ -96,7 +133,8 @@ async function render() {
 const uniformLayout = tgpu.bindGroupLayout({
   lutTexture: { texture: 'float', dimension: '3d', viewDimension: '3d', sampleType: 'float' },
   lutSampler: { sampler: 'filtering' },
-  lut: { uniform: LUTDesc }
+  lut: { uniform: LUTDesc },
+  adjustments: { uniform: Adjustments },
 });
 
 const renderLayout = tgpu.bindGroupLayout({
@@ -109,7 +147,6 @@ const VertexOutput = d.struct({
   position: d.builtin.position,
   uv: d.location(0, d.vec2f),
 });
-
 
 // Load input image
 const response = await fetch('/TypeGPU/Lenna.png');
@@ -128,16 +165,6 @@ device.queue.copyExternalImageToTexture(
   { texture: root.unwrap(imageTexture) },
   [srcWidth, srcHeight],
 );
-
-// Create 2D texture array for LUT
-//  = root['~unstable']
-//   .createTexture({
-//     size: [lut!.size, lut!.size, lut!.size],
-//     format: 'rgba16float',
-//     dimension: '3d',
-//   })
-//   .$usage('sampled');
-
 
 
 // Create samplers
@@ -159,6 +186,7 @@ const uniformBindGroup = root.createBindGroup(uniformLayout, {
   lutTexture: root.unwrap(lutTexture!).createView({ dimension: '3d' }),
   lutSampler: lutSampler,
   lut: lutDescBuffer,
+  adjustments: adjustmentsBuffer,
 });
 
 const renderBindGroup = root.createBindGroup(renderLayout, {
@@ -200,7 +228,6 @@ const renderPassDescriptor: GPURenderPassDescriptor = {
 };
 
 
-
   (renderPassDescriptor.colorAttachments as [GPURenderPassColorAttachment])[0].view =
     context.getCurrentTexture().createView();
 
@@ -215,7 +242,17 @@ const renderPassDescriptor: GPURenderPassDescriptor = {
   device.queue.submit([encoder.finish()]);
 }
 
-// render();
+  const defaultLUT = root['~unstable']
+    .createTexture({
+      size: [1, 1, 1],
+      format: 'rgba16float',
+      dimension: '3d',
+    })
+    .$usage('sampled');
+
+lutTexture = defaultLUT;
+lutDescBuffer.writePartial({enabled: 0})
+render();
 
 
 // Cleanup
@@ -233,7 +270,8 @@ function parse(str: string): ParsedLUT {
   let type: string = '';
   let size = 0;
   const domain = [[0.0, 0.0, 0.0], [1.0, 1.0, 1.0]];
-  const data: number[][] = [];
+  let data: Float16Array | undefined = undefined;
+  let index: number = 0;
 
   const lines = str.split('\n');
 
@@ -259,13 +297,22 @@ function parse(str: string): ParsedLUT {
       case 'LUT_1D_SIZE':
         type = '1D';
         size = Number(parts[1]);
+        data = new Float16Array(size * 4);
         break;
       case 'LUT_3D_SIZE':
         type = '3D';
         size = Number(parts[1]);
+        data = new Float16Array(size * size * size * 4);
         break;
       default:
-        data.push(parts.map(Number));
+        if (data !== undefined) {
+          const dataOffset = index * 4;
+          data[dataOffset + 0] = Number(parts[0]); // R
+          data[dataOffset + 1] = Number(parts[1]); // G
+          data[dataOffset + 2] = Number(parts[2]); // B
+          data[dataOffset + 3] = 1.0; // A
+          index++; // Increment index for the next entry
+        }        
     }
   }
 
@@ -279,88 +326,152 @@ function parse(str: string): ParsedLUT {
 }
 
 
-async function fetchLUT(file: string) {
-  // Load and parse LUT data
+async function fetchLUT(file: string): Promise<ParsedLUT> {
   const lutResponse = await fetch(file);
   const lutData = await lutResponse.text();
 
-  lut = parse(lutData);
+  let lut = parse(lutData);
   if (lut.type !== '3D') {
     throw new Error('This example only supports 3D LUTs');
   }
 
-  if (lut !== undefined)
-  {
-    // Convert LUT data to a flat Float32Array for 2D texture array
-    const flatLutData = new Float16Array(lut.size * lut.size * lut.size * 4);
-    let index = 0;
-    for (let r = 0; r < lut.size; r++) {
-      for (let g = 0; g < lut.size; g++) {
-        for (let b = 0; b < lut.size; b++) {
-          const dataIndex = b + g * lut.size + r * lut.size * lut.size;
-
-          flatLutData[index++] = lut.data[dataIndex][0]; // R
-          flatLutData[index++] = lut.data[dataIndex][1]; // G
-          flatLutData[index++] = lut.data[dataIndex][2]; // B
-          flatLutData[index++] = 1.0; // A
-        }
-      }
-    }
-
-    lutDescBuffer.write({
-      lutSize: d.f32(lut!.size),
-      lutMin: d.vec3f(lut!.domain[0][0], lut!.domain[0][1], lut!.domain[0][2]),
-      lutMax: d.vec3f(lut!.domain[1][0], lut!.domain[1][1], lut!.domain[1][2]),
-    });
-
-    lutTexture = root['~unstable']
-      .createTexture({
-        size: [lut.size, lut.size, lut.size],
-        format: 'rgba16float',
-        dimension: '3d',
-      })
-      .$usage('sampled');
-
-    device.queue.writeTexture(
-      { texture: root.unwrap(lutTexture) },
-      flatLutData,
-      {
-        bytesPerRow: lut.size * 4 * 2, // 4 channels, 2 bytes per float
-        rowsPerImage: lut.size,
-      },
-      [lut.size, lut.size, lut.size]
-    );
-  }
+  return lut;
 }
 
 
 
 // #endregion
 
+// #region Example controls and cleanup
 
 const LUTFiles = {
-  "Classic Chrome": '/TypeGPU/assets/cube-luts/classic-chrome.cube',
-  "HollywoodBlue Day": '/TypeGPU/assets/cube-luts/HollywoodBlue_Day.cube',
-  "Dramatic_BlockBuster_33": '/TypeGPU/assets/cube-luts/Dramatic_BlockBuster_33.cube',
-  "pro neg hi_srgb.cube": "/TypeGPU/assets/cube-luts/pro neg hi_srgb.cube",
-  "tinyglade-Cold_Ice.cube": "/TypeGPU/assets/cube-luts/tinyglade-Cold_Ice.cube",
-  "tinyglade-Bluecine_75.cube": "/TypeGPU/assets/cube-luts/tinyglade-Bluecine_75.cube",
-  "tinyglade-Sam_Kolder.cube": "/TypeGPU/assets/cube-luts/tinyglade-Sam_Kolder.cube",
-  "resolve-LMT ACES v0.1.1.cube": "/TypeGPU/assets/cube-luts/resolve-LMT ACES v0.1.1.cube",
+  "None": '',
+  "Chrome": '/TypeGPU/assets/cube-luts/classic-chrome.cube',
+  "Hollywood": '/TypeGPU/assets/cube-luts/HollywoodBlue_Day.cube',
+  "Dramatic": '/TypeGPU/assets/cube-luts/Dramatic_BlockBuster_33.cube',
+  "Pro Neg": "/TypeGPU/assets/cube-luts/pro neg hi_srgb.cube",
+  "Cold Ice": "/TypeGPU/assets/cube-luts/tinyglade-Cold_Ice.cube",
+  "Bluecine": "/TypeGPU/assets/cube-luts/tinyglade-Bluecine_75.cube",
+  "Sam Kolder": "/TypeGPU/assets/cube-luts/tinyglade-Sam_Kolder.cube",
+  "LMT ACES": "/TypeGPU/assets/cube-luts/resolve-LMT ACES v0.1.1.cube",
 };
 
-const selections = {
-  lut: LUTFiles['Classic Chrome']
+const settings = {
+  exposure: 0.0,
+  contrast: 1.0,
+  highlights: 1.0,
+  shadows: 1.0,
+  saturation: 1.0,
 }
 
 export const controls = {
-  'LUT': {
+  'color lookup': {
     options: Object.keys(LUTFiles),
     onSelectChange: async (selected: keyof typeof LUTFiles) => {
-      selections.lut = LUTFiles[selected];
-      await fetchLUT(LUTFiles[selected]);
-debugger;
+      if (selected == "None") {
+        lutTexture = defaultLUT;
+        lutDescBuffer.writePartial({enabled: 0})
+      } else {
+        let parsed = await fetchLUT(LUTFiles[selected]);
+        debugger;
+
+        if (parsed.data === undefined) return;
+
+        lutDescBuffer.write({
+          lutSize: d.f32(parsed.size),
+          lutMin: d.vec3f(parsed.domain[0][0], parsed.domain[0][1], parsed.domain[0][2]),
+          lutMax: d.vec3f(parsed.domain[1][0], parsed.domain[1][1], parsed.domain[1][2]),
+          enabled: 1,
+        });
+
+        lutTexture = root['~unstable']
+          .createTexture({
+            size: [parsed.size, parsed.size, parsed.size],
+            format: 'rgba16float',
+            dimension: '3d',
+          })
+          .$usage('sampled');
+
+        device.queue.writeTexture(
+          { texture: root.unwrap(lutTexture) },
+          parsed.data,
+          {
+            bytesPerRow: parsed.size * 4 * 2,
+            rowsPerImage: parsed.size,
+          },
+          [parsed.size, parsed.size, parsed.size]
+        );
+      }
       await render();
+
+    },
+  },
+
+  'exposure': {
+    initial: 0.0,
+    min: -2.0,
+    max: 2.0,
+    step: 0.1,
+    onSliderChange(newValue: number) {
+      settings.exposure = newValue;
+      adjustmentsBuffer.writePartial({
+        exposure: newValue * 0.25,
+      });
+      render();
+    },
+  },
+  'contrast': {
+    initial: 1.0,
+    min: 0.0,
+    max: 2.0,
+    step: 0.01,
+    onSliderChange(newValue: number) {
+      settings.contrast = newValue;
+      adjustmentsBuffer.writePartial({
+        contrast: newValue,
+      });
+      render();
+    },
+  },
+  'highlights': {
+    initial: 1.0,
+    min: 0.0,
+    max: 2.0,
+    step: 0.01,
+    onSliderChange(newValue: number) {
+      settings.highlights = newValue;
+      adjustmentsBuffer.writePartial({
+        highlights: newValue,
+      });
+      render();
+    },
+  },
+  'shadows': {
+    initial: 1.0,
+    min: 0.1,
+    max: 1.9,
+    step: 0.01,
+    onSliderChange(newValue: number) {
+      settings.shadows = newValue;
+      adjustmentsBuffer.writePartial({
+        shadows: newValue,
+      });
+      render();
+    },
+  },
+  'saturation': {
+    initial: 1.0,
+    min: 0.0,
+    max: 2.0,
+    step: 0.01,
+    onSliderChange(newValue: number) {
+      settings.saturation = newValue;
+      adjustmentsBuffer.writePartial({
+        saturation: newValue,
+      });
+      render();
     },
   },
 };
+
+// #endregion
