3c0e4e3bca311abbbbf1a58970cdb3b0468dd57f
lursz <lukoszr@gmail.com>
2025-05-28 11:55:42 +0200

    tgpu resolve test

--- packages/typegpu/tests/std/numeric/translate4x4.test.ts
+++ packages/typegpu/tests/std/numeric/translate4x4.test.ts
@@ -1,7 +1,8 @@
 import { describe, expect, it } from 'vitest';
 import { translate4x4 } from '../../../src/std/index.ts';
 import { mat4x4f, vec3f } from '../../../src/data/index.ts';
-import { parseResolved } from '../../utils/parseResolved.ts';
+import { parse, parseResolved } from '../../utils/parseResolved.ts';
+import tgpu from '../../../src/index.ts';
 
 describe('translate', () => {
   it('translates a matrix by a vec3f vector', () => {
@@ -22,19 +23,21 @@ describe('translate', () => {
     );
   });
 
-  it('generates correct WGSL for translate4x4 with identity matrix', () => {
-    const M = mat4x4f(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
-    const T = vec3f(1, 2, 3);
-    const resultExpression = translate4x4(M, T);
-    const wgsl = parseResolved({ resultExpression });
-    expect(wgsl).toMatchSnapshot();
-  });
-
   it('generates correct WGSL for translate4x4 with custom matrix', () => {
     const M = mat4x4f(1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1);
     const T = vec3f(2, 2, 4);
-    const resultExpression = translate4x4(M, T);
-    const wgsl = parseResolved({ resultExpression });
-    expect(wgsl).toMatchSnapshot();
+
+    const wgsl = tgpu['~unstable']
+      .fn([])(() => {
+        const resultExpression = translate4x4(M, T);
+      }).$name('translate4x4');
+
+    expect(parseResolved({ wgsl })).toBe(
+      parse(
+        `fn translate4x4 () { 
+          var resultExpression = mat4x4f ( 1 , 0 , 0 , 1 , 0 , 1 , 0 , 0 , 1 , 0 , 1 , 0 , 0 , 1 , 0 , 1 ) * mat4x4 < f32 > ( 1 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 1 , 0 , vec3f ( 2 , 2 , 4 ) . x , vec3f ( 2 , 2 , 4 ) . y , vec3f ( 2 , 2 , 4 ) . z , 1 ) ; 
+        }`,
+      ),
+    );
   });
 });
