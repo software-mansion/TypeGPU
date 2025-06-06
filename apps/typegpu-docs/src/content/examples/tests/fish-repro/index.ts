97474848f1b95c3c9ae45aa4cc7306aa6faa4958
Aleksander Katan <aleksander.katan@swmansion.com>
2025-06-06 11:34:48 +0200

    Clean up repro

--- apps/typegpu-docs/src/content/examples/tests/fish-repro/index.ts
+++ apps/typegpu-docs/src/content/examples/tests/fish-repro/index.ts
@@ -1,15 +1,13 @@
-import tgpu from 'typegpu';
-const t = tgpu;
+import fs from 'fs';
+const f = fs;
+
+// setup
 
 const adapter = await navigator.gpu?.requestAdapter();
 const device = await adapter?.requestDevice();
 const copyModule = device!.createShaderModule({
   label: 'copying compute module',
   code: `
-    struct Input {
-      @builtin(global_invocation_id) gid: vec3u,
-    }
-
     struct Item {
       vec: vec3f,
       num: u32,
@@ -18,14 +16,9 @@ const copyModule = device!.createShaderModule({
     @group(0) @binding(0) var<storage, read> sourceBuffer: array<Item>;
     @group(0) @binding(1) var<storage, read_write> targetBuffer: array<Item>;
 
-    @compute @workgroup_size(1) fn computeShader_0(input: Input){
-      var index = input.gid.x;
-//      var itemData1: vec3f = sourceBuffer[index].vec;
-//      targetBuffer[index].vec = itemData1;
-//      var itemData2: u32 = sourceBuffer[index].num;
-//      targetBuffer[index].num = itemData2;
-      var item = sourceBuffer[index];
-      targetBuffer[index] = item;
+    @compute @workgroup_size(1) fn computeShader_0(@builtin(global_invocation_id) gid: vec3u){
+      var item = sourceBuffer[gid.x];
+      targetBuffer[gid.x] = item;
     }
     `,
 });
@@ -38,32 +31,27 @@ const pipeline = device!.createComputePipeline({
   },
 });
 
+// work buffer 1
 const sourceBuffer = device!.createBuffer({
   label: 'source buffer',
-  size: 48,
+  size: 32,
   usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC |
     GPUBufferUsage.COPY_DST,
 });
-const input = new DataView(new ArrayBuffer(48));
-input.setFloat32(0, 1);
-input.setFloat32(4, 3);
-input.setFloat32(8, 5);
-input.setUint32(12, 7);
-input.setFloat32(16 + 0, 100);
-input.setFloat32(16 + 4, 300);
-input.setFloat32(16 + 8, 500);
-input.setUint32(16 + 12, 700);
-input.setFloat32(32 + 0, 10000);
-input.setFloat32(32 + 4, 30000);
-input.setFloat32(32 + 8, 50000);
-input.setUint32(32 + 12, 70000);
-device!.queue.writeBuffer(sourceBuffer, 0, input);
 
+// work buffer 2
 const targetBuffer = device!.createBuffer({
-  label: 'source buffer',
-  size: 48,
+  label: 'target buffer',
+  size: 32,
   usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC |
     GPUBufferUsage.COPY_DST,
+});
+
+// buffer for reading the results
+const resultBuffer = device!.createBuffer({
+  label: 'result buffer',
+  size: 32,
+  usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
 });
 
 const bindGroup = device!.createBindGroup({
@@ -75,7 +63,19 @@ const bindGroup = device!.createBindGroup({
   ],
 });
 
-// Encode commands to do the computation
+//
+
+const input = new DataView(new ArrayBuffer(32));
+input.setFloat32(0, 1);
+input.setFloat32(4, 3);
+input.setFloat32(8, 5);
+input.setUint32(12, 7);
+input.setFloat32(16, 100);
+input.setFloat32(20, 300);
+input.setFloat32(24, 500);
+input.setUint32(28, 700);
+device!.queue.writeBuffer(sourceBuffer, 0, input);
+
 const encoder = device!.createCommandEncoder({
   label: 'copying encoder',
 });
@@ -84,20 +84,12 @@ const pass = encoder.beginComputePass({
 });
 pass.setPipeline(pipeline);
 pass.setBindGroup(0, bindGroup);
-pass.dispatchWorkgroups(3);
+pass.dispatchWorkgroups(2);
 pass.end();
 
-const resultBuffer = device!.createBuffer({
-  label: 'result buffer',
-  size: 48,
-  usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
-});
+encoder.copyBufferToBuffer(targetBuffer, 0, resultBuffer, 0, 32);
+device!.queue.submit([encoder.finish()]);
 
-encoder.copyBufferToBuffer(targetBuffer, 0, resultBuffer, 0, 48);
-const commandBuffer = encoder.finish();
-device!.queue.submit([commandBuffer]);
-
-// Read the results
 await resultBuffer.mapAsync(GPUMapMode.READ);
 const result = new DataView(resultBuffer.getMappedRange().slice());
 resultBuffer.unmap();
