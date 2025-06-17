046addea25fe0f2140e9a81c43a3c8f75437d3d7
Iwo Plaza <iwoplaza@gmail.com>
2025-06-17 15:38:16 +0200

    A bit more intense rays

--- apps/typegpu-docs/src/content/examples/rendering/caustics/index.ts
+++ apps/typegpu-docs/src/content/examples/rendering/caustics/index.ts
@@ -7,11 +7,11 @@ const mainVertex = tgpu['~unstable'].vertexFn({
   in: { vertexIndex: d.builtin.vertexIndex },
   out: { pos: d.builtin.position, uv: d.vec2f },
 })(({ vertexIndex }) => {
-  const pos = [d.vec2f(0.0, 0.8), d.vec2f(-0.8, -0.8), d.vec2f(0.8, -0.8)];
-  const uv = [d.vec2f(0.5, 1.0), d.vec2f(0.0, 0.0), d.vec2f(1.0, 0.0)];
+  const pos = [d.vec2f(0, 0.8), d.vec2f(-0.8, -0.8), d.vec2f(0.8, -0.8)];
+  const uv = [d.vec2f(0.5, 1), d.vec2f(0, 0), d.vec2f(1, 0)];
 
   return {
-    pos: d.vec4f(pos[vertexIndex], 0.0, 1.0),
+    pos: d.vec4f(pos[vertexIndex], 0, 1),
     uv: uv[vertexIndex],
   };
 });
@@ -119,7 +119,7 @@ const mainFragment = tgpu['~unstable'].fragmentFn({
   // -- GOD RAYS --
 
   const godRayUv = std.mul(std.mul(rotateXY(-0.3), uv), d.vec2f(15, 3));
-  const godRayFactor = std.pow(uv.y, 2);
+  const godRayFactor = std.pow(uv.y, 1);
   const godRay1 = std.mul(
     std.add(perlin3d.sample(d.vec3f(godRayUv, time * 0.5)), 1),
     // Tinting
