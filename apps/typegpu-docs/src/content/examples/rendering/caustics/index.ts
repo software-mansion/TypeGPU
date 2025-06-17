0dd1c56ab049a559145b1e8e7c1e9f72f32f9a77
Iwo Plaza <iwoplaza@gmail.com>
2025-06-17 14:32:02 +0200

    God rays

--- apps/typegpu-docs/src/content/examples/rendering/caustics/index.ts
+++ apps/typegpu-docs/src/content/examples/rendering/caustics/index.ts
@@ -3,20 +3,6 @@ import * as d from 'typegpu/data';
 import * as std from 'typegpu/std';
 import { perlin3d } from '@typegpu/noise';
 
-const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
-const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-const context = canvas.getContext('webgpu') as GPUCanvasContext;
-
-const root = await tgpu.init();
-
-context.configure({
-  device: root.device,
-  format: presentationFormat,
-  alphaMode: 'premultiplied',
-});
-
-const timeUniform = root['~unstable'].createUniform(d.f32);
-
 const mainVertex = tgpu['~unstable'].vertexFn({
   in: { vertexIndex: d.builtin.vertexIndex },
   out: { pos: d.builtin.position, uv: d.vec2f },
@@ -51,12 +37,33 @@ const caustics = tgpu['~unstable'].fn([d.vec2f, d.f32, d.vec3f], d.vec3f)(
   },
 );
 
+const clamp01 = tgpu['~unstable'].fn([d.f32], d.f32)((v) => {
+  return std.clamp(v, 0, 1);
+});
+
+/**
+ * Returns a transformation matrix that represents an `angle` rotation
+ * in the XY plane (around the imaginary Z axis)
+ */
+const rotateXY = tgpu['~unstable'].fn([d.f32], d.mat2x2f)((angle) => {
+  return d.mat2x2f(
+    /* right */ d.vec2f(std.cos(angle), std.sin(angle)),
+    /* up    */ d.vec2f(-std.sin(angle), std.cos(angle)),
+  );
+});
+
+const root = await tgpu.init();
+
+const timeUniform = root['~unstable'].createUniform(d.f32);
+
 /** Controls the angle of rotation for the pool tile texture */
 const angle = 0.2;
 /** The bigger the number, the denser the pool tile texture is */
 const tileDensityUniform = root['~unstable'].createUniform(d.f32);
 /** The scene fades into this color at a distance */
 const fogColor = d.vec3f(0.05, 0.2, 0.7);
+/** The ambient light color */
+const ambientColor = d.vec3f(0.2, 0.5, 1);
 
 const mainFragment = tgpu['~unstable'].fragmentFn({
   in: { uv: d.vec2f },
@@ -86,21 +93,52 @@ const mainFragment = tgpu['~unstable'].fragmentFn({
   const c1 = std.mul(
     caustics(cuv, time * 0.2, /* profile */ d.vec3f(4, 4, 1)),
     // Tinting
-    d.vec3f(0.3, 0.5, 1),
+    d.vec3f(0.4, 0.65, 1),
   );
   const c2 = std.mul(
     caustics(std.mul(cuv, 2), time * 0.4, /* profile */ d.vec3f(16, 1, 4)),
     // Tinting
-    d.vec3f(0.18, 0.3, 0.3),
+    d.vec3f(0.18, 0.3, 0.5),
   );
-  // A smooth blending factor, so that caustics only appear at certain spots
-  const blend = std.clamp(perlin3d.sample(d.vec3f(std.mul(uv, d.vec2f(5, 10)), time * 0.2 + 5)) + 0.3, 0, 1);
 
-  const noFogColor = std.mix(std.mul(albedo, fogColor), d.vec3f(std.mul(albedo, std.add(c1, c2))), blend);
+  // -- BLEND --
+
+  const blendCoord = d.vec3f(std.mul(uv, d.vec2f(5, 10)), time * 0.2 + 5);
+  // A smooth blending factor, so that caustics only appear at certain spots
+  const blend = clamp01(perlin3d.sample(blendCoord) + 0.3);
+
+  // -- FOG --
+
+  const noFogColor = std.mul(albedo, std.mix(ambientColor, std.add(c1, c2), blend));
   // Fog blending factor, based on the height of the pixels
   const fog = std.min(std.pow(uv.y, 0.5) * 1.2, 1);
 
-  return d.vec4f(std.mix(noFogColor, fogColor, fog), 1);
+  // -- GOD RAYS --
+
+  const godRayUv = std.mul(std.mul(rotateXY(-0.3), uv), d.vec2f(15, 3));
+  const godRayFactor = std.pow(uv.y, 2);
+  const godRay = std.mul(
+    std.add(perlin3d.sample(d.vec3f(godRayUv, time * 0.5)), 1),
+    // Tinting
+    std.mul(d.vec3f(0.18, 0.3, 0.5), godRayFactor),
+  );
+  const godRay2 = std.mul(
+    std.add(perlin3d.sample(d.vec3f(std.mul(godRayUv, 2), time * 0.3)), 1),
+    // Tinting
+    std.mul(d.vec3f(0.18, 0.3, 0.5), godRayFactor * 0.4),
+  );
+
+  return d.vec4f(std.add(std.mix(noFogColor, fogColor, fog), std.add(godRay, godRay2)), 1);
+});
+
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const context = canvas.getContext('webgpu') as GPUCanvasContext;
+
+context.configure({
+  device: root.device,
+  format: presentationFormat,
+  alphaMode: 'premultiplied',
 });
 
 const pipeline = root['~unstable']
