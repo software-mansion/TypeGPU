02fcfde88016ab9cc7a674297e526d4ac12b8ee1
Iwo Plaza <iwoplaza@gmail.com>
2025-06-17 12:31:35 +0200

    Cleanup

--- apps/typegpu-docs/src/content/examples/rendering/caustics/index.ts
+++ apps/typegpu-docs/src/content/examples/rendering/caustics/index.ts
@@ -114,7 +114,6 @@ function draw() {
   pipeline
     .withColorAttachment({
       view: context.getCurrentTexture().createView(),
-      clearValue: [0, 0, 0, 0],
       loadOp: 'clear',
       storeOp: 'store',
     })
