67408cf6219647dbd23b88774ebb42a4b5246a68
Iwo Plaza <iwoplaza@gmail.com>
2025-06-19 20:41:56 +0200

    chore: Simplify tsconfig.json

---
+++ packages/typegpu/src/shared/dev.ts
@@ -0,0 +1,8 @@
+/**
+ * Even though the value of this constant uses Node.js specific APIs, pretty much every
+ * bundler replaces the expression below with either `development` or `production`
+ *
+ * This can be used to branch functionality between "dev" and "prod" modes, so that our
+ * library can omit doing unnecessary work once it's out in the wild
+ */
+export const DEV = (globalThis as any).process.env.NODE_ENV === 'development';
