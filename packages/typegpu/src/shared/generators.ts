28d00bdb4f5d0a17a8ea9c7b032c8cfebb067eff
Aleksander Katan <aleksander.katan@swmansion.com>
2025-06-27 12:37:21 +0200

    Add a basic clone implementation

--- packages/typegpu/src/shared/generators.ts
+++ packages/typegpu/src/shared/generators.ts
@@ -1,8 +1,8 @@
 import type { Snippet, TgpuDualFn } from '../data/dataTypes.ts';
 import { inGPUMode } from '../gpuMode.ts';
 import type { FnArgsConversionHint } from '../types.ts';
-import { $internal } from './symbols.ts';
 import { setName } from './meta.ts';
+import { $internal } from './symbols.ts';
 
 /**
  * Yields values in the sequence 0,1,2..∞ except for the ones in the `excluded` set.
@@ -21,6 +21,95 @@ export function* naturalsExcept(
   }
 }
 
+// nie ma symbolów, getterów, itp.
+// function cloneWgslData<T>(item: T): T {
+//   return structuredClone(item);
+// }
+
+// nie ma symbolów, getterów, itp.
+// function cloneWgslData<T>(item: T): T {
+//   if (item === null || typeof item !== 'object') {
+//     return item;
+//   }
+
+//   return clone(item); // just-clone
+// }
+
+// wszystko jest tylko wektor staje się obiektem zamiast arraya, nie jest rekurencyjne
+// function cloneWgslData<T>(item: T): T {
+//   return Object.create(
+//     Object.getPrototypeOf(item),
+//     Object.getOwnPropertyDescriptors(item),
+//   );
+// }
+
+// wszystko jest tylko wektor staje się obiektem zamiast arraya
+// function cloneWgslData<T>(item: T): T {
+//   if (item === null || typeof item !== 'object') {
+//     return item;
+//   }
+
+//   const copy = Object.create(Object.getPrototypeOf(item)) as any;
+
+//   for (
+//     const key of [
+//       ...Object.getOwnPropertyNames(item),
+//       ...Object.getOwnPropertySymbols(item),
+//     ]
+//   ) {
+//     const desc = Object.getOwnPropertyDescriptor(item, key)!;
+//     if (desc.get || desc.set) {
+//       Object.defineProperty(copy, key, desc);
+//     } else {
+//       copy[key] = cloneWgslData((item as any)[key]);
+//     }
+//   }
+
+//   return copy;
+// }
+
+function cloneWgslData<T>(item: T): T {
+  if (item === null || typeof item !== 'object') {
+    return item;
+  }
+
+  if (Array.isArray(item)) {
+    const arr = new (item.constructor as any)(
+      ...item.map((v) => cloneWgslData(v)),
+    );
+
+    for (const key of Reflect.ownKeys(item) as (string | symbol)[]) {
+      if (typeof key === 'string' && /^\d+$/.test(key)) continue;
+      if (key === 'length') continue;
+
+      const desc = Object.getOwnPropertyDescriptor(item, key)!;
+      if ('value' in desc) {
+        desc.value = cloneWgslData((item as any)[key]);
+      }
+      Object.defineProperty(arr, key, desc);
+    }
+    return arr as any;
+  }
+
+  const copy = Object.create(Object.getPrototypeOf(item)) as any;
+
+  for (
+    const key of [
+      ...Object.getOwnPropertyNames(item),
+      ...Object.getOwnPropertySymbols(item),
+    ]
+  ) {
+    const desc = Object.getOwnPropertyDescriptor(item, key)!;
+    if (desc.get || desc.set) {
+      Object.defineProperty(copy, key, desc);
+    } else {
+      copy[key] = cloneWgslData((item as any)[key]);
+    }
+  }
+
+  return copy;
+}
+
 type MapValueToSnippet<T> = { [K in keyof T]: Snippet };
 
 export function createDualImpl<T extends (...args: never[]) => unknown>(
@@ -33,7 +122,7 @@ export function createDualImpl<T extends (...args: never[]) => unknown>(
     if (inGPUMode()) {
       return gpuImpl(...(args as MapValueToSnippet<Parameters<T>>)) as Snippet;
     }
-    return jsImpl(...args);
+    return jsImpl(...args.map((arg) => cloneWgslData(arg)));
   }) as T;
 
   (impl as TgpuDualFn<T>)[$internal] = {
--- packages/typegpu/tests/tgsl/createDualImpl.test.ts
+++ packages/typegpu/tests/tgsl/createDualImpl.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+import * as d from '../../src/data/index.ts';
 import { createDualImpl } from '../../src/shared/generators.ts';
 import { getName } from '../../src/shared/meta.ts';
 
@@ -10,5 +11,56 @@ describe('createDualImpl', () => {
       'myDualImpl',
     );
     expect(getName(dual)).toBe('myDualImpl');
+  });
+
+  it('returns a clone of its argument', () => {
+    const dual = createDualImpl<(a: d.v3f) => d.v3f>(
+      (a) => a,
+      (snippet) => snippet,
+      'myClone',
+    );
+    const vec = d.vec3f(1, 2, 3);
+
+    const clonedVec = dual(vec);
+
+    // console.dir(vec, { showHidden: true, depth: null });
+    // console.dir(clonedVec, { showHidden: true, depth: null });
+
+    expect(clonedVec).toStrictEqual(vec);
+    expect(clonedVec).not.toBe(vec);
+  });
+
+  it('returns a deep clone of its argument', () => {
+    const dual = createDualImpl<(a: { a: d.v2f }) => { a: d.v2f }>(
+      (a) => a,
+      (snippet) => snippet,
+      'myClone',
+    );
+    const item = d.struct({ a: d.vec2f })({ a: d.vec2f(1, 2) });
+
+    const clonedItem = dual(item);
+
+    // console.dir(item, { showHidden: true, depth: null });
+    // console.dir(clonedItem, { showHidden: true, depth: null });
+
+    expect(clonedItem).toStrictEqual(item);
+    expect(clonedItem).not.toBe(item);
+  });
+
+  it('does not modify its argument', () => {
+    const dual = createDualImpl<(a: d.v3f) => d.v3f>(
+      (a) => {
+        a.x = 4;
+        return a;
+      },
+      (snippet) => snippet,
+      'myModify',
+    );
+    const vec = d.vec3f(1, 2, 3);
+
+    const clonedVec = dual(vec);
+
+    expect(vec).toStrictEqual(d.vec3f(1, 2, 3));
+    expect(clonedVec).toStrictEqual(d.vec3f(4, 2, 3));
   });
 });
