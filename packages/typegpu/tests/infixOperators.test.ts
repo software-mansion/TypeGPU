28f70ac1f82a93e7ea761f1f7c8d2a53f15d9d47
Aleksander Katan <aleksander.katan@swmansion.com>
2025-07-15 16:36:18 +0200

    Add JS tests

--- packages/typegpu/tests/infixOperators.test.ts
+++ packages/typegpu/tests/infixOperators.test.ts
@@ -3,6 +3,46 @@ import * as d from '../src/data/index.ts';
 import { isCloseTo } from '../src/std/boolean.ts';
 
 describe('infix operators', () => {
+  it('correctly applies add to vectors', () => {
+    // vector + scalar
+    expect(isCloseTo(d.vec2f(1, 2).add(2), d.vec2f(3, 4))).toBe(true);
+
+    // vector + vector
+    expect(d.vec3i(1, 2, 3).add(d.vec3i(-1, -2, -3))).toStrictEqual(d.vec3i());
+  });
+
+  it('correctly applies add to matrices', () => {
+    // matrix + matrix
+    expect(
+      d.mat3x3f(10, 10, 10, 10, 10, 10, 10, 10, 10).add(
+        d.mat3x3f(1, 2, 3, 4, 5, 6, 7, 8, 9),
+      ),
+    ).toStrictEqual(
+      d.mat3x3f(11, 12, 13, 14, 15, 16, 17, 18, 19),
+    );
+  });
+
+  it('correctly applies sub to vectors', () => {
+    // vector - scalar
+    expect(isCloseTo(d.vec2f(1, 2).sub(2), d.vec2f(-1, 0))).toBe(true);
+
+    // vector - vector
+    expect(d.vec3i(1, 2, 3).sub(d.vec3i(-1, -2, -3))).toStrictEqual(
+      d.vec3i(2, 4, 6),
+    );
+  });
+
+  it('correctly applies sub to matrices', () => {
+    // matrix - matrix
+    expect(
+      d.mat3x3f(10, 10, 10, 10, 10, 10, 10, 10, 10).sub(
+        d.mat3x3f(1, 2, 3, 4, 5, 6, 7, 8, 9),
+      ),
+    ).toStrictEqual(
+      d.mat3x3f(9, 8, 7, 6, 5, 4, 3, 2, 1),
+    );
+  });
+
   it('correctly applies mul to vectors', () => {
     // vector * scalar
     expect(isCloseTo(d.vec2f(1, 2).mul(2), d.vec2f(2, 4))).toBe(true);
@@ -23,34 +63,28 @@ describe('infix operators', () => {
 
   it('correctly applies mul to matrices', () => {
     // Matrix * scalar
-    const m = d.mat2x2f(1, 2, 3, 4);
-    console.log(m);
-    console.log(Object.getPrototypeOf(m));
-    console.log(Object.getPrototypeOf(Object.getPrototypeOf(m)));
-
-    expect(
-      d.mat2x2f(1, 2, 3, 4).mul(2),
-    ).toStrictEqual(
-      d.mat2x2f(2, 4, 6, 8),
+    expect(d.mat3x3f(1, 2, 3, 4, 5, 6, 7, 8, 9).mul(2)).toStrictEqual(
+      d.mat3x3f(2, 4, 6, 8, 10, 12, 14, 16, 18),
     );
 
-    // // Matrix * vector
-    // expect(
-    //   d.mat2x2f(1, 2, 3, 4).mul(d.vec2f(5, 6)),
-    // ).toStrictEqual(
-    //   d.vec2f(1 * 5 + 3 * 6, 2 * 5 + 4 * 6),
-    // );
+    // Matrix * vector
+    expect(d.mat2x2f(1, 2, 3, 4).mul(d.vec2f(5, 6))).toStrictEqual(
+      d.vec2f(23, 34),
+    );
 
-    // // Matrix * matrix
-    // expect(
-    //   d.mat2x2f(1, 2, 3, 4).mul(d.mat2x2f(7, 8, 9, 10)),
-    // ).toStrictEqual(
-    //   d.mat2x2f(
-    //     1 * 7 + 3 * 8,
-    //     2 * 7 + 4 * 8,
-    //     1 * 9 + 3 * 10,
-    //     2 * 9 + 4 * 10,
-    //   ),
-    // );
+    // Matrix * matrix
+    expect(d.mat2x2f(1, 2, 3, 4).mul(d.mat2x2f(2, 0, 0, 3))).toStrictEqual(
+      d.mat2x2f(2, 4, 9, 12),
+    );
+  });
+
+  it('correctly applies div to vectors', () => {
+    // vector / scalar
+    expect(isCloseTo(d.vec2f(1, 2).div(2), d.vec2f(0.5, 1))).toBe(true);
+
+    // vector / vector
+    expect(d.vec3i(-1, -4, -9).div(d.vec3i(-1, -2, -3))).toStrictEqual(
+      d.vec3i(1, 2, 3),
+    );
   });
 });
