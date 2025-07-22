31a77dde9e8d28ee59342c157859234f3d59b020
lursz <lukoszr@gmail.com>
2025-07-22 16:22:55 +0200

    no more fixes, I promise

--- apps/typegpu-docs/src/content/examples/simulation/fluid-with-atomics/index.ts
+++ apps/typegpu-docs/src/content/examples/simulation/fluid-with-atomics/index.ts
@@ -209,7 +209,7 @@ const decideWaterLevel = tgpu.fn([d.u32, d.u32])((x, y) => {
     if (flowRaw > 0) {
       const change = std.max(
         std.min(4, remainingWater),
-        d.u32(d.u32(flowRaw) / d.f32(4)),
+        d.u32(flowRaw / 4),
       );
       const flow = std.min(change, viscosity.$);
       subtractFromCell(x, y, flow);
