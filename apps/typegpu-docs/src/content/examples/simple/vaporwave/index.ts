4d24d86e2a4f568c5d46cf2094f92a6854bdf5bb
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2025-07-25 12:05:59 +0200

    Pulsar

--- apps/typegpu-docs/src/content/examples/simple/vaporwave/index.ts
+++ apps/typegpu-docs/src/content/examples/simple/vaporwave/index.ts
@@ -2,12 +2,15 @@ import tgpu from "typegpu";
 import * as d from "typegpu/data";
 import * as std from "typegpu/std";
 import { sdSphere, sdPlane } from "@typegpu/sdf";
+import { perlin3d } from "@typegpu/noise";
 
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const context = canvas.getContext("webgpu") as GPUCanvasContext;
 const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 
-const root = await tgpu.init();
+const root = await tgpu.init({
+  device: { requiredFeatures: ["timestamp-query"] },
+});
 
 context.configure({
   device: root.device,
@@ -24,11 +27,11 @@ const SURF_DIST = 0.001;
 const SPEED_PER_FRAME = 11;
 const GRID_SEP = 1.2;
 const GRID_TIGHTNESS = 7;
-
 const skyColor = d.vec4f(0.1, 0, 0.2, 1);
 const gridColor = d.vec3f(0.92, 0.21, 0.96);
 const gridInnerColor = d.vec3f(0, 0, 0);
 const ballColor = d.vec3f(0, 1, 0.961);
+const ballCenter = d.vec3f(0, 6, 12);
 
 const Ray = d.struct({
   color: d.vec3f,
@@ -54,37 +57,32 @@ const grid = tgpu.fn(
   );
 });
 
-const noise = tgpu.fn(
-  [d.vec3f, d.f32],
-  d.f32,
-)((p, t) => {
-  // some random func from the internet
-  return (
-    std.sin(4.0 * p.x + t + 1) *
-    std.sin(5.0 * p.y + t + 2) *
-    std.sin(5.0 * p.z + t + 3) *
-    3
-  );
-});
-
 const getBall = tgpu.fn(
   [d.vec3f, d.f32],
   Ray,
 )((p, t) => {
-  // Center position
-  const center = d.vec3f(0, 6, 17); // hardcoded
-  const localP = std.sub(p, center); // way from center to p
+  const localP = std.sub(p, ballCenter); // way from center to p
+  const rotMatZ = d.mat4x4f.rotationZ(-t * 0.4);
+  const rotMatX = d.mat4x4f.rotationX(-t * 0.4);
+  const rotatedP = std.mul(rotMatZ, std.mul(rotMatX, d.vec4f(localP, 1))).xyz;
 
-  // okay some periodic pattern let it be
-  const sphere1Offset = d.vec3f(
-    0, // x
-    std.sin(t * 0.7) * 0.4,
-    0, // z
+  const sphereOffset = d.vec3f(
+    std.cos(t) * 2, // x
+    std.sin(t * 0.7) * 0.4, // y motion
+    std.sin(t) * 2, // z
   );
+
+  const rayPoint = std.sub(rotatedP, sphereOffset);
+
+  // breathing effect
+  const radius = 3 + std.sin(t) * 0.33;
+
+  // perlin noise
+  const noise = perlin3d.sample(rayPoint);
 
   // calculate distances and assign colors
   return Ray({
-    dist: sdSphere(std.sub(localP, sphere1Offset), 3), // center is relative to p
+    dist: sdSphere(rayPoint, radius) + noise, // center is relative to p
     color: ballColor,
   });
 });
@@ -93,12 +91,7 @@ const shapeUnion = tgpu.fn(
   [Ray, Ray],
   Ray,
 )((a, b) => ({
-  // hardcoded that ball is light source
-  color: std.mix(
-    std.select(a.color, b.color, a.dist > b.dist),
-    ballColor,
-    2 / std.select(b.dist, 0, b.dist < 0), // IDK WHAT I DID
-  ),
+  color: std.select(a.color, b.color, a.dist > b.dist),
   dist: std.min(a.dist, b.dist),
 }));
 
@@ -108,7 +101,7 @@ const getSceneDist = tgpu.fn(
   Ray,
 )((p) => {
   const floor = Ray({
-    dist: sdPlane(p, d.vec3f(0, 1, 0), 1),
+    dist: sdPlane(p, d.vec3f(0, 1, 0), 1), // hardcoded plane location
     color: grid(p.xz),
   });
   const ball = getBall(p, time.$);
@@ -118,22 +111,26 @@ const getSceneDist = tgpu.fn(
 
 const rayMarch = tgpu.fn(
   [d.vec3f, d.vec3f],
-  Ray,
+  d.struct({ ray: Ray, bloom: d.vec3f }),
 )((ro, rd) => {
   let dO = d.f32(0);
   const result = Ray({
     dist: d.f32(MAX_DIST),
-    color: d.vec3f(0, 1, 0),
+    color: d.vec3f(0, 1, 0), // green for debug
   });
+
+  let bloom = d.vec3f(0, 0, 0);
 
   for (let i = 0; i < MAX_STEPS; i++) {
     const p = std.add(ro, std.mul(rd, dO));
     const scene = getSceneDist(p);
+    const ballDist = getBall(p, time.$);
+    bloom = std.add(bloom, std.mul(ballColor, std.exp(-ballDist.dist)));
     dO += scene.dist;
 
     if (dO > MAX_DIST) {
       result.dist = MAX_DIST;
-      result.color = d.vec3f(0, 1, 0); // green for debug
+      result.color = d.vec3f(0, 1, 0); // also green for debug
       break;
     }
 
@@ -144,7 +141,7 @@ const rayMarch = tgpu.fn(
     }
   }
 
-  return result;
+  return { ray: result, bloom };
 });
 
 const vertexMain = tgpu["~unstable"].vertexFn({
@@ -168,19 +165,29 @@ const fragmentMain = tgpu["~unstable"].fragmentFn({
   uv.x *= resolution.$.x / resolution.$.y;
 
   // ray origin and direction
-  const ro = d.vec3f(0, 2, 3);
+  const ro = d.vec3f(0, 2, -1);
   const rd = std.normalize(d.vec3f(uv.x, uv.y, 1));
 
   // marching
   const march = rayMarch(ro, rd);
 
   // fog calculations
-  const fog = std.min(march.dist / MAX_DIST, 1);
+  const fog = std.min(march.ray.dist / MAX_DIST, 1);
 
-  return std.mix(d.vec4f(march.color, 1), skyColor, fog);
+  return std.mix(
+    d.vec4f(march.bloom, 1),
+    std.mix(d.vec4f(march.ray.color, 1), skyColor, fog),
+    0.87, // this should not be hardcoded
+  );
+});
+
+const perlinCache = perlin3d.staticCache({
+  root: root,
+  size: d.vec3u(64),
 });
 
 const renderPipeline = root["~unstable"]
+  .pipe(perlinCache.inject())
   .withVertex(vertexMain, {})
   .withFragment(fragmentMain, { format: presentationFormat })
   .createPipeline();
