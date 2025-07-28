6fc4aac17c5e567b34703d9d3f6101c0c9a5bb31
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2025-07-28 12:07:30 +0200

    Gradient sky

--- apps/typegpu-docs/src/content/examples/simple/vaporwave/constans.ts
+++ apps/typegpu-docs/src/content/examples/simple/vaporwave/constans.ts
@@ -6,7 +6,8 @@ const SURF_DIST = 0.001;
 const GRID_SEP = 1.2;
 const GRID_TIGHTNESS = 7;
 
-const skyColor = d.vec4f(0.1, 0, 0.2, 1);
+const skyColor1 = d.vec4f(0.1, 0, 0.2, 1);
+const skyColor2 = d.vec4f(0.28, 0, 0.54, 1);
 const gridColor = d.vec3f(0.92, 0.21, 0.96);
 const gridInnerColor = d.vec3f(0, 0, 0);
 const ballCenter = d.vec3f(0, 6, 12);
@@ -17,7 +18,8 @@ export {
   SURF_DIST,
   GRID_SEP,
   GRID_TIGHTNESS,
-  skyColor,
+  skyColor1,
+  skyColor2,
   gridColor,
   gridInnerColor,
   ballCenter,
