2d1d95ae3de0e5dc182b80918698a4b6c8d50432
Rafal Lukosz <93160829+lursz@users.noreply.github.com>
2025-07-28 09:36:42 +0200

    fix: div-operator (#1520)

--- packages/typegpu/src/std/numeric.ts
+++ packages/typegpu/src/std/numeric.ts
@@ -1,5 +1,10 @@
 import { vecTypeToConstructor } from '../data/vector.ts';
-import { type AnyData, snip, type Snippet } from '../data/dataTypes.ts';
+import {
+  type AnyData,
+  snip,
+  type Snippet,
+  type TgpuDualFn,
+} from '../data/dataTypes.ts';
 import { smoothstepScalar } from '../data/numberOps.ts';
 import { f32 } from '../data/numeric.ts';
 import { VectorOps } from '../data/vectorOps.ts';
@@ -195,37 +200,32 @@ export const mul = createDualImpl(
   'mul',
 );
 
-function cpuDiv(lhs: number, rhs: number): number; // default js division
-function cpuDiv<MV extends NumVec>(lhs: number, rhs: MV): MV; // scale
-function cpuDiv<MV extends NumVec>(lhs: MV, rhs: number): MV; // scale
-function cpuDiv<V extends NumVec>(lhs: V, rhs: V): V; // component-wise division
-function cpuDiv<
-  // union overload
-  Lhs extends number | NumVec,
-  Rhs extends (Lhs extends number ? number | NumVec
-    : Lhs extends NumVec ? number | Lhs
-    : never),
->(lhs: Lhs, rhs: Rhs): Lhs | Rhs;
-function cpuDiv(lhs: number | NumVec, rhs: number | NumVec) {
-  if (typeof lhs === 'number' && typeof rhs === 'number') {
-    return (lhs / rhs);
-  }
-  if (typeof lhs === 'number' && isVecInstance(rhs)) {
-    return VectorOps.divMixed[rhs.kind](rhs, lhs);
-  }
-  if (isVecInstance(lhs) && typeof rhs === 'number') {
-    return VectorOps.divMixed[lhs.kind](lhs, rhs);
-  }
-  if (isVecInstance(lhs) && isVecInstance(rhs)) {
-    return VectorOps.div[lhs.kind](lhs, rhs);
-  }
+type DivOverload = {
+  (lhs: number, rhs: number): number; // default js division
+  <T extends NumVec | number>(lhs: T, rhs: T): T; // component-wise division
+  <T extends NumVec | number>(lhs: number, rhs: T): T; // mixed division
+  <T extends NumVec | number>(lhs: T, rhs: number): T; // mixed division
+};
 
-  throw new Error('Div called with invalid arguments.');
-}
-
-export const div = createDualImpl(
+export const div: TgpuDualFn<DivOverload> = createDualImpl(
   // CPU implementation
-  cpuDiv,
+  <T extends NumVec | number>(lhs: T, rhs: T): T => {
+    if (typeof lhs === 'number' && typeof rhs === 'number') {
+      return (lhs / rhs) as T;
+    }
+    if (typeof lhs === 'number' && isVecInstance(rhs)) {
+      const schema = vecTypeToConstructor[rhs.kind];
+      return VectorOps.div[rhs.kind](schema(lhs), rhs) as T;
+    }
+    if (isVecInstance(lhs) && typeof rhs === 'number') {
+      const schema = vecTypeToConstructor[lhs.kind];
+      return VectorOps.div[lhs.kind](lhs, schema(rhs)) as T;
+    }
+    if (isVecInstance(lhs) && isVecInstance(rhs)) {
+      return VectorOps.div[lhs.kind](lhs, rhs) as T;
+    }
+    throw new Error('Div called with invalid arguments.');
+  },
   // GPU implementation
   (lhs, rhs) => {
     if (isSnippetNumeric(lhs) && isSnippetNumeric(rhs)) {
