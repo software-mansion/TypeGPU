0ad72491b0846a9e299eaa71e09e335fd915e0a5
Rafal Lukosz <93160829+lursz@users.noreply.github.com>
2025-07-29 10:33:49 +0200

    feat: tgpu.resolve supports config (#1536)

--- packages/typegpu/src/resolutionCtx.ts
+++ packages/typegpu/src/resolutionCtx.ts
@@ -1,4 +1,6 @@
 import { resolveData } from './core/resolve/resolveData.ts';
+import { ConfigurableImpl } from './core/root/configurableImpl.ts';
+import type { Configurable } from './core/root/rootTypes.ts';
 import {
   type Eventual,
   isDerived,
@@ -694,9 +696,15 @@ export interface ResolutionResult {
 export function resolve(
   item: Wgsl,
   options: ResolutionCtxImplOptions,
+  config?: (cfg: Configurable) => Configurable,
 ): ResolutionResult {
   const ctx = new ResolutionCtxImpl(options);
-  let code = ctx.resolve(item);
+  let code = config
+    ? ctx.withSlots(
+      config(new ConfigurableImpl([])).bindings,
+      () => ctx.resolve(item),
+    )
+    : ctx.resolve(item);
 
   const memoMap = ctx.bindGroupLayoutsToPlaceholderMap;
   const usedBindGroupLayouts: TgpuBindGroupLayout[] = [];
