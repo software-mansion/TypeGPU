f5cf328516f1ab27af1b8ca52cffc16049e25c6a
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2025-07-31 22:47:18 +0200

    Smooth speed change

--- apps/typegpu-docs/src/content/examples/simple/vaporwave/constants.ts
+++ apps/typegpu-docs/src/content/examples/simple/vaporwave/constants.ts
@@ -6,11 +6,13 @@ export const SURF_DIST = 0.001;
 export const GRID_SEP = 1.2;
 export const GRID_TIGHTNESS = 7;
 export const CIRCLE_FLOOR_MASS = 5;
+export const PERIOD = 7;
+export const NUM_CYCLES = 5000;
 
 export const skyColor1 = d.vec4f(0.1, 0, 0.2, 1);
 export const skyColor2 = d.vec4f(0.28, 0, 0.54, 1);
 export const gridColor = d.vec3f(0.92, 0.21, 0.96);
-export const gridInnerColor = d.vec3f(0, 0, 0);
+export const gridInnerColor = d.vec3f();
 export const sphereCenter = d.vec3f(0, 6, 12);
 export const sphereRadius = d.f32(3);
 export const planeOrthonormal = d.vec3f(0, 1, 0);
