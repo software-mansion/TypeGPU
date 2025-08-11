d005a380ca32c59fbe8ec9e6d487cd3341393d15
lursz <lukoszr@gmail.com>
2025-08-11 12:53:20 +0200

    some improvements

--- apps/typegpu-docs/src/content/examples/algorithms/concurrent-chart/index.ts
+++ apps/typegpu-docs/src/content/examples/algorithms/concurrent-chart/index.ts
@@ -2,11 +2,13 @@ import tgpu from 'typegpu';
 import * as std from 'typegpu/std';
 import { performCalculationsWithTime } from './calculator.ts';
 
-const bars = Array.from(document.querySelectorAll('.bar')) as HTMLDivElement[];
 
 const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
 const context = canvas.getContext('webgpu') as GPUCanvasContext;
+const bars = Array.from(document.querySelectorAll<HTMLDivElement>('.bar'));
+const labels = Array.from(bars, bar => bar.querySelector<HTMLDivElement>('.label'));
+
 
 const root = await tgpu.init({
   device: {
@@ -47,18 +49,19 @@ async function initCalc() {
   return inputArrays;
 }
 
-  function drawCharts() {
-    const keys = Object.keys(lengthMap);
-    for (let i = 0; i < bars.length/2; i++) {
-      const value = lengthMap[keys[i]];
-        bars[2*i].style.setProperty('--bar-height', `${std.min(value.jsTime / 50, 1)}`);
-        bars[2*i].style.setProperty('--highlight-opacity', '1');
-        bars[2*i].textContent = `${(Number(value.jsTime) / Number(value.gpuTime)).toFixed(1)}x`;
+function drawCharts() {
+  const keys = Object.keys(lengthMap);
+  for (let i = 0; i < bars.length / 2; i++) {
+    const value = lengthMap[keys[i]];
+    bars[2 * i].style.setProperty('--bar-height', `${std.min(value.jsTime / 50, 1)}`);
+    bars[2 * i].style.setProperty('--highlight-opacity', '1');
+    labels[2 * i]!.textContent = `${(Number(value.jsTime) / Number(value.gpuTime)).toFixed(1)}x`;
 
-        bars[2*i+1].style.setProperty('--bar-height', `${std.min(value.gpuTime / 50, 1)}`);
-        bars[2*i+1].style.setProperty('--highlight-opacity', '1');
-    }
+    bars[2 * i + 1].style.setProperty('--bar-height', `${std.min(value.gpuTime / 50, 1)}`);
+    bars[2 * i + 1].style.setProperty('--highlight-opacity', '1');
+    // labels[2 * i + 1]!.textContent = `size: ${keys[i]}`;
   }
+}
 
 const uiState = {
   isDrawing: false,
@@ -72,6 +75,8 @@ function resetDrawing() {
 
   for (const bar of bars) {
     bar.style.setProperty('--bar-width', '0.2');
+    const label = bar.querySelector<HTMLDivElement>('.label');
+    if (label) label.textContent = 'Duap';
   }
 }
 
