cd45d7ee0178dec2d3044c40a6de8646bf8a8089
lursz <lukoszr@gmail.com>
2025-08-22 14:01:27 +0200

    proper private values

--- packages/typegpu-concurrent-scan/src/prefixScan.ts
+++ packages/typegpu-concurrent-scan/src/prefixScan.ts
@@ -21,15 +21,13 @@ import { scanGreatestBlock } from './compute/singleScan.ts';
 const cache = new WeakMap<TgpuRoot, WeakMap<BinaryOp, PrefixScanComputer>>();
 
 class PrefixScanComputer {
-  private _scanPipeline?: TgpuComputePipeline;
-  private _addPipeline?: TgpuComputePipeline;
-  private scratchBuffers: Map<
-    number,
-    TgpuBuffer<d.WgslArray<d.F32>> & StorageFlag
-  > = new Map();
-  private querySet: TgpuQuerySet<'timestamp'> | null = null;
-  private first = true;
-  private _timeCallback?:
+  #scanPipeline?: TgpuComputePipeline;
+  #addPipeline?: TgpuComputePipeline;
+  #scratchBuffers: Map<number, TgpuBuffer<d.WgslArray<d.F32>> & StorageFlag> =
+    new Map();
+  #querySet: TgpuQuerySet<'timestamp'> | null = null;
+  #first = true;
+  #timeCallback?:
     | ((timeTgpuQuery: TgpuQuerySet<'timestamp'>) => void)
     | undefined;
 
@@ -39,37 +37,35 @@ class PrefixScanComputer {
     private onlyGreatestElement: boolean,
     timeCallback?: (timeTgpuQuery: TgpuQuerySet<'timestamp'>) => void,
   ) {
-    this._timeCallback = timeCallback;
-    this.querySet = root.createQuerySet('timestamp', 2);
+    this.#timeCallback = timeCallback;
+    this.#querySet = root.createQuerySet('timestamp', 2);
   }
 
   updateTimeCallback(
     timeCallback?: (timeTgpuQuery: TgpuQuerySet<'timestamp'>) => void,
   ): void {
-    this._timeCallback = timeCallback;
+    this.#timeCallback = timeCallback;
   }
 
   private get scanPipeline(): TgpuComputePipeline {
-    if (!this._scanPipeline) {
-      this._scanPipeline = this.root['~unstable'].with(
-        operatorSlot,
-        this.binaryOp.operation as d.TgpuCallable,
-      ).with(identitySlot, this.binaryOp.identityElement)
+    if (!this.#scanPipeline) {
+      this.#scanPipeline = this.root['~unstable']
+        .with(operatorSlot, this.binaryOp.operation as d.TgpuCallable)
+        .with(identitySlot, this.binaryOp.identityElement)
         .withCompute(this.onlyGreatestElement ? scanGreatestBlock : scanBlock)
         .createPipeline();
     }
-    return this._scanPipeline;
+    return this.#scanPipeline;
   }
 
   private get addPipeline(): TgpuComputePipeline {
-    if (!this._addPipeline) {
-      this._addPipeline = this.root['~unstable'].with(
-        operatorSlot,
-        this.binaryOp.operation as d.TgpuCallable,
-      ).withCompute(uniformAdd)
+    if (!this.#addPipeline) {
+      this.#addPipeline = this.root['~unstable']
+        .with(operatorSlot, this.binaryOp.operation as d.TgpuCallable)
+        .withCompute(uniformAdd)
         .createPipeline();
     }
-    return this._addPipeline;
+    return this.#addPipeline;
   }
 
   private getScratchBuffer(
@@ -77,13 +73,13 @@ class PrefixScanComputer {
     level: number,
   ): TgpuBuffer<d.WgslArray<d.F32>> & StorageFlag {
     const key = (level << 20) | size;
-    if (!this.scratchBuffers.has(key)) {
-      this.scratchBuffers.set(
+    if (!this.#scratchBuffers.has(key)) {
+      this.#scratchBuffers.set(
         key,
         this.root.createBuffer(d.arrayOf(d.f32, size)).$usage('storage'),
       );
     }
-    return this.scratchBuffers.get(key) as
+    return this.#scratchBuffers.get(key) as
       & TgpuBuffer<d.WgslArray<d.F32>>
       & StorageFlag;
   }
@@ -102,13 +98,16 @@ class PrefixScanComputer {
         input: buffer,
         sums: dummySums,
       });
-      this.scanPipeline.with(scanLayout, bg).withTimestampWrites({
-        querySet: this.querySet as TgpuQuerySet<'timestamp'>,
-        beginningOfPassWriteIndex: this.first
-          ? 0
-          : undefined as unknown as number,
-        endOfPassWriteIndex: 1,
-      }).dispatchWorkgroups(1);
+      this.scanPipeline
+        .with(scanLayout, bg)
+        .withTimestampWrites({
+          querySet: this.#querySet as TgpuQuerySet<'timestamp'>,
+          beginningOfPassWriteIndex: this.#first
+            ? 0
+            : (undefined as unknown as number),
+          endOfPassWriteIndex: 1,
+        })
+        .dispatchWorkgroups(1);
 
       if (this.onlyGreatestElement) {
         return dummySums;
@@ -123,18 +122,19 @@ class PrefixScanComputer {
       input: buffer,
       sums: sumsBuffer,
     });
-    if (this.first) {
-      this.scanPipeline.with(scanLayout, scanBg).withTimestampWrites({
-        querySet: this.querySet as TgpuQuerySet<'timestamp'>,
-        beginningOfPassWriteIndex: 0,
-      }).dispatchWorkgroups(
-        numWorkgroups,
-      );
-      this.first = false;
+    if (this.#first) {
+      this.scanPipeline
+        .with(scanLayout, scanBg)
+        .withTimestampWrites({
+          querySet: this.#querySet as TgpuQuerySet<'timestamp'>,
+          beginningOfPassWriteIndex: 0,
+        })
+        .dispatchWorkgroups(numWorkgroups);
+      this.#first = false;
     } else {
-      this.scanPipeline.with(scanLayout, scanBg).dispatchWorkgroups(
-        numWorkgroups,
-      );
+      this.scanPipeline
+        .with(scanLayout, scanBg)
+        .dispatchWorkgroups(numWorkgroups);
     }
 
     // Recursively scan the sums
@@ -148,12 +148,13 @@ class PrefixScanComputer {
       input: buffer,
       sums: sumsBuffer,
     });
-    this.addPipeline.with(uniformAddLayout, addBg).withTimestampWrites({
-      querySet: this.querySet as TgpuQuerySet<'timestamp'>,
-      endOfPassWriteIndex: 1,
-    }).dispatchWorkgroups(
-      numWorkgroups,
-    );
+    this.addPipeline
+      .with(uniformAddLayout, addBg)
+      .withTimestampWrites({
+        querySet: this.#querySet as TgpuQuerySet<'timestamp'>,
+        endOfPassWriteIndex: 1,
+      })
+      .dispatchWorkgroups(numWorkgroups);
     return buffer;
   }
 
@@ -167,9 +168,9 @@ class PrefixScanComputer {
     const result = this.recursiveScan(buffer, buffer.dataType.elementCount);
     this.root['~unstable'].flush();
 
-    this.querySet?.resolve();
-    if (this._timeCallback && this.querySet) {
-      this._timeCallback(this.querySet);
+    this.#querySet?.resolve();
+    if (this.#timeCallback && this.#querySet) {
+      this.#timeCallback(this.#querySet);
     }
     return result;
   }
