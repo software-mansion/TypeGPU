f4427bb4a925bbdc95328c6808a6e3c7372037fa
Aleksander Katan <aleksander.katan@swmansion.com>
2025-08-27 16:13:08 +0200

    Throw when dispatch/workgroup has 0 threads

--- packages/typegpu/src/prepareDispatch.ts
+++ packages/typegpu/src/prepareDispatch.ts
@@ -11,21 +11,24 @@ import { ceil } from './std/numeric.ts';
 /**
  * Changes the given array to a vec of 3 numbers, filling missing values with 1.
  */
-function sanitizeArray(arr: readonly number[]): v3u {
+function toVec3(arr: readonly number[]): v3u {
+  if (arr.includes(0)) {
+    throw new Error('Size and workgroupSize cannot contain zeroes.');
+  }
   return vec3u(arr[0] ?? 1, arr[1] ?? 1, arr[2] ?? 1);
 }
 
 /**
  * Creates a dispatch function for a compute pipeline.
- * 
+ *
  * The returned function can be called multiple times to run GPU computations.
  * It returns a promise that resolves when the device queue completes its work.
- * 
+ *
  * @param options.root A TgpuRoot instance.
  * @param options.callback A function converted to WGSL and executed on the GPU. Its arguments correspond to the global invocation IDs.
  * @param options.size A 3D (or shorter) array specifying the total number of threads to run.
  * @param options.workgroupSize (optional) A 3D (or shorter) array specifying the workgroup dimensions. Defaults to [1, 1, 1].
- * 
+ *
  * The callback is not invoked for any invocation IDs that exceed `options.size`.
  */
 export function prepareDispatch(options: {
@@ -52,8 +55,8 @@ export function prepareDispatch(options: {
   size: readonly number[];
   workgroupSize?: readonly number[];
 }): () => Promise<undefined> {
-  const size = sanitizeArray(options.size);
-  const workgroupSize = sanitizeArray(options.workgroupSize ?? []);
+  const size = toVec3(options.size);
+  const workgroupSize = toVec3(options.workgroupSize ?? []);
   const workgroupCount = ceil(vec3f(size).div(vec3f(workgroupSize)));
 
   const wrappedCallback = fn([u32, u32, u32])(options.callback);
