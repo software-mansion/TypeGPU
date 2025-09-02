765485f5e54db1282975fd22aabf31c38c97da72
Aleksander Katan <aleksander.katan@swmansion.com>
2025-09-02 11:30:28 +0200

    Update test example

--- apps/typegpu-docs/src/content/examples/tests/log-test/index.ts
+++ apps/typegpu-docs/src/content/examples/tests/log-test/index.ts
@@ -1,36 +1,41 @@
-import tgpu from 'typegpu';
+import tgpu, { prepareDispatch } from 'typegpu';
 import * as d from 'typegpu/data';
 
 const root = await tgpu.init();
 
-function run(callback: (input: { gid: d.v3u }) => undefined, size: number) {
-  const computeRunTests = tgpu['~unstable']
-    .computeFn({
-      workgroupSize: [1],
-      in: { gid: d.builtin.globalInvocationId },
-    })(callback);
-
-  const pipeline = root['~unstable']
-    .withCompute(computeRunTests)
-    .createPipeline();
-
-  pipeline.dispatchWorkgroups(size);
-
-  console.log(tgpu.resolve({ externals: { pipeline } }));
-}
-
-// run(({ gid }) => {
-//   'kernel';
-//   console.log(gid.x + 10);
-//   console.log(gid.add(1).mul(3));
-// }, 2);
-
-run(() => {
-  'kernel';
-  console.log(d.u32(1), d.u32(2), d.vec3u(9, 8, 7), d.u32(3));
-}, 1);
-
 // #region Example controls and cleanup
+
+export const controls = {
+  'Single log': {
+    onButtonClick: () =>
+      prepareDispatch(root, () => {
+        'kernel';
+        console.log(d.u32(321));
+      })(),
+  },
+  'Two logs': {
+    onButtonClick: () =>
+      prepareDispatch(root, () => {
+        'kernel';
+        console.log(d.u32(98));
+        console.log(d.u32(76));
+      })(),
+  },
+  'Two threads': {
+    onButtonClick: () =>
+      prepareDispatch(root, (x) => {
+        'kernel';
+        console.log(x);
+      })(2),
+  },
+  'Multiple arguments': {
+    onButtonClick: () =>
+      prepareDispatch(root, () => {
+        'kernel';
+        console.log(d.u32(1), d.vec3u(2, 3, 4), d.u32(5), d.u32(6));
+      })(),
+  },
+};
 
 export function onCleanup() {
   root.destroy();
