665c0c856b6c262baef5dcc615fab31a3735de24
Aleksander Katan <aleksander.katan@swmansion.com>
2025-09-08 16:02:18 +0200

    Add render test to test example

--- apps/typegpu-docs/src/content/examples/tests/log-test/index.ts
+++ apps/typegpu-docs/src/content/examples/tests/log-test/index.ts
@@ -1,10 +1,11 @@
 import tgpu, { prepareDispatch } from 'typegpu';
 import * as d from 'typegpu/data';
+import * as std from 'typegpu/std';
 
 const root = await tgpu.init({
   unstable_logOptions: {
     logCountPerDispatchLimit: 30,
-    serializedLogDataSizeLimit: 128,
+    serializedLogDataSizeLimit: 32,
   },
 });
 
@@ -76,6 +77,64 @@ export const controls = {
         'kernel';
         console.log(d.vec3u(), d.vec3u(), d.vec3u());
       })(16),
+  },
+  'Render pipeline': {
+    onButtonClick: () => {
+      const purple = d.vec4f(0.769, 0.392, 1.0, 1);
+      const blue = d.vec4f(0.114, 0.447, 0.941, 1);
+
+      const mainVertex = tgpu['~unstable'].vertexFn({
+        in: { vertexIndex: d.builtin.vertexIndex },
+        out: { pos: d.builtin.position, uv: d.vec2f },
+      })((input, Out) => {
+        const positions = [
+          d.vec2f(0, 0.5),
+          d.vec2f(-0.5, -0.5),
+          d.vec2f(0.5, -0.5),
+        ];
+
+        const uv = [d.vec2f(0.5, 1), d.vec2f(0, 0), d.vec2f(1, 0)];
+
+        return Out({
+          pos: d.vec4f(positions[input.vertexIndex], 0, 1),
+          uv: uv[input.vertexIndex],
+        });
+      });
+
+      const mainFragment = tgpu['~unstable'].fragmentFn({
+        in: { pos: d.builtin.position },
+        out: d.vec4f,
+      })(({ pos }) => {
+        console.log('X:', d.u32(pos.x), 'Y:', d.u32(pos.y));
+        return std.mix(blue, purple, pos.x / 20);
+      });
+
+      const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+      const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+      const context = canvas.getContext('webgpu') as GPUCanvasContext;
+
+      context.configure({
+        device: root.device,
+        format: presentationFormat,
+        alphaMode: 'premultiplied',
+      });
+
+      const pipeline = root['~unstable']
+        .withVertex(mainVertex, {})
+        .withFragment(mainFragment, { format: presentationFormat })
+        .createPipeline();
+
+      setTimeout(() => {
+        pipeline
+          .withColorAttachment({
+            view: context.getCurrentTexture().createView(),
+            clearValue: [0, 0, 0, 0],
+            loadOp: 'clear',
+            storeOp: 'store',
+          })
+          .draw(3);
+      }, 100);
+    },
   },
 };
 
