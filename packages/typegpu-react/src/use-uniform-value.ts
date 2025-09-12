20391bd3961680075582e1d74dcc04bd5c71fd01
Sebastian Piaskowy <sebastian.piaskowy@swmansion.com>
2025-09-12 12:42:27 +0200

    Implement hook logic

--- packages/typegpu-react/src/use-uniform-value.ts
+++ packages/typegpu-react/src/use-uniform-value.ts
@@ -1,13 +1,44 @@
-import type * as d from 'typegpu/data';
+import * as d from 'typegpu/data';
+import { useRoot } from './root-context';
+import { useMemo } from 'react';
 
 interface UniformValue<TSchema, TValue extends d.Infer<TSchema>> {
   schema: TSchema;
-  value: TValue;
+  value: TValue | undefined;
+  readonly $: d.InferGPU<TSchema>;
 }
 
-export function useUniformValue<TSchema, TValue extends d.Infer<TSchema>>(
-  schema: d.AnyWgslData,
-  initialValue?: TValue | undefined,
+export function useUniformValue<
+  TSchema extends d.AnyWgslData,
+  TValue extends d.Infer<TSchema>,
+>(
+  schema: TSchema,
+  initialValue?: TValue | undefined
 ): UniformValue<TSchema, TValue> {
-  // TODO: Implement
+  const root = useRoot();
+
+  const uniformBuffer = useMemo(() => {
+    return root.createUniform(schema as any, initialValue);
+  }, [root, schema, initialValue]);
+
+  const uniformValue = useMemo(() => {
+    let currentValue = initialValue;
+    return {
+      schema,
+      get value() {
+        return currentValue;
+      },
+      set value(newValue: TValue | undefined) {
+        currentValue = newValue;
+        if (newValue !== undefined) {
+          uniformBuffer.write(newValue);
+        }
+      },
+      get $() {
+        return uniformBuffer.$;
+      },
+    };
+  }, [schema, uniformBuffer, initialValue]);
+
+  return uniformValue;
 }
