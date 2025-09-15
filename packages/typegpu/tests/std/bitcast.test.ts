131a74f4d78bac2613de81736ab72a09b7c1cdb8
lursz <lukoszr@gmail.com>
2025-09-15 10:55:39 +0200

    🦕

--- packages/typegpu/tests/std/bitcast.test.ts
+++ packages/typegpu/tests/std/bitcast.test.ts
@@ -132,44 +132,42 @@ describe('bitcast', () => {
     const c4 = std.bitcastU32toI32(v4);
     expect(c4).toEqual(vec4i(-2147483648, 1, 0, 2147483647));
   });
+});
 
-  describe('bitcast in shaders', () => {
-    it('works for primitives', () => {
-      const fnf32 = tgpu.fn([], d.f32)(() => std.bitcastU32toF32(1234));
-      const fni32 = tgpu.fn([], d.i32)(() =>
-        std.bitcastU32toI32(d.u32(2 ** 31))
-      );
+describe('bitcast in shaders', () => {
+  it('works for primitives', () => {
+    const fnf32 = tgpu.fn([], d.f32)(() => std.bitcastU32toF32(1234));
+    const fni32 = tgpu.fn([], d.i32)(() => std.bitcastU32toI32(d.u32(2 ** 31)));
 
-      expect(asWgsl(fnf32)).toMatchInlineSnapshot(`
-      "fn fnf32() -> f32 {
-        return 1.7292023049768243e-42;
-      }"
-    `);
-      expect(asWgsl(fni32)).toMatchInlineSnapshot(`
-      "fn fni32() -> i32 {
-        return -2147483648;
-      }"
-    `);
-    });
+    expect(asWgsl(fnf32)).toMatchInlineSnapshot(`
+    "fn fnf32() -> f32 {
+      return 1.7292023049768243e-42;
+    }"
+  `);
+    expect(asWgsl(fni32)).toMatchInlineSnapshot(`
+    "fn fni32() -> i32 {
+      return -2147483648;
+    }"
+  `);
+  });
 
-    it('works for vectors', () => {
-      const fnvec4i = tgpu.fn([], d.vec4i)(() =>
-        std.bitcastU32toI32(vec4u(1, 2, 3, 4))
-      );
-      const fnvec4f = tgpu.fn([], d.vec4f)(() =>
-        std.bitcastU32toF32(vec4u(1, 2, 3, 4))
-      );
+  it('works for vectors', () => {
+    const fnvec4i = tgpu.fn([], d.vec4i)(() =>
+      std.bitcastU32toI32(vec4u(1, 2, 3, 4))
+    );
+    const fnvec4f = tgpu.fn([], d.vec4f)(() =>
+      std.bitcastU32toF32(vec4u(1, 2, 3, 4))
+    );
 
-      expect(asWgsl(fnvec4i)).toMatchInlineSnapshot(`
-      "fn fnvec4i() -> vec4i {
-        return vec4i(1, 2, 3, 4);
-      }"
-    `);
-      expect(asWgsl(fnvec4f)).toMatchInlineSnapshot(`
-      "fn fnvec4f() -> vec4f {
-        return vec4f(1.401298464324817e-45, 2.802596928649634e-45, 4.203895392974451e-45, 5.605193857299268e-45);
-      }"
-    `);
-    });
+    expect(asWgsl(fnvec4i)).toMatchInlineSnapshot(`
+    "fn fnvec4i() -> vec4i {
+      return vec4i(1, 2, 3, 4);
+    }"
+  `);
+    expect(asWgsl(fnvec4f)).toMatchInlineSnapshot(`
+    "fn fnvec4f() -> vec4f {
+      return vec4f(1.401298464324817e-45, 2.802596928649634e-45, 4.203895392974451e-45, 5.605193857299268e-45);
+    }"
+  `);
   });
 });
