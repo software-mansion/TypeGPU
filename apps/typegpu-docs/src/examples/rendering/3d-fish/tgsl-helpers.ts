29e050dcdea83e91947d467f64d7b8ff28b359e0
Iwo Plaza <iwoplaza@gmail.com>
2025-09-18 14:41:24 +0200

    Better and smaller

--- apps/typegpu-docs/src/examples/rendering/3d-fish/tgsl-helpers.ts
+++ apps/typegpu-docs/src/examples/rendering/3d-fish/tgsl-helpers.ts
@@ -1,11 +1,11 @@
 import * as d from 'typegpu/data';
-import * as std from 'typegpu/std';
+import { add, cos, dot, normalize, sin } from 'typegpu/std';
 import type { Line3 } from './schemas.ts';
 
 export const projectPointOnLine = (point: d.v3f, line: Line3): d.v3f => {
   'kernel';
-  const pointVector = std.sub(point, line.origin);
-  const projection = std.dot(pointVector, line.dir);
+  const pointVector = point.sub(line.origin);
+  const projection = dot(pointVector, line.dir);
   return line.origin.add(line.dir.mul(projection));
 };
 
@@ -19,28 +19,26 @@ export const applySinWave = (
   index: number,
   vertex: PosAndNormal,
   time: number,
-): PosAndNormal => {
+) => {
   'kernel';
   const a = -60.1;
   const b = 0.8;
   const c = 6.1;
   // z += sin(index + (time / a + x) / b) / c
 
-  const positionModification = d.vec3f();
-  positionModification.z =
-    std.sin(d.f32(index) + (time / a + vertex.position.x) / b) / c;
+  const posMod = d.vec3f();
+  posMod.z = sin(d.f32(index) + (time / a + vertex.position.x) / b) / c;
 
-  const coeff = std.cos(d.f32(index) + (time / a + vertex.position.x) / b) /
-    c;
-  const newOX = std.normalize(d.vec3f(1, 0, coeff));
+  const coeff = cos(d.f32(index) + (time / a + vertex.position.x) / b) / c;
+  const newOX = normalize(d.vec3f(1, 0, coeff));
   const newOZ = d.vec3f(-newOX.z, 0, newOX.x);
-  const newNormalXZ = std.add(
-    std.mul(vertex.normal.x, newOX),
-    std.mul(vertex.normal.z, newOZ),
+  const newNormalXZ = add(
+    newOX.mul(vertex.normal.x),
+    newOZ.mul(vertex.normal.z),
   );
 
   const wavedNormal = d.vec3f(newNormalXZ.x, vertex.normal.y, newNormalXZ.z);
-  const wavedPosition = std.add(vertex.position, positionModification);
+  const wavedPosition = vertex.position.add(posMod);
 
   return PosAndNormal({
     position: wavedPosition,
