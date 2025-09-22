17ab16771ea87d163109ca941720a1d146a9909e
Aleksander Katan <aleksander.katan@swmansion.com>
2025-09-22 15:13:40 +0200

    smol

--- packages/typegpu/src/tgsl/consoleLog/serializers.ts
+++ packages/typegpu/src/tgsl/consoleLog/serializers.ts
@@ -44,9 +44,9 @@ type SerializerMap = {
   >;
 };
 
-const dataBlockIndex = privateVar(u32, 0);
-const dataByteIndex = privateVar(u32, 0);
-const dataBufferSlot = slot();
+const dataBlockIndex = privateVar(u32, 0).$name('dataBlockIndex');
+const dataByteIndex = privateVar(u32, 0).$name('dataByteIndex');
+const dataBufferSlot = slot().$name('dataBuffer');
 const nextByteIndex = fn([], u32)`() {
   let i = dataByteIndex;
   dataByteIndex = dataByteIndex + 1u;
@@ -57,99 +57,99 @@ const nextByteIndex = fn([], u32)`() {
 const nextU32 = 'dataBuffer[dataBlockIndex].serializedData[nextByteIndex()]';
 
 export const serializerMap: SerializerMap = {
-  f32: fn([f32], Void)`(n) => {
+  f32: fn([f32])`(n) => {
   ${nextU32} = bitcast<u32>(n);
 }`,
-  f16: fn([f16], Void)`(n) => {
+  f16: fn([f16])`(n) => {
   ${nextU32} = pack2x16float(vec2f(f32(n), 0.0));
 }`,
-  i32: fn([i32], Void)`(n) => {
+  i32: fn([i32])`(n) => {
   ${nextU32} = bitcast<u32>(n);
 }`,
-  u32: fn([u32], Void)`(n) => {
+  u32: fn([u32])`(n) => {
   ${nextU32} = n;
 }`,
-  bool: fn([bool], Void)`(b) => {
+  bool: fn([bool])`(b) => {
   ${nextU32} = u32(b);
 }`,
-  vec2f: fn([vec2f], Void)`(v) => {
+  vec2f: fn([vec2f])`(v) => {
   ${nextU32} = bitcast<u32>(v.x);
   ${nextU32} = bitcast<u32>(v.y);
 }`,
-  vec3f: fn([vec3f], Void)`(v) => {
+  vec3f: fn([vec3f])`(v) => {
   ${nextU32} = bitcast<u32>(v.x);
   ${nextU32} = bitcast<u32>(v.y);
   ${nextU32} = bitcast<u32>(v.z);
 }`,
-  vec4f: fn([vec4f], Void)`(v) => {
+  vec4f: fn([vec4f])`(v) => {
   ${nextU32} = bitcast<u32>(v.x);
   ${nextU32} = bitcast<u32>(v.y);
   ${nextU32} = bitcast<u32>(v.z);
   ${nextU32} = bitcast<u32>(v.w);
 }`,
-  vec2h: fn([vec2h], Void)`(v) => {
+  vec2h: fn([vec2h])`(v) => {
   ${nextU32} = pack2x16float(vec2f(f32(v.x), f32(v.y)));
 }`,
-  vec3h: fn([vec3h], Void)`(v) => {
+  vec3h: fn([vec3h])`(v) => {
   ${nextU32} = pack2x16float(vec2f(f32(v.x), f32(v.y)));
   ${nextU32} = pack2x16float(vec2f(f32(v.z), 0.0));
 }`,
-  vec4h: fn([vec4h], Void)`(v) => {
+  vec4h: fn([vec4h])`(v) => {
   ${nextU32} = pack2x16float(vec2f(f32(v.x), f32(v.y)));
   ${nextU32} = pack2x16float(vec2f(f32(v.z), f32(v.w)));
 }`,
-  vec2i: fn([vec2i], Void)`(v) => {
+  vec2i: fn([vec2i])`(v) => {
   ${nextU32} = bitcast<u32>(v.x);
   ${nextU32} = bitcast<u32>(v.y);
 }`,
-  vec3i: fn([vec3i], Void)`(v) => {
+  vec3i: fn([vec3i])`(v) => {
   ${nextU32} = bitcast<u32>(v.x);
   ${nextU32} = bitcast<u32>(v.y);
   ${nextU32} = bitcast<u32>(v.z);
 }`,
-  vec4i: fn([vec4i], Void)`(v) => {
+  vec4i: fn([vec4i])`(v) => {
   ${nextU32} = bitcast<u32>(v.x);
   ${nextU32} = bitcast<u32>(v.y);
   ${nextU32} = bitcast<u32>(v.z);
   ${nextU32} = bitcast<u32>(v.w);
 }`,
-  vec2u: fn([vec2u], Void)`(v) => {
+  vec2u: fn([vec2u])`(v) => {
   ${nextU32} = v.x;
   ${nextU32} = v.y;
 }`,
-  vec3u: fn([vec3u], Void)`(v) => {
+  vec3u: fn([vec3u])`(v) => {
   ${nextU32} = v.x;
   ${nextU32} = v.y;
   ${nextU32} = v.z;
 }`,
-  vec4u: fn([vec4u], Void)`(v) => {
+  vec4u: fn([vec4u])`(v) => {
   ${nextU32} = v.x;
   ${nextU32} = v.y;
   ${nextU32} = v.z;
   ${nextU32} = v.w;
 }`,
-  'vec2<bool>': fn([vec2b], Void)`(v) => {
+  'vec2<bool>': fn([vec2b])`(v) => {
   ${nextU32} = u32(v.x);
   ${nextU32} = u32(v.y);
 }`,
-  'vec3<bool>': fn([vec3b], Void)`(v) => {
+  'vec3<bool>': fn([vec3b])`(v) => {
   ${nextU32} = u32(v.x);
   ${nextU32} = u32(v.y);
   ${nextU32} = u32(v.z);
 }`,
-  'vec4<bool>': fn([vec4b], Void)`(v) => {
+  'vec4<bool>': fn([vec4b])`(v) => {
   ${nextU32} = u32(v.x);
   ${nextU32} = u32(v.y);
   ${nextU32} = u32(v.z);
   ${nextU32} = u32(v.w);
 }`,
-  mat2x2f: fn([mat2x2f], Void)`(m) => {
+  mat2x2f: fn([mat2x2f])`(m) => {
   ${nextU32} = bitcast<u32>(m[0][0]);
   ${nextU32} = bitcast<u32>(m[0][1]);
   ${nextU32} = bitcast<u32>(m[1][0]);
   ${nextU32} = bitcast<u32>(m[1][1]);
 }`,
-  mat3x3f: fn([mat3x3f], Void)`(m) => {
+  mat3x3f: fn([mat3x3f])`(m) => {
   ${nextU32} = bitcast<u32>(m[0][0]);
   ${nextU32} = bitcast<u32>(m[0][1]);
   ${nextU32} = bitcast<u32>(m[0][2]);
@@ -163,7 +163,7 @@ export const serializerMap: SerializerMap = {
   ${nextU32} = bitcast<u32>(m[2][2]);
   ${nextU32} = 0u;
 }`,
-  mat4x4f: fn([mat4x4f], Void)`(m) => {
+  mat4x4f: fn([mat4x4f])`(m) => {
   ${nextU32} = bitcast<u32>(m[0][0]);
   ${nextU32} = bitcast<u32>(m[0][1]);
   ${nextU32} = bitcast<u32>(m[0][2]);
@@ -185,9 +185,11 @@ export const serializerMap: SerializerMap = {
 
 // rename the functions and add externals
 for (const [name, serializer] of Object.entries(serializerMap)) {
-  serializer.$name(
-    `serialize${(name[0] as string).toLocaleUpperCase()}${name.slice(1)}`,
-  ).$uses({ dataBlockIndex, nextByteIndex, dataBuffer: dataBufferSlot });
+  serializer
+    .$name(
+      `serialize${(name[0] as string).toLocaleUpperCase()}${name.slice(1)}`,
+    )
+    .$uses({ dataBlockIndex, nextByteIndex, dataBuffer: dataBufferSlot });
 }
 
 // -------
@@ -209,10 +211,11 @@ function getSerializer<T extends AnyWgslData>(
   if (isWgslStruct(dataType)) {
     const props = Object.keys(dataType.propTypes);
     const propTypes = Object.values(dataType.propTypes) as AnyWgslData[];
-    const propSerializer = createCompoundSerializer(propTypes, dataBuffer);
-    return fn([dataType])`(arg) {\n  propSerializer(${
+    const propsSerializer = createCompoundSerializer(propTypes, dataBuffer);
+    return fn([dataType])`(arg) {\n  propsSerializer(${
       props.map((prop) => `arg.${prop}`).join(', ')
-    });\n}`.$uses({ propSerializer })
+    });\n}`
+      .$uses({ propsSerializer })
       .$name(`${getName(dataType) ?? 'struct'}Serializer`);
   }
   if (isWgslArray(dataType)) {
