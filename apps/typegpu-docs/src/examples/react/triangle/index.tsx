c44ab837d1199421cd4b62cc03d7145ad4b6ee07
Sebastian Piaskowy <sebastian.piaskowy@swmansion.com>
2025-09-23 18:06:56 +0200

    Add useMirroredUniform example usage

--- apps/typegpu-docs/src/examples/react/triangle/index.tsx
+++ apps/typegpu-docs/src/examples/react/triangle/index.tsx
@@ -1,9 +1,42 @@
 import * as d from 'typegpu/data';
-import { useFrame, useRender, useUniformValue } from '@typegpu/react';
+import {
+  useFrame,
+  useMirroredUniform,
+  useRender,
+  useUniformValue,
+} from '@typegpu/react';
 import { hsvToRgb } from '@typegpu/color';
 
+// TODO: We can come up with a more sophisticated example later
+function ColorBox(props: { color: d.v3f }) {
+  const color = useMirroredUniform(d.vec3f, props.color);
+
+  const { ref } = useRender({
+    fragment: () => {
+      'kernel';
+      return d.vec4f(color.$, 1);
+    },
+  });
+
+  return (
+    <canvas
+      ref={ref}
+      width='32'
+      height='32'
+      style={{ border: '2px black solid' }}
+    />
+  );
+}
+
+let randomizeColor: () => void;
+
 function App() {
+  const [currentColor, setCurrentColor] = useState(d.vec3f(1, 0, 0));
   const time = useUniformValue(d.f32, 0);
+
+  randomizeColor = () => {
+    setCurrentColor(d.vec3f(Math.random(), Math.random(), Math.random()));
+  };
 
   useFrame(() => {
     time.value = performance.now() / 1000;
@@ -14,13 +47,15 @@ function App() {
       'kernel';
       const t = time.$;
       const rgb = hsvToRgb(d.vec3f(t * 0.5, 1, 1));
+
       return d.vec4f(rgb, 1);
     },
   });
 
   return (
-    <main>
+    <main style={{ display: 'flex', alignItems: 'center', gap: 32 }}>
       <canvas ref={ref} width='256' height='256' />
+      <ColorBox color={currentColor} />
     </main>
   );
 }
@@ -28,10 +63,19 @@ function App() {
 // #region Example controls and cleanup
 
 import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
 const reactRoot = createRoot(
   document.getElementById('example-app') as HTMLDivElement,
 );
 reactRoot.render(<App />);
+
+export const controls = {
+  'Randomize box color': {
+    onButtonClick: () => {
+      randomizeColor();
+    },
+  },
+};
 
 export function onCleanup() {
   setTimeout(() => reactRoot.unmount(), 0);
