7a3135b3566b6f5fed7cd43b0f076cc6eb223477
Aleksander Katan <aleksander.katan@swmansion.com>
2025-09-24 17:12:27 +0200

    Add some tests

--- packages/typegpu/tests/tgsl/createDualImpl.test.ts
+++ packages/typegpu/tests/tgsl/createDualImpl.test.ts
@@ -1,10 +1,61 @@
 import { describe, expect, it } from 'vitest';
-import { createDualImpl } from '../../src/core/function/dualImpl.ts';
+import { createDualImpl, dualImpl } from '../../src/core/function/dualImpl.ts';
 import { getName } from '../../src/shared/meta.ts';
+import { Void } from '../../src/data/wgslTypes.ts';
+import tgpu from '../../src/index.ts';
+import { asWgsl } from '../utils/parseResolved.ts';
 
 describe('createDualImpl', () => {
   it('names functions created by createDualImpl', () => {
     const dual = createDualImpl((a) => a, (snippet) => snippet, 'myDualImpl');
     expect(getName(dual)).toBe('myDualImpl');
+  });
+});
+
+describe('dualImpl', () => {
+  it('names functions created by dualImpl', () => {
+    const dual = dualImpl({
+      normalImpl: () => {},
+      signature: () => ({ argTypes: [], returnType: Void }),
+      codegenImpl: () => 'code',
+      name: 'myDualImpl',
+    });
+    expect(getName(dual)).toBe('myDualImpl');
+  });
+
+  it('inlines results when possible', () => {
+    const dual = dualImpl({
+      normalImpl: (a: number) => a + 3,
+      signature: (snippet) => ({ argTypes: [snippet], returnType: snippet }),
+      codegenImpl: (snippet) => `(${snippet.value} + 3)`,
+      name: 'myDualImpl',
+    });
+
+    const myFn = tgpu.fn([])(() => {
+      const a = dual(2);
+    });
+
+    expect(asWgsl(myFn)).toMatchInlineSnapshot(`
+      "fn myFn() {
+        var a = 5;
+      }"
+    `);
+  });
+
+  it('fallbacks to codegenImpl', () => {
+    const dual = dualImpl({
+      normalImpl: (a: number) => {
+        throw new Error('Not implemented yet.');
+      },
+      signature: (snippet) => ({ argTypes: [snippet], returnType: snippet }),
+      codegenImpl: (snippet) => `fallback(${snippet.value})`,
+      name: 'myDualImpl',
+    });
+
+    const myFn = tgpu.fn([])(() => {
+      const a = dual(2);
+    });
+
+    expect(asWgsl(myFn)).toMatchInlineSnapshot();
   });
 });
