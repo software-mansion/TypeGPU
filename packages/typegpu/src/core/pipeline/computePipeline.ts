927b1e6320f265c55a8f55e399e47fa4f442fc6c
Aleksander Katan <aleksander.katan@swmansion.com>
2025-09-30 17:36:11 +0200

    Add one argument `with` implementations

--- packages/typegpu/src/core/pipeline/computePipeline.ts
+++ packages/typegpu/src/core/pipeline/computePipeline.ts
@@ -6,10 +6,11 @@ import { type ResolutionResult, resolve } from '../../resolutionCtx.ts';
 import type { TgpuNamable } from '../../shared/meta.ts';
 import { getName, PERF, setName } from '../../shared/meta.ts';
 import { $getNameForward, $internal, $resolve } from '../../shared/symbols.ts';
-import type {
-  TgpuBindGroup,
-  TgpuBindGroupLayout,
-  TgpuLayoutEntry,
+import {
+  isBindGroup,
+  type TgpuBindGroup,
+  type TgpuBindGroupLayout,
+  type TgpuLayoutEntry,
 } from '../../tgpuBindGroupLayout.ts';
 import { logDataFromGPU } from '../../tgsl/consoleLog/deserializers.ts';
 import type { LogResources } from '../../tgsl/consoleLog/types.ts';
@@ -50,6 +51,7 @@ export interface TgpuComputePipeline
     bindGroupLayout: TgpuBindGroupLayout<Entries>,
     bindGroup: TgpuBindGroup<Entries>,
   ): this;
+  with(bindGroup: TgpuBindGroup): this;
 
   dispatchWorkgroups(
     x: number,
@@ -122,12 +124,27 @@ class TgpuComputePipelineImpl implements TgpuComputePipeline {
   with<Entries extends Record<string, TgpuLayoutEntry | null>>(
     bindGroupLayout: TgpuBindGroupLayout<Entries>,
     bindGroup: TgpuBindGroup<Entries>,
+  ): this;
+  with(bindGroup: TgpuBindGroup): this;
+  with(
+    layoutOrBindGroup: TgpuBindGroupLayout | TgpuBindGroup,
+    bindGroup?: TgpuBindGroup,
   ): this {
+    if (isBindGroup(layoutOrBindGroup)) {
+      return new TgpuComputePipelineImpl(this._core, {
+        ...this._priors,
+        bindGroupLayoutMap: new Map([
+          ...(this._priors.bindGroupLayoutMap ?? []),
+          [layoutOrBindGroup.layout, layoutOrBindGroup],
+        ]),
+      }) as this;
+    }
+
     return new TgpuComputePipelineImpl(this._core, {
       ...this._priors,
       bindGroupLayoutMap: new Map([
         ...(this._priors.bindGroupLayoutMap ?? []),
-        [bindGroupLayout, bindGroup],
+        [layoutOrBindGroup as TgpuBindGroupLayout, bindGroup as TgpuBindGroup],
       ]),
     }) as this;
   }
--- packages/typegpu/src/core/pipeline/renderPipeline.ts
+++ packages/typegpu/src/core/pipeline/renderPipeline.ts
@@ -26,6 +26,7 @@ import { getName, PERF, setName } from '../../shared/meta.ts';
 import { $getNameForward, $internal, $resolve } from '../../shared/symbols.ts';
 import type { AnyVertexAttribs } from '../../shared/vertexFormat.ts';
 import {
+  isBindGroup,
   isBindGroupLayout,
   type TgpuBindGroup,
   type TgpuBindGroupLayout,
@@ -99,6 +100,7 @@ export interface TgpuRenderPipeline<Output extends IOLayout = IOLayout>
     bindGroupLayout: TgpuBindGroupLayout<Entries>,
     bindGroup: TgpuBindGroup<Entries>,
   ): this;
+  with(bindGroup: TgpuBindGroup): this;
 
   withColorAttachment(
     attachment: FragmentOutToColorAttachment<Output>,
@@ -330,28 +332,45 @@ class TgpuRenderPipelineImpl implements TgpuRenderPipeline {
     bindGroupLayout: TgpuBindGroupLayout,
     bindGroup: TgpuBindGroup,
   ): this;
+  with(bindGroup: TgpuBindGroup): this;
   with(
-    definition: TgpuVertexLayout | TgpuBindGroupLayout,
-    resource: (TgpuBuffer<AnyWgslData> & VertexFlag) | TgpuBindGroup,
+    layoutOrBindGroup:
+      | TgpuVertexLayout
+      | TgpuBindGroupLayout
+      | TgpuBindGroup,
+    resource?: (TgpuBuffer<AnyWgslData> & VertexFlag) | TgpuBindGroup,
   ): this {
     const internals = this[$internal];
 
-    if (isBindGroupLayout(definition)) {
+    if (isBindGroup(layoutOrBindGroup)) {
       return new TgpuRenderPipelineImpl(internals.core, {
         ...internals.priors,
         bindGroupLayoutMap: new Map([
           ...(internals.priors.bindGroupLayoutMap ?? []),
-          [definition, resource as TgpuBindGroup],
+          [layoutOrBindGroup.layout, layoutOrBindGroup],
         ]),
       }) as this;
     }
 
-    if (isVertexLayout(definition)) {
+    if (isBindGroupLayout(layoutOrBindGroup)) {
+      return new TgpuRenderPipelineImpl(internals.core, {
+        ...internals.priors,
+        bindGroupLayoutMap: new Map([
+          ...(internals.priors.bindGroupLayoutMap ?? []),
+          [layoutOrBindGroup, resource as TgpuBindGroup],
+        ]),
+      }) as this;
+    }
+
+    if (isVertexLayout(layoutOrBindGroup)) {
       return new TgpuRenderPipelineImpl(internals.core, {
         ...internals.priors,
         vertexLayoutMap: new Map([
           ...(internals.priors.vertexLayoutMap ?? []),
-          [definition, resource as TgpuBuffer<AnyWgslData> & VertexFlag],
+          [
+            layoutOrBindGroup,
+            resource as TgpuBuffer<AnyWgslData> & VertexFlag,
+          ],
         ]),
       }) as this;
     }
