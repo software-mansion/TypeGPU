16899a53ba248e9c332775a5d85b06cac34458b8
lursz <lukoszr@gmail.com>
2025-10-07 10:38:38 +0200

    🦕

--- apps/typegpu-docs/src/examples/rendering/clouds/consts.ts
+++ apps/typegpu-docs/src/examples/rendering/clouds/consts.ts
@@ -1,8 +1,8 @@
 import * as d from 'typegpu/data';
 import tgpu, {
   type TgpuFn,
-  type TgpuSampledTexture,
   type TgpuSampler,
+  type TgpuTextureView,
 } from 'typegpu';
 
 export const MAX_ITERATIONS = 120; // 50 - 200
@@ -22,7 +22,9 @@ export const SKY = d.vec3f(0.6, 0.45, 0.75);
 export const SUN = d.vec3f(1.0, 0.7, 0.3);
 
 export const raymarchSlot = tgpu.slot<TgpuFn>();
-export const sampledViewSlot = tgpu.slot<TgpuSampledTexture<'2d', d.F32>>();
+export const sampledViewSlot = tgpu.slot<
+  TgpuTextureView<d.WgslTexture2d<d.F32>>
+>();
 export const samplerSlot = tgpu.slot<TgpuSampler>();
 export const timeAccess = tgpu['~unstable'].accessor(d.f32);
 export const resolutionAccess = tgpu['~unstable'].accessor(d.vec2f);
