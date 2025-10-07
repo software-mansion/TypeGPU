91e97513bf7e41d5d5092e6a50b6367249bbfae5
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2025-10-07 16:45:50 +0200

    docs: Phong reflection model (#1781)

---
+++ apps/typegpu-docs/src/examples/rendering/phong-reflection/params.ts
@@ -0,0 +1,12 @@
+import * as d from 'typegpu/data';
+import { ExampleControls } from './schemas.ts';
+
+export const backgroundColor = d.vec3f(28, 28, 28).div(255);
+
+export const initialControls = ExampleControls({
+  lightColor: d.vec3f(1, 0.7, 0),
+  lightDirection: d.vec3f(0, 7, -7),
+  ambientColor: d.vec3f(0.6, 0.6, 0.6),
+  ambientStrength: 0.5,
+  specularExponent: 8,
+});
