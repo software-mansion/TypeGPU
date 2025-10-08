fa182028bae2e0cbf7f3d4f0218b631fe0cad17c
Aleksander Katan <aleksander.katan@swmansion.com>
2025-10-08 12:41:20 +0200

    Fork chroma-keying, rewrite it to tgsl

---
+++ apps/typegpu-docs/src/examples/image-processing/background-segmentation/index.ts
@@ -0,0 +1,249 @@
+import { rgbToYcbcrMatrix } from '@typegpu/color';
+import tgpu from 'typegpu';
+import * as d from 'typegpu/data';
+import * as std from 'typegpu/std';
+
+const rareLayout = tgpu.bindGroupLayout({
+  sampling: { sampler: 'filtering' },
+  color: { uniform: d.vec3f },
+  threshold: { uniform: d.f32 },
+  uvTransform: { uniform: d.mat2x2f },
+});
+
+const frequentLayout = tgpu.bindGroupLayout({
+  inputTexture: { externalTexture: d.textureExternal() },
+});
+
+const VertexOutput = d.struct({
+  position: d.builtin.position,
+  uv: d.location(0, d.vec2f),
+});
+
+const mainVert = tgpu['~unstable'].vertexFn({
+  in: { idx: d.builtin.vertexIndex },
+  out: VertexOutput.propTypes,
+})((input, Out) => {
+  const pos = [
+    d.vec2f(1.0, 1.0),
+    d.vec2f(1.0, -1.0),
+    d.vec2f(-1.0, -1.0),
+    d.vec2f(1.0, 1.0),
+    d.vec2f(-1.0, -1.0),
+    d.vec2f(-1.0, 1.0),
+  ];
+
+  const uv = [
+    d.vec2f(1.0, 0.0),
+    d.vec2f(1.0, 1.0),
+    d.vec2f(0.0, 1.0),
+    d.vec2f(1.0, 0.0),
+    d.vec2f(0.0, 1.0),
+    d.vec2f(0.0, 0.0),
+  ];
+
+  const output = Out();
+  output.position = d.vec4f(pos[input.idx], 0.0, 1.0);
+  output.uv = uv[input.idx];
+  return output;
+});
+
+const mainFrag = tgpu['~unstable'].fragmentFn({
+  in: { uv: d.location(0, d.vec2f) },
+  out: d.vec4f,
+})((input) => {
+  const uv2 = rareLayout.$.uvTransform.mul(input.uv.sub(d.vec2f(0.5))).add(
+    d.vec2f(0.5),
+  );
+  let col = std.textureSampleBaseClampToEdge(
+    frequentLayout.$.inputTexture,
+    rareLayout.$.sampling,
+    uv2,
+  );
+  const ycbcr = col.xyz.mul(rgbToYcbcrMatrix.$);
+  const colycbcr = rareLayout.$.color.mul(rgbToYcbcrMatrix.$);
+
+  const crDiff = std.abs(ycbcr.y - colycbcr.y);
+  const cbDiff = std.abs(ycbcr.z - colycbcr.z);
+  const distance = std.length(d.vec2f(crDiff, cbDiff));
+
+  if (distance < std.pow(rareLayout.$.threshold, 2)) {
+    col = d.vec4f();
+  }
+
+  return col;
+});
+
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const video = document.querySelector('video') as HTMLVideoElement;
+
+if (navigator.mediaDevices.getUserMedia) {
+  video.srcObject = await navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: 'user',
+      width: { ideal: 1280 },
+      height: { ideal: 720 },
+      frameRate: { ideal: 60 },
+    },
+  });
+} else {
+  throw new Error('getUserMedia not supported');
+}
+
+const root = await tgpu.init();
+const device = root.device;
+
+const context = canvas.getContext('webgpu') as GPUCanvasContext;
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+context.configure({
+  device,
+  format: presentationFormat,
+  alphaMode: 'premultiplied',
+});
+
+const thresholdBuffer = root.createBuffer(d.f32, 0.5).$usage('uniform');
+
+const colorBuffer = root
+  .createBuffer(d.vec3f, d.vec3f(0, 1.0, 0))
+  .$usage('uniform');
+
+const uvTransformBuffer = root
+  .createBuffer(d.mat2x2f, d.mat2x2f.identity())
+  .$usage('uniform');
+
+const sampler = device.createSampler({
+  magFilter: 'linear',
+  minFilter: 'linear',
+});
+
+const rareBindGroup = root.createBindGroup(rareLayout, {
+  color: colorBuffer,
+  sampling: sampler,
+  threshold: thresholdBuffer,
+  uvTransform: uvTransformBuffer,
+});
+
+const renderPipeline = root['~unstable']
+  .withVertex(mainVert, {})
+  .withFragment(mainFrag, { format: presentationFormat })
+  .createPipeline();
+
+function onVideoChange(size: { width: number; height: number }) {
+  const aspectRatio = size.width / size.height;
+  video.style.height = `${video.clientWidth / aspectRatio}px`;
+  if (canvas.parentElement) {
+    canvas.parentElement.style.aspectRatio = `${aspectRatio}`;
+    canvas.parentElement.style.height =
+      `min(100cqh, calc(100cqw/(${aspectRatio})))`;
+  }
+}
+
+const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+function setUVTransformForIOS() {
+  const angle = screen.orientation.type;
+
+  let m = d.mat2x2f(1, 0, 0, 1);
+  if (angle === 'portrait-primary') {
+    m = d.mat2x2f(0, -1, 1, 0);
+  } else if (angle === 'portrait-secondary') {
+    m = d.mat2x2f(0, 1, -1, 0);
+  } else if (angle === 'landscape-primary') {
+    m = d.mat2x2f(-1, 0, 0, -1);
+  }
+
+  uvTransformBuffer.write(m);
+}
+
+if (isIOS) {
+  setUVTransformForIOS();
+  window.addEventListener('orientationchange', setUVTransformForIOS);
+}
+
+const renderPassDescriptor: GPURenderPassDescriptor = {
+  colorAttachments: [
+    {
+      view: undefined as unknown as GPUTextureView,
+      clearValue: [1, 1, 1, 1],
+      loadOp: 'clear' as const,
+      storeOp: 'store' as const,
+    },
+  ],
+};
+
+let videoFrameCallbackId: number | undefined;
+let lastFrameSize: { width: number; height: number } | undefined = undefined;
+
+function processVideoFrame(
+  _: number,
+  metadata: VideoFrameCallbackMetadata,
+) {
+  if (video.readyState < 2) {
+    videoFrameCallbackId = video.requestVideoFrameCallback(processVideoFrame);
+    return;
+  }
+
+  const frameWidth = metadata.width;
+  const frameHeight = metadata.height;
+
+  if (
+    !lastFrameSize ||
+    lastFrameSize.width !== frameWidth ||
+    lastFrameSize.height !== frameHeight
+  ) {
+    lastFrameSize = { width: frameWidth, height: frameHeight };
+    onVideoChange(lastFrameSize);
+  }
+
+  (
+    renderPassDescriptor.colorAttachments as [GPURenderPassColorAttachment]
+  )[0].view = context.getCurrentTexture().createView();
+
+  renderPipeline
+    .withColorAttachment(
+      renderPassDescriptor.colorAttachments[0] as GPURenderPassColorAttachment,
+    )
+    .with(rareLayout, rareBindGroup)
+    .with(
+      frequentLayout,
+      root.createBindGroup(frequentLayout, {
+        inputTexture: device.importExternalTexture({ source: video }),
+      }),
+    )
+    .draw(6);
+
+  videoFrameCallbackId = video.requestVideoFrameCallback(processVideoFrame);
+}
+
+videoFrameCallbackId = video.requestVideoFrameCallback(processVideoFrame);
+
+// #region Example controls & Cleanup
+
+export const controls = {
+  color: {
+    onColorChange: (value: readonly [number, number, number]) => {
+      colorBuffer.write(d.vec3f(...value));
+    },
+    initial: [0, 1, 0] as const,
+  },
+  threshold: {
+    initial: 0.1,
+    min: 0,
+    max: 1,
+    step: 0.01,
+    onSliderChange: (value: number) => thresholdBuffer.write(value),
+  },
+};
+
+export function onCleanup() {
+  if (videoFrameCallbackId !== undefined) {
+    video.cancelVideoFrameCallback(videoFrameCallbackId);
+  }
+  if (video.srcObject) {
+    for (const track of (video.srcObject as MediaStream).getTracks()) {
+      track.stop();
+    }
+  }
+  root.destroy();
+}
+
+// #endregion
