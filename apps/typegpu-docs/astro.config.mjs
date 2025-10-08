75bb4c4028812bc5787df502cd7377637db833e2
lursz <lukoszr@gmail.com>
2025-10-08 15:17:08 +0200

    code editor init

--- apps/typegpu-docs/astro.config.mjs
+++ apps/typegpu-docs/astro.config.mjs
@@ -30,6 +30,10 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathJax],
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      wrap: true,
+    },
   },
   vite: {
     // Allowing query params, for invalidation
