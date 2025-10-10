b4520b614dd12e5f61fd908db012a8827eb8d26f
Iwo Plaza <iwoplaza@gmail.com>
2025-10-10 21:14:35 +0200

    Trying better constant handling

--- packages/typegpu/tests/examples/individual/increment.test.ts
+++ packages/typegpu/tests/examples/individual/increment.test.ts
@@ -20,17 +20,24 @@ describe('increment example', () => {
     expect(shaderCodes).toMatchInlineSnapshot(`
       "@group(0) @binding(0) var<uniform> sizeUniform_1: vec3u;
 
-      @group(0) @binding(1) var<storage, read_write> counter_3: u32;
+      const offset_4: vec3f = vec3f(1, 2, 3);
 
-      fn wrappedCallback_2(_arg_0: u32, _arg_1: u32, _arg_2: u32) {
-        counter_3 += 1;
+      @group(0) @binding(1) var<storage, read_write> counter_5: f32;
+
+      fn foo_3() {
+        let off = (&offset_4);
+        counter_5 += (*off).x;
       }
 
-      struct mainCompute_Input_4 {
+      fn wrappedCallback_2(_arg_0: u32, _arg_1: u32, _arg_2: u32) {
+        foo_3();
+      }
+
+      struct mainCompute_Input_6 {
         @builtin(global_invocation_id) id: vec3u,
       }
 
-      @compute @workgroup_size(1, 1, 1) fn mainCompute_0(in: mainCompute_Input_4)  {
+      @compute @workgroup_size(1, 1, 1) fn mainCompute_0(in: mainCompute_Input_6)  {
         if (any(in.id >= sizeUniform_1)) {
           return;
         }
