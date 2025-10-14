0b0caa6023f10ccb796ec2687f4fb7d30af37d3b
Iwo Plaza <iwoplaza@gmail.com>
2025-10-14 16:35:19 +0200

    Update rolldown-browser.ts

--- packages/unplugin-typegpu/src/rolldown-browser.ts
+++ packages/unplugin-typegpu/src/rolldown-browser.ts
@@ -1,8 +1,5 @@
-import type { Options } from './common.ts';
 import { rollUpImpl } from './rollup-impl.ts';
 
-export default (options: Options) => {
-  // The unplugin API is based on the rollup/rolldown APIs, so it
-  // should just be a compatible rolldown plugin.
-  return rollUpImpl(options);
-};
+// The unplugin API is based on the rollup/rolldown APIs, so it
+// should just be a compatible rolldown plugin.
+export default rollUpImpl;
