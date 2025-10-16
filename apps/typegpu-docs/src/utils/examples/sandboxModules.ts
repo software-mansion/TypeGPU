efbebb527ecd5ec05219416a34b50fdf63c4d68e
Iwo Plaza <iwoplaza@gmail.com>
2025-10-16 14:23:43 +0200

    docs: Using rolldown in translator app (#1611)

--- apps/typegpu-docs/src/utils/examples/sandboxModules.ts
+++ apps/typegpu-docs/src/utils/examples/sandboxModules.ts
@@ -4,7 +4,32 @@ import dtsWebGPU from '@webgpu/types/dist/index.d.ts?raw';
 import dtsWgpuMatrix from 'wgpu-matrix/dist/3.x/wgpu-matrix.d.ts?raw';
 
 interface SandboxModuleDefinition {
-  typeDef: { filename?: string; content: string } | { reroute: string[] };
+  typeDef:
+    | { filename?: string; content: string }
+    | { reroute: string };
+  import?:
+    | { filename?: string; content: string }
+    | { reroute: string }
+    | undefined;
+}
+
+function srcFileToModule(
+  [filepath, content]: [string, string],
+  baseUrl: string,
+): [moduleKey: string, moduleDef: SandboxModuleDefinition] {
+  const filename = filepath.replace(baseUrl, '');
+  const def = {
+    filename,
+    content,
+  };
+
+  return [
+    filename,
+    {
+      typeDef: def,
+      import: def,
+    },
+  ] as const;
 }
 
 function dtsFileToModule(
@@ -26,13 +51,16 @@ function dtsFileToModule(
 
 const allPackagesSrcFiles = pipe(
   entries(
-    import.meta.glob('../../../../../packages/*/src/**/*.ts', {
+    import.meta.glob([
+      '../../../../../packages/*/src/**/*.ts',
+      '../../../../../packages/*/package.json',
+    ], {
       query: 'raw',
       eager: true,
       import: 'default',
     }) as Record<string, string>,
   ),
-  map((dtsFile) => dtsFileToModule(dtsFile, '../../../../../packages/')),
+  map((dtsFile) => srcFileToModule(dtsFile, '../../../../../packages/')),
   fromEntries(),
 );
 
@@ -61,21 +89,30 @@ export const SANDBOX_MODULES: Record<string, SandboxModuleDefinition> = {
   'wgpu-matrix': {
     typeDef: { filename: 'wgpu-matrix.d.ts', content: dtsWgpuMatrix },
   },
+  tinyest: {
+    import: { reroute: 'tinyest/src/index.ts' },
+    typeDef: { reroute: 'tinyest/src/index.ts' },
+  },
   typegpu: {
-    typeDef: { reroute: ['typegpu/src/index.ts'] },
+    import: { reroute: 'typegpu/src/index.ts' },
+    typeDef: { reroute: 'typegpu/src/index.ts' },
   },
   'typegpu/data': {
-    typeDef: { reroute: ['typegpu/src/data/index.ts'] },
+    import: { reroute: 'typegpu/src/data/index.ts' },
+    typeDef: { reroute: 'typegpu/src/data/index.ts' },
   },
   'typegpu/std': {
-    typeDef: { reroute: ['typegpu/src/std/index.ts'] },
+    import: { reroute: 'typegpu/src/std/index.ts' },
+    typeDef: { reroute: 'typegpu/src/std/index.ts' },
   },
 
   // Utility modules
   '@typegpu/noise': {
-    typeDef: { reroute: ['typegpu-noise/src/index.ts'] },
+    import: { reroute: 'typegpu-noise/src/index.ts' },
+    typeDef: { reroute: 'typegpu-noise/src/index.ts' },
   },
   '@typegpu/color': {
-    typeDef: { reroute: ['typegpu-color/src/index.ts'] },
+    import: { reroute: 'typegpu-color/src/index.ts' },
+    typeDef: { reroute: 'typegpu-color/src/index.ts' },
   },
 };
