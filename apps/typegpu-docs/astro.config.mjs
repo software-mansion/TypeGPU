79a5061455fecabc91ce45458129d4c8a7a8d4f2
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2025-11-04 17:32:42 +0100

    benchmark github action

--- apps/typegpu-docs/astro.config.mjs
+++ apps/typegpu-docs/astro.config.mjs
@@ -36,6 +36,9 @@ export default defineConfig({
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
   },
+  image: {
+    domains: ['raw.githubusercontent.com'],
+  },
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathJax],
