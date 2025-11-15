f9bcd962e8e6ee8194a934480ec80d9dbc335001
Iwo Plaza <iwoplaza@gmail.com>
2025-11-15 17:42:54 +0100

    Docs for comptime

--- packages/typegpu/src/core/function/comptime.ts
+++ packages/typegpu/src/core/function/comptime.ts
@@ -6,8 +6,28 @@ import { $internal } from '../../shared/symbols.ts';
 import { coerceToSnippet } from '../../tgsl/generationHelpers.ts';
 import { isKnownAtComptime } from '../../types.ts';
 
+/**
+ * Creates a version of `func` that can called safely in a TypeGPU function to
+ * precompute and inject a value into the final shader code.
+ *
+ * Note how the function passed into `comptime` doesn't have to be marked with
+ * 'use gpu'. That's because the function doesn't execute on the GPU, it gets
+ * executed before the shader code gets sent to the GPU.
+ *
+ * @example
+ * ```ts
+ * const injectRand01 = tgpu['~unstable']
+ *   .comptime(() => Math.random());
+ *
+ * const getColor = (diffuse: d.v3f) => {
+ *   'use gpu';
+ *   const albedo = hsvToRgb(injectRand01(), 1, 0.5);
+ *   return albedo.mul(diffuse);
+ * };
+ * ```
+ */
 export function comptime<T extends (...args: never[]) => unknown>(
-  jsImpl: T,
+  func: T,
 ): T {
   const gpuImpl = (...args: MapValueToSnippet<Parameters<T>>) => {
     const argSnippets = args as MapValueToSnippet<Parameters<T>>;
@@ -22,22 +42,20 @@ export function comptime<T extends (...args: never[]) => unknown>(
       );
     }
 
-    return coerceToSnippet(
-      jsImpl(...argSnippets.map((s) => s.value) as never[]),
-    );
+    return coerceToSnippet(func(...argSnippets.map((s) => s.value) as never[]));
   };
 
   const impl = ((...args: Parameters<T>) => {
     if (inCodegenMode()) {
       return gpuImpl(...args as MapValueToSnippet<Parameters<T>>);
     }
-    return jsImpl(...args);
+    return func(...args);
   }) as T;
 
   impl.toString = () => 'comptime';
   Object.defineProperty(impl, $internal, {
     value: {
-      jsImpl,
+      jsImpl: func,
       gpuImpl,
       argConversionHint: 'keep',
     },
