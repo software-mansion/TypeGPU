d250100dd8560b60ed643cf23f340f2be7751444
David Emanuel Luksic <deluksic@gmail.com>
2025-11-19 00:25:37 +0100

    Simplify lines

--- packages/typegpu-geometry/src/lines/caps/wedge.ts
+++ packages/typegpu-geometry/src/lines/caps/wedge.ts
@@ -1,9 +1,9 @@
 import { vec2f } from 'typegpu/data';
-import { normalize, select, sign } from 'typegpu/std';
+import { normalize, sign } from 'typegpu/std';
 import { addMul, cross2d, rot90ccw } from '../../utils.ts';
 import type { JoinInput } from '../types.ts';
 
-export function square(join: JoinInput, joinVertexIndex: number, _maxJoinCount: number) {
+export function wedge(join: JoinInput, joinVertexIndex: number, _maxJoinCount: number) {
   'use gpu';
   if (joinVertexIndex === 0) {
     return vec2f(join.v);
@@ -11,9 +11,5 @@ export function square(join: JoinInput, joinVertexIndex: number, _maxJoinCount: 
   const fw = normalize(join.fw);
   const vert = rot90ccw(fw);
   const sgn = sign(cross2d(fw, join.d));
-  return addMul(
-    join.C.position,
-    select(addMul(fw, vert, sgn), fw, joinVertexIndex > 1),
-    join.C.radius,
-  );
+  return addMul(join.C.position, addMul(fw, vert, sgn), join.C.radius);
 }
