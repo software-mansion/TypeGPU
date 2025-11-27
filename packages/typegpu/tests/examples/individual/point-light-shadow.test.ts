9db4a2a2ee47006e084d8c68ad95838f9ce09505
Konrad Reczko <66403540+reczkok@users.noreply.github.com>
2025-11-27 17:44:56 +0100

    Update packages/typegpu/tests/examples/individual/point-light-shadow.test.ts
    Co-authored-by: Szymon Szulc <103948576+cieplypolar@users.noreply.github.com>

--- packages/typegpu/tests/examples/individual/point-light-shadow.test.ts
+++ packages/typegpu/tests/examples/individual/point-light-shadow.test.ts
@@ -7,7 +7,7 @@ import { it } from '../../utils/extendedIt.ts';
 import { runExampleTest, setupCommonMocks } from '../utils/baseTest.ts';
 import { mockResizeObserver } from '../utils/commonMocks.ts';
 
-describe('perlin noise example', () => {
+describe('point light shadow example', () => {
   setupCommonMocks();
 
   it('should produce valid code', async ({ device }) => {
