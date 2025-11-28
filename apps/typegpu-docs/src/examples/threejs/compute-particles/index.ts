7efe07c005356da3af7a9377c90a7a242d0cb67e
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2025-11-28 14:13:09 +0100

    TSL import

--- apps/typegpu-docs/src/examples/threejs/compute-particles/index.ts
+++ apps/typegpu-docs/src/examples/threejs/compute-particles/index.ts
@@ -3,18 +3,7 @@
  */
 
 import * as THREE from 'three/webgpu';
-import {
-  float,
-  Fn,
-  hash,
-  If,
-  instancedArray,
-  instanceIndex,
-  shapeCircle,
-  uniform,
-  uv,
-  vec3,
-} from 'three/tsl';
+import * as TSL from 'three/tsl';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
@@ -25,11 +14,11 @@ renderer.setClearColor(0X000000);
 await renderer.init();
 
 const particleCount = 200000;
-const gravity = uniform(-.00098);
-const bounce = uniform(.8);
-const friction = uniform(.99);
-const size = uniform(.12);
-const clickPosition = uniform(new THREE.Vector3());
+const gravity = TSL.uniform(-.00098);
+const bounce = TSL.uniform(.8);
+const friction = TSL.uniform(.99);
+const size = TSL.uniform(.12);
+const clickPosition = TSL.uniform(new THREE.Vector3());
 let isOrbitControlsActive = false;
 
 const camera = new THREE.PerspectiveCamera(
@@ -42,38 +31,38 @@ camera.position.set(0, 5, 20);
 
 const scene = new THREE.Scene();
 
-const positions = instancedArray(particleCount, 'vec3');
-const velocities = instancedArray(particleCount, 'vec3');
-const colors = instancedArray(particleCount, 'vec3');
+const positions = TSL.instancedArray(particleCount, 'vec3');
+const velocities = TSL.instancedArray(particleCount, 'vec3');
+const colors = TSL.instancedArray(particleCount, 'vec3');
 const separation = 0.2;
 const amount = Math.sqrt(particleCount);
-const offset = float(amount / 2);
+const offset = TSL.float(amount / 2);
 
-const computeInit = Fn(() => {
-  const position = positions.element(instanceIndex);
-  const color = colors.element(instanceIndex);
+const computeInit = TSL.Fn(() => {
+  const position = positions.element(TSL.instanceIndex);
+  const color = colors.element(TSL.instanceIndex);
 
-  const x = instanceIndex.mod(amount);
-  const z = instanceIndex.div(amount);
+  const x = TSL.instanceIndex.mod(amount);
+  const z = TSL.instanceIndex.div(amount);
 
   position.x = offset.sub(x).mul(separation);
   position.z = offset.sub(z).mul(separation);
 
-  color.x = hash(instanceIndex);
-  color.y = hash(instanceIndex.add(2));
+  color.x = TSL.hash(TSL.instanceIndex);
+  color.y = TSL.hash(TSL.instanceIndex.add(2));
 })().compute(particleCount).setName('Init Particles');
 renderer.compute(computeInit);
 
-const computeUpdate = Fn(() => {
-  const position = positions.element(instanceIndex);
-  const velocity = velocities.element(instanceIndex);
+const computeUpdate = TSL.Fn(() => {
+  const position = positions.element(TSL.instanceIndex);
+  const velocity = velocities.element(TSL.instanceIndex);
 
-  velocity.addAssign(vec3(0.00, gravity, 0.00));
+  velocity.addAssign(TSL.vec3(0.00, gravity, 0.00));
   position.addAssign(velocity);
 
   velocity.mulAssign(friction);
 
-  If(position.y.lessThan(0), () => {
+  TSL.If(position.y.lessThan(0), () => {
     position.y.assign(0);
     velocity.y = velocity.y.negate().mul(bounce);
 
@@ -87,10 +76,10 @@ const computeParticles = computeUpdate().compute(particleCount).setName(
 );
 
 const material = new THREE.SpriteNodeMaterial();
-material.colorNode = uv().mul(colors.element(instanceIndex));
+material.colorNode = TSL.uv().mul(colors.element(TSL.instanceIndex));
 material.positionNode = positions.toAttribute();
 material.scaleNode = size;
-material.opacityNode = shapeCircle();
+material.opacityNode = TSL.shapeCircle();
 material.alphaToCoverage = true;
 material.transparent = true;
 
@@ -114,16 +103,16 @@ scene.add(plane);
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
-const computeHit = Fn(() => {
-  const position = positions.element(instanceIndex);
-  const velocity = velocities.element(instanceIndex);
+const computeHit = TSL.Fn(() => {
+  const position = positions.element(TSL.instanceIndex);
+  const velocity = velocities.element(TSL.instanceIndex);
 
   const dist = position.distance(clickPosition);
   const direction = position.sub(clickPosition).normalize();
-  const distArea = float(3).sub(dist).max(0);
+  const distArea = TSL.float(3).sub(dist).max(0);
 
   const power = distArea.mul(.01);
-  const relativePower = power.mul(hash(instanceIndex).mul(1.5).add(.5));
+  const relativePower = power.mul(TSL.hash(TSL.instanceIndex).mul(1.5).add(.5));
 
   velocity.assign(velocity.add(direction.mul(relativePower)));
 })().compute(particleCount).setName('Hit Particles');
