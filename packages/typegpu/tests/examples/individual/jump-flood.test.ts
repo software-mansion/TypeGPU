0b455539419873de12f504128c449c81dfbf27c4
Konrad Reczko <reczkok@gmail.com>
2025-12-04 17:04:10 +0100

    update tests

--- packages/typegpu/tests/examples/individual/jump-flood.test.ts
+++ packages/typegpu/tests/examples/individual/jump-flood.test.ts
@@ -23,50 +23,52 @@ describe('jump flood example', () => {
 
       @group(1) @binding(0) var writeView_3: texture_storage_2d_array<rgba16float, write>;
 
-      var<private> seed_6: vec2f;
+      @group(0) @binding(1) var<uniform> timeUniform_4: f32;
 
-      fn seed2_5(value: vec2f) {
-        seed_6 = value;
+      var<private> seed_7: vec2f;
+
+      fn seed2_6(value: vec2f) {
+        seed_7 = value;
       }
 
-      fn randSeed2_4(seed: vec2f) {
-        seed2_5(seed);
+      fn randSeed2_5(seed: vec2f) {
+        seed2_6(seed);
       }
 
-      fn item_8() -> f32 {
-        let a = dot(seed_6, vec2f(23.140779495239258, 232.6168975830078));
-        let b = dot(seed_6, vec2f(54.47856521606445, 345.8415222167969));
-        seed_6.x = fract((cos(a) * 136.8168f));
-        seed_6.y = fract((cos(b) * 534.7645f));
-        return seed_6.y;
+      fn item_9() -> f32 {
+        let a = dot(seed_7, vec2f(23.140779495239258, 232.6168975830078));
+        let b = dot(seed_7, vec2f(54.47856521606445, 345.8415222167969));
+        seed_7.x = fract((cos(a) * 136.8168f));
+        seed_7.y = fract((cos(b) * 534.7645f));
+        return seed_7.y;
       }
 
-      fn randFloat01_7() -> f32 {
-        return item_8();
+      fn randFloat01_8() -> f32 {
+        return item_9();
       }
 
-      @group(0) @binding(1) var<uniform> seedThresholdUniform_9: f32;
+      @group(0) @binding(2) var<uniform> seedThresholdUniform_10: f32;
 
-      const palette_10: array<vec3f, 4> = array<vec3f, 4>(vec3f(0.9215686321258545, 0.8117647171020508, 1), vec3f(0.7176470756530762, 0.545098066329956, 0.9803921580314636), vec3f(0.545098066329956, 0.3607843220233917, 0.9647058844566345), vec3f(0.4274509847164154, 0.2666666805744171, 0.9490196108818054));
+      const palette_11: array<vec3f, 4> = array<vec3f, 4>(vec3f(0.9215686321258545, 0.8117647171020508, 1), vec3f(0.7176470756530762, 0.545098066329956, 0.9803921580314636), vec3f(0.545098066329956, 0.3607843220233917, 0.9647058844566345), vec3f(0.4274509847164154, 0.2666666805744171, 0.9490196108818054));
 
       fn wrappedCallback_2(x: u32, y: u32, _arg_2: u32) {
         var size = textureDimensions(writeView_3);
-        randSeed2_4((vec2f(f32(x), f32(y)) / vec2f(size)));
-        let randomVal = randFloat01_7();
-        let isSeed = (randomVal >= seedThresholdUniform_9);
-        let paletteColor = palette_10[u32(floor((randFloat01_7() * 4f)))];
-        var variation = (vec3f((randFloat01_7() - 0.5f), (randFloat01_7() - 0.5f), (randFloat01_7() - 0.5f)) * 0.15);
+        randSeed2_5(((vec2f(f32(x), f32(y)) / vec2f(size)) + timeUniform_4));
+        let randomVal = randFloat01_8();
+        let isSeed = (randomVal >= seedThresholdUniform_10);
+        let paletteColor = palette_11[u32(floor((randFloat01_8() * 4f)))];
+        var variation = (vec3f((randFloat01_8() - 0.5f), (randFloat01_8() - 0.5f), (randFloat01_8() - 0.5f)) * 0.15);
         var color = select(vec4f(), vec4f(saturate((paletteColor + variation)), 1f), isSeed);
         var coord = select(vec2f(-1), (vec2f(f32(x), f32(y)) / vec2f(size)), isSeed);
         textureStore(writeView_3, vec2i(i32(x), i32(y)), 0, color);
         textureStore(writeView_3, vec2i(i32(x), i32(y)), 1, vec4f(coord, 0f, 0f));
       }
 
-      struct mainCompute_Input_11 {
+      struct mainCompute_Input_12 {
         @builtin(global_invocation_id) id: vec3u,
       }
 
-      @compute @workgroup_size(16, 16, 1) fn mainCompute_0(in: mainCompute_Input_11)  {
+      @compute @workgroup_size(16, 16, 1) fn mainCompute_0(in: mainCompute_Input_12)  {
         if (any(in.id >= sizeUniform_1)) {
           return;
         }
@@ -89,16 +91,16 @@ describe('jump flood example', () => {
         return fullScreenTriangle_Output_2(vec4f(pos[in.vertexIndex], 0, 1), uv[in.vertexIndex]);
       }
 
-      @group(1) @binding(0) var floodTexture_4: texture_2d<f32>;
+      @group(0) @binding(0) var floodTexture_4: texture_2d<f32>;
 
-      @group(0) @binding(0) var filteringSampler_5: sampler;
+      @group(0) @binding(1) var sampler_5: sampler;
 
-      struct sampleFrag_Input_6 {
+      struct voronoiFrag_Input_6 {
         @location(0) uv: vec2f,
       }
 
-      @fragment fn sampleFrag_3(_arg_0: sampleFrag_Input_6) -> @location(0) vec4f {
-        return textureSample(floodTexture_4, filteringSampler_5, _arg_0.uv);
+      @fragment fn voronoiFrag_3(_arg_0: voronoiFrag_Input_6) -> @location(0) vec4f {
+        return textureSample(floodTexture_4, sampler_5, _arg_0.uv);
       }
 
       @group(0) @binding(0) var<uniform> sizeUniform_1: vec3u;
