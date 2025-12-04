f840fe5a43b51bedc656295f75df63e32ec66a8a
lursz <lukoszr@gmail.com>
2025-12-04 17:43:35 +0100

    added more models

--- apps/typegpu-docs/src/examples/image-processing/background-segmentation/model.ts
+++ apps/typegpu-docs/src/examples/image-processing/background-segmentation/model.ts
@@ -8,7 +8,7 @@ const modelCache = new Map<string, ArrayBuffer>();
 
 export const MODELS: ModelConfig[] = [
   {
-    name: 'u2netp',
+    name: 'u2netp - [5mb]',
     path: '/TypeGPU/assets/background-segmentation/u2netp.onnx',
     inputName: 'input.1',
     outputName: '1964',
@@ -29,6 +29,54 @@ export const MODELS: ModelConfig[] = [
     ],
     description:
       'MobileNetV2 based lightweight model, light augmentation, 200 epochs',
+  },
+  {
+    name: 'mobileV4 - cutting [7mb]',
+    path:
+      'https://huggingface.co/lursz/bg-segm/resolve/main/mobileNetv4-cutAug/model_91.onnx',
+    inputName: 'input',
+    outputName: 'output',
+    externalData: [
+      {
+        data:
+          'https://huggingface.co/lursz/bg-segm/resolve/main/mobileNetv4-cutAug/model_91.onnx.data',
+        path: 'model_91.onnx.data',
+      },
+    ],
+    description:
+      'MobileNetV4 based lightweight model, was fed only parts of pictures, 200 epochs',
+  },
+  {
+    name: 'mobileV2 - anti-noise [11mb]',
+    path:
+      'https://huggingface.co/lursz/bg-segm/resolve/main/mobileNetv2-heavyAug/model_197.onnx',
+    inputName: 'input',
+    outputName: 'output',
+    externalData: [
+      {
+        data:
+          'https://huggingface.co/lursz/bg-segm/resolve/main/mobileNetv2-heavyAug/model_197.onnx.data',
+        path: 'model_197.onnx.data',
+      },
+    ],
+    description:
+      'MobileNetV2 based lightweight model, endured heavy augmentation, lots of noise and distortion 200 epochs',
+  },
+  {
+    name: 'autoencoder - [17mb]',
+    path:
+      'https://huggingface.co/lursz/bg-segm/resolve/main/autoencoder-heavyAug/model_37.onnx',
+    inputName: 'input',
+    outputName: 'output',
+    externalData: [
+      {
+        data:
+          'https://huggingface.co/lursz/bg-segm/resolve/main/autoencoder-heavyAug/model_37.onnx.data',
+        path: 'model_37.onnx.data',
+      },
+    ],
+    description:
+      'Fully custom autoencoder model, no pretrained backbone, endured heavy augmentation, 200 epochs',
   },
 ];
 
