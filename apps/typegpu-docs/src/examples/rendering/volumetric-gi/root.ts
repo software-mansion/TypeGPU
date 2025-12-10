f9b2e49faed002aff4c3fff1239e14f5166d80d2
Aleksander Katan <aleksander.katan@swmansion.com>
2025-12-10 13:08:40 +0100

    Presentation format

--- apps/typegpu-docs/src/examples/rendering/volumetric-gi/root.ts
+++ apps/typegpu-docs/src/examples/rendering/volumetric-gi/root.ts
@@ -1,7 +1,7 @@
 import tgpu from 'typegpu';
 
 export const root = await tgpu.init();
-export const presentationFormat = 'bgra8unorm' as const; //navigator.gpu.getPreferredCanvasFormat();
+export const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 export const canvas = document.querySelector('canvas') as HTMLCanvasElement;
 export const context = canvas.getContext('webgpu') as GPUCanvasContext;
 
