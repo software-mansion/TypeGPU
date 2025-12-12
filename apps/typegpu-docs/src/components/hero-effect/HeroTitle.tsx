4d20403a6fef8435751637c9c1872d8622d45a2e
Iwo Plaza <iwoplaza@gmail.com>
2025-12-12 16:06:27 +0100

    Text shadow for better readability

--- apps/typegpu-docs/src/components/hero-effect/HeroTitle.tsx
+++ apps/typegpu-docs/src/components/hero-effect/HeroTitle.tsx
@@ -1,7 +1,7 @@
 export function HeroTitle() {
   return (
     <div>
-      <h1 className='text-pretty font-normal text-2xl text-blue-950 md:pr-40 md:text-3xl dark:text-gray-200'>
+      <h1 className='text-pretty font-normal text-2xl text-blue-950 text-shadow-[1px_1px_0px_white,2px_2px_4px_white] md:pr-40 md:text-3xl dark:text-gray-200'>
         A modular and open-ended toolkit for WebGPU, with advanced type
         interface and ability to write shaders in TypeScript.
       </h1>
