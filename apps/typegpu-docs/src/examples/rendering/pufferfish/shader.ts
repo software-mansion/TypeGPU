f6379489888677477c4d8ae05be8d27749604ff9
Iwo Plaza <iwoplaza@gmail.com>
2025-12-16 17:05:24 +0100

    More performant pufferfish

--- apps/typegpu-docs/src/examples/rendering/pufferfish/shader.ts
+++ apps/typegpu-docs/src/examples/rendering/pufferfish/shader.ts
@@ -1,23 +1,7 @@
+import * as sdf from '@typegpu/sdf';
 import tgpu from 'typegpu';
 import * as d from 'typegpu/data';
-import {
-  abs,
-  clamp,
-  cos,
-  cross,
-  dot,
-  length,
-  max,
-  min,
-  mix,
-  normalize,
-  radians,
-  sign,
-  sin,
-  smoothstep,
-  sqrt,
-} from 'typegpu/std';
-import * as sdf from '@typegpu/sdf';
+import * as std from 'typegpu/std';
 
 /**
  * Holds custom parameters that can be sent to the shader via Smelter
@@ -51,7 +35,6 @@ const MAX_STEPS = 1000;
 const MAX_DIST = 30.0;
 const SURF_DIST = 0.001;
 const SPIKES_NUM = 60;
-const SMOOTHNESS_FACTOR = 0.2;
 
 const bodyRadiusAccess = tgpu['~unstable'].accessor(d.f32, 0.6);
 const bodyPositionAccess = tgpu['~unstable'].accessor(
@@ -69,48 +52,48 @@ type Shape = d.Infer<typeof Shape>;
 
 const rotationMatrixY = (angle: number): d.m3x3f => {
   'use gpu';
-  const c = cos(angle);
-  const s = sin(angle);
+  const c = std.cos(angle);
+  const s = std.sin(angle);
 
   return d.mat3x3f(c, 0.0, s, 0.0, 1.0, 0.0, -s, 0.0, c);
 };
 
 const rotationMatrixX = (angle: number): d.m3x3f => {
   'use gpu';
-  const c = cos(angle);
-  const s = sin(angle);
+  const c = std.cos(angle);
+  const s = std.sin(angle);
   return d.mat3x3f(1.0, 0.0, 0.0, 0.0, c, -s, 0.0, s, c);
 };
 
 const rotationMatrixZ = (angle: number): d.m3x3f => {
   'use gpu';
-  const c = cos(angle);
-  const s = sin(angle);
+  const c = std.cos(angle);
+  const s = std.sin(angle);
   return d.mat3x3f(c, -s, 0.0, s, c, 0.0, 0.0, 0.0, 1.0);
 };
 
 const sdEllipsoid = (p: d.v3f, r: d.v3f): number => {
   'use gpu';
-  const k0 = length(p.div(r));
-  const k1 = length(p.div(r.mul(r)));
+  const k0 = std.length(p.div(r));
+  const k1 = std.length(p.div(r.mul(r)));
   return (k0 * (k0 - 1.0)) / k1;
 };
 
 const sdCone = (p: d.v3f, c: d.v2f, h: number): number => {
   'use gpu';
   const q = d.vec2f(c.x / c.y, -1).mul(h);
-  const w = d.vec2f(length(p.xz), p.y);
-  const a = w.sub(q.mul(clamp(dot(w, q) / dot(q, q), 0, 1)));
-  const b = w.sub(q.mul(d.vec2f(clamp(w.x / q.x, 0, 1), 1)));
-  const k = sign(q.y);
-  const dd = min(dot(a, a), dot(b, b));
-  const s = max(k * (w.x * q.y - w.y * q.x), k * (w.y - q.y));
-  return sqrt(dd) * sign(s);
+  const w = d.vec2f(std.length(p.xz), p.y);
+  const a = w.sub(q.mul(std.clamp(std.dot(w, q) / std.dot(q, q), 0, 1)));
+  const b = w.sub(q.mul(d.vec2f(std.clamp(w.x / q.x, 0, 1), 1)));
+  const k = std.sign(q.y);
+  const dd = std.min(std.dot(a, a), std.dot(b, b));
+  const s = std.max(k * (w.x * q.y - w.y * q.x), k * (w.y - q.y));
+  return std.sqrt(dd) * std.sign(s);
 };
 
 const dot2 = (a: d.v3f): number => {
   'use gpu';
-  return dot(a, a);
+  return std.dot(a, a);
 };
 
 const udTriangle = (p: d.v3f, a: d.v3f, b: d.v3f, c: d.v3f): number => {
@@ -121,74 +104,24 @@ const udTriangle = (p: d.v3f, a: d.v3f, b: d.v3f, c: d.v3f): number => {
   const pb = p.sub(b);
   const ac = a.sub(c);
   const pc = p.sub(c);
-  const nor = cross(ba, ac);
-  const condition = sign(dot(cross(ba, nor), pa)) +
-      sign(dot(cross(cb, nor), pb)) +
-      sign(dot(cross(ac, nor), pc)) <
+  const nor = std.cross(ba, ac);
+  const condition = std.sign(std.dot(std.cross(ba, nor), pa)) +
+      std.sign(std.dot(std.cross(cb, nor), pb)) +
+      std.sign(std.dot(std.cross(ac, nor), pc)) <
     2;
   if (condition) {
-    return sqrt(
-      min(
-        min(
-          dot2(ba.mul(clamp(dot(ba, pa) / dot2(ba), 0, 1)).sub(pa)),
-          dot2(cb.mul(clamp(dot(cb, pb) / dot2(cb), 0, 1)).sub(pb)),
+    return std.sqrt(
+      std.min(
+        std.min(
+          dot2(ba.mul(std.clamp(std.dot(ba, pa) / dot2(ba), 0, 1)).sub(pa)),
+          dot2(cb.mul(std.clamp(std.dot(cb, pb) / dot2(cb), 0, 1)).sub(pb)),
         ),
-        dot2(ac.mul(clamp(dot(ac, pc) / dot2(ac), 0, 1)).sub(pc)),
+        dot2(ac.mul(std.clamp(std.dot(ac, pc) / dot2(ac), 0, 1)).sub(pc)),
       ),
     );
   } else {
-    return sqrt((dot(nor, pa) * dot(nor, pa)) / dot2(nor));
+    return std.sqrt((std.dot(nor, pa) * std.dot(nor, pa)) / dot2(nor));
   }
-};
-
-const isPointInSphereCap = (point: d.v3f, capAngle: number): boolean => {
-  'use gpu';
-  const pointDir = normalize(point.sub(bodyPositionAccess.$));
-  const capDir = normalize(d.vec3f(0.0, 0.0, -1.0));
-  const capDot = dot(pointDir, capDir);
-  return capDot > cos(capAngle);
-};
-
-// Golden angle
-const phi = Math.PI * (sqrt(5.0) - 1.0);
-
-const fibonacciSphereDistribution = (index: number): d.v3f => {
-  'use gpu';
-  const y = 1.0 - (d.f32(index) / d.f32(SPIKES_NUM - 1)) * 2.0;
-  const radius = sqrt(1.0 - y * y);
-  const theta = phi * d.f32(index);
-  const x = cos(theta) * radius;
-  const z = sin(theta) * radius;
-  return d.vec3f(x, y, z);
-};
-
-const calcConeRotationMatrix = (p: d.v3f, radii: d.v3f): d.m3x3f => {
-  'use gpu';
-  // The normal of an ellipsoid at point p is normalize(p / (radii*radii))
-  const newY = normalize(p.div(radii.mul(radii)));
-  const worldUp = d.vec3f(0.0, 1.0, 0.0);
-
-  let newZ = d.vec3f();
-  if (abs(newY.y) > 0.999) {
-    newZ = d.vec3f(1, 0, 0);
-  } else {
-    newZ = normalize(cross(newY, worldUp));
-  }
-
-  const newX = normalize(cross(newY, newZ));
-
-  // prettier-ignore
-  return d.mat3x3f(
-    newX.x,
-    newY.x,
-    newZ.x,
-    newX.y,
-    newY.y,
-    newZ.y,
-    newX.z,
-    newY.z,
-    newZ.z,
-  );
 };
 
 const shapeUnion = (a: Shape, b: Shape): Shape => {
@@ -206,10 +139,10 @@ const shapeUnion = (a: Shape, b: Shape): Shape => {
 
 const smoothShapeUnion = (a: Shape, b: Shape, k: number): Shape => {
   'use gpu';
-  const h = max(k - abs(a.dist - b.dist), 0.0) / k;
+  const h = std.max(k - std.abs(a.dist - b.dist), 0.0) / k;
   const m = h * h;
 
-  const dist = min(a.dist, b.dist) - m * k * 0.25;
+  const dist = std.min(a.dist, b.dist) - m * k * 0.25;
 
   let weight = d.f32();
   if (a.dist > b.dist) {
@@ -218,7 +151,7 @@ const smoothShapeUnion = (a: Shape, b: Shape, k: number): Shape => {
     weight = m;
   }
 
-  const color = mix(a.color, b.color, weight);
+  const color = std.mix(a.color, b.color, weight);
 
   return Shape({ color, dist });
 };
@@ -244,48 +177,32 @@ const getPufferfish = (p: d.v3f): Shape => {
   });
 
   // Spikes
-  const spikeAngle = radians(5.0);
-  const spikeConeC = d.vec2f(sin(spikeAngle), cos(spikeAngle));
-  const spikeHeight = uniformsAccess.$.spike_height;
-  const spikeFreeAreaAngle = radians(40.0);
 
-  for (let i = d.i32(0); i < SPIKES_NUM; i++) {
-    const spikeUnitPoint = fibonacciSphereDistribution(i);
-    // Scale unit sphere point to ellipsoid surface
-    const spikeSurfacePoint = spikeUnitPoint.mul(bodyRadii);
-    const spikeWorldPoint = bodyPosition.add(spikeSurfacePoint);
+  const spikeFreq = 10;
+  // Changing the coordinate system into something more cylindrical
+  const yaw = (std.atan2(bodyLocalP.y, bodyLocalP.x) / (Math.PI * 2)) *
+    spikeFreq;
+  const pitch = bodyLocalP.z;
+  const fyaw = std.fract(yaw) - 0.5;
+  const surfDist = std.length(bodyLocalP) - bodyRadius;
 
-    // Skip spikes from face area
-    if (isPointInSphereCap(spikeWorldPoint, spikeFreeAreaAngle)) {
-      continue;
-    }
-
-    // The normal is used to orient the cone outwards
-    const spikeRotation = calcConeRotationMatrix(spikeSurfacePoint, bodyRadii);
-
-    // Position the cone slightly above the surface
-    const spikeNormal = normalize(
-      spikeSurfacePoint.div(bodyRadii.mul(bodyRadii)),
-    );
-    const spikePoint = spikeWorldPoint.add(spikeNormal.mul(spikeHeight));
-    const spikeLocalP = p.sub(spikePoint);
-    const spikeRotatedP = spikeRotation.mul(spikeLocalP);
-
-    const spike = Shape({
-      color: bodyColor,
-      dist: sdCone(spikeRotatedP, spikeConeC, spikeHeight),
-    });
-    fishBody = smoothShapeUnion(fishBody, spike, SMOOTHNESS_FACTOR);
-  }
+  const spikeAngle = std.radians(60.0);
+  const spikeConeC = d.vec2f(std.sin(spikeAngle), std.cos(spikeAngle));
+  const spikeDist = sdCone(
+    d.vec3f(fyaw, surfDist - 0.1, pitch),
+    spikeConeC,
+    0.2,
+  );
+  fishBody.dist = sdf.opSmoothUnion(fishBody.dist, spikeDist, 0.03);
 
   // Fins
   const finLength = 0.2;
   const finYOffset = 0.3;
   const finXOffset = 0.2;
   const finAnimationFreq = -3;
-  const leftFinXOffset = sin(time * finAnimationFreq) * 0.05;
-  const leftFinYOffset = cos(time * finAnimationFreq) * 0.1;
-  const leftFinZTwist = sin(time * finAnimationFreq) * 0.2;
+  const leftFinXOffset = std.sin(time * finAnimationFreq) * 0.05;
+  const leftFinYOffset = std.cos(time * finAnimationFreq) * 0.1;
+  const leftFinZTwist = std.sin(time * finAnimationFreq) * 0.2;
   const leftFinA = d.vec3f(
     bodyPosition.x + (bodyRadii.x + finLength) + leftFinXOffset,
     bodyPosition.y + finYOffset + leftFinYOffset,
@@ -305,9 +222,9 @@ const getPufferfish = (p: d.v3f): Shape => {
     color: bodyColor,
     dist: udTriangle(p, leftFinA, leftFinB, leftFinC) - 0.05,
   });
-  const rightFinXOffset = cos(time * finAnimationFreq) * 0.05;
-  const rightFinYOffset = sin(time * finAnimationFreq) * 0.1;
-  const rightFinZTwist = cos(time * finAnimationFreq) * 0.2;
+  const rightFinXOffset = std.cos(time * finAnimationFreq) * 0.05;
+  const rightFinYOffset = std.sin(time * finAnimationFreq) * 0.1;
+  const rightFinZTwist = std.cos(time * finAnimationFreq) * 0.2;
   const rightFinA = d.vec3f(
     bodyPosition.x - (bodyRadii.x + finLength) + rightFinXOffset,
     bodyPosition.y + finYOffset + rightFinYOffset,
@@ -328,7 +245,7 @@ const getPufferfish = (p: d.v3f): Shape => {
     dist: udTriangle(p, rightFinA, rightFinB, rightFinC) - 0.05,
   });
   const fins = shapeUnion(leftFin, rightFin);
-  fishBody = shapeUnion(fishBody, fins);
+  fishBody = smoothShapeUnion(fishBody, fins, 0.03);
 
   // Recording min distance
   if (fishBody.dist < minPufferfishDist.$) {
@@ -375,7 +292,7 @@ const getNormal = (p: d.v3f): d.v3f => {
     getPufferfish(p.add(d.vec3f(0, 0, e))).dist - dist,
   );
 
-  return normalize(n);
+  return std.normalize(n);
 };
 
 export const fullColorFragment = tgpu['~unstable'].fragmentFn({
@@ -383,7 +300,7 @@ export const fullColorFragment = tgpu['~unstable'].fragmentFn({
   out: d.vec4f,
 })((input) => {
   'use gpu';
-  const uv = input.uv.mul(2).sub(1);
+  const uv = input.uv.mul(2).sub(1).mul(1.5);
   const bodyPosition = bodyPositionAccess.$;
   const faceOval = uniformsAccess.$.face_oval;
   // Ray setup
@@ -411,19 +328,22 @@ export const fullColorFragment = tgpu['~unstable'].fragmentFn({
   if (distance < MAX_DIST) {
     // Hit the pufferfish
     // const localPos = hitPos.sub(bodyPosition);
-    normal = normalize(getNormal(hitPos));
-    const smoothNormal = normalize(hitPos.sub(bodyPosition));
+    normal = std.normalize(getNormal(hitPos));
+    const smoothNormal = std.normalize(hitPos.sub(bodyPosition));
 
     // Face fish mode logic
     const maskOuterThreshold = 0.3;
     const maskInnerThreshold = 0.8;
-    blendFactor = smoothstep(
+    blendFactor = std.smoothstep(
       maskOuterThreshold,
       maskInnerThreshold,
       -smoothNormal.z,
     );
-    textureUV = mix(faceOval.xy, faceOval.zw, faceUv);
+    textureUV = std.mix(faceOval.xy, faceOval.zw, faceUv);
   }
+
+  // TODO: Comment to show the face
+  blendFactor = 0;
 
   // Has to be called in uniform control flow
   const textureColor = sampleFaceTexture(textureUV);
@@ -431,13 +351,13 @@ export const fullColorFragment = tgpu['~unstable'].fragmentFn({
 
   if (distance < MAX_DIST) {
     // Lighting
-    const lightDir = normalize(d.vec3f(0.0, -1, -0.5)); // Light comes from the top
-    const att = clamp(dot(normal, lightDir) * 5, 0, 1);
+    const lightDir = std.normalize(d.vec3f(0.0, -1, -0.5)); // Light comes from the top
+    const att = std.clamp(std.dot(normal, lightDir) * 5, 0, 1);
     const diffuseLight = d.vec3f(att * 0.6);
     const ambientLight = d.vec3f(0.4, 0.45, 0.5);
     const litFishColor = march.color.mul(diffuseLight.add(ambientLight));
 
-    const finalColor = mix(litFishColor, textureColor.xyz, blendFactor);
+    const finalColor = std.mix(litFishColor, textureColor.xyz, blendFactor);
 
     result = d.vec4f(finalColor, 1);
   } else {
@@ -456,7 +376,9 @@ export const fullColorFragment = tgpu['~unstable'].fragmentFn({
 
   // Pulsating when immune
   const immunity = uniformsAccess.$.color.w;
-  result = result.mul(mix(1, sin(timeAccess.$ * 10) * 0.05 + 0.4, immunity));
+  result = result.mul(
+    std.mix(1, std.sin(timeAccess.$ * 10) * 0.05 + 0.4, immunity),
+  );
 
   return result;
 });
