c790d0acf7b27f1823ee5949cc50cc392026e3ec
Sergei Sharabai <sharabai.sergei@gmail.com>
2025-12-22 18:16:58 +0100

    Add latest changes

--- apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/index.ts
+++ apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/index.ts
@@ -1,52 +1,80 @@
 import tgpu from 'typegpu';
 import * as d from 'typegpu/data';
 import * as std from 'typegpu/std';
-import { createBezier } from './bezier';
-
+import { createBezier } from './bezier.ts';
 
 const ease = createBezier(0.18, 0.7, 0.68, 1.03);
-const green = d.vec4f(0.11764705882352941, 0.8392156862745098, 0.5137254901960784, 1);
-const yellow = d.vec4f(0.8392156862745098, 0.6470588235294118, 0.11764705882352941, 1);
-const indigo = d.vec4f(0.3803921568627451, 0.3333333333333333, 0.9607843137254902, 1);
+const green = d.vec4f(
+  0.11764705882352941,
+  0.8392156862745098,
+  0.5137254901960784,
+  1,
+);
+const yellow = d.vec4f(
+  0.8392156862745098,
+  0.6470588235294118,
+  0.11764705882352941,
+  1,
+);
+const indigo = d.vec4f(
+  0.3803921568627451,
+  0.3333333333333333,
+  0.9607843137254902,
+  1,
+);
 
 const colors = [green, yellow, indigo];
 
 const root = await tgpu.init();
 
+const STEP_ROTATION_ANGLE = 60;
+
 function rotate(coordinate: d.v2f, angleInDegrees: number) {
   'use gpu';
-  const angle = angleInDegrees * Math.PI / 180;
+  const angle = (angleInDegrees * Math.PI) / 180;
   const x = coordinate.x;
   const y = coordinate.y;
-  return d.vec2f(x*std.cos(angle) - y*std.sin(angle), x*std.sin(angle) + y*std.cos(angle))
+  return d.vec2f(
+    x * std.cos(angle) - y * std.sin(angle),
+    x * std.sin(angle) + y * std.cos(angle),
+  );
 }
 
-const PositionArray = d.arrayOf(d.vec2f); 
+function interpolateToScaleFactor(angle: number) {
+  'use gpu';
+  return angle / STEP_ROTATION_ANGLE;
+}
+
+const PositionArray = d.arrayOf(d.vec2f);
 
 const TrianglePositions = d.struct({
   positions: PositionArray(6),
 });
 
-
 const originalPositions = PositionArray(3)([
   d.vec2f(-std.sqrt(3), -1),
   d.vec2f(0, 2),
-  d.vec2f(std.sqrt(3), -1)
+  d.vec2f(std.sqrt(3), -1),
 ]);
 
-const pos = originalPositions.map( pos => std.mul(pos, 0.5))
+const pos = originalPositions.map((pos) => std.mul(pos, 0.5));
 
-const pos2 = pos.map(pos => rotate(std.mul(pos, 0.5), 180));
+// const pos2 = pos.map(pos => rotate(std.mul(pos, 0.5), 180));
 
-const trianglePositionBuffer = root.createReadonly(TrianglePositions,
-  { positions: PositionArray(6)([...pos, ...pos2]) });
+const trianglePositionBuffer = root.createReadonly(TrianglePositions, {
+  positions: PositionArray(6)([...pos, ...pos]),
+});
 
 const time = root.createUniform(d.f32);
 
 const mainFragment = tgpu['~unstable'].fragmentFn({
-  in: {color: d.vec4f},
+  in: { color: d.vec4f },
   out: d.vec4f,
 })((input) => {
+  // const angle = std.mod(time.$, STEP_ROTATION_ANGLE);
+  //   const scaleFactor = interpolateToScaleFactor(angle)
+  // console.log("ScaleFactor:",scaleFactor)
+  // console.log("Angle:", angle);
   // randf.seed(time.$);
   // const random = randf.sample();
   // console.log(random);
@@ -55,35 +83,52 @@ const mainFragment = tgpu['~unstable'].fragmentFn({
   // }
   // return yellow;
   return input.color;
-}) 
+});
 
 const wgsl2 = tgpu.resolve([mainFragment]);
 console.log(wgsl2);
 
 const mainVertex = tgpu['~unstable'].vertexFn({
-  in: { vertexIndex: d.builtin.vertexIndex},
-  out: { outPos: d.builtin.position, color: d.vec4f},
-})((input) => {
-  const vertexPosition = trianglePositionBuffer.$.positions[input.vertexIndex];
+  in: { vertexIndex: d.builtin.vertexIndex },
+  out: { outPos: d.builtin.position, color: d.vec4f },
+})(({ vertexIndex }) => {
+  const originalPositions = PositionArray(6)([
+    d.vec2f(-0.5, -0.5),
+    d.vec2f(-0.5, 0.5),
+    d.vec2f(0.5, -0.5),
+    d.vec2f(1, 1),
+    d.vec2f(1, -1),
+    d.vec2f(-1, 1),
+  ]);
+  const vertexPosition = trianglePositionBuffer.$.positions[vertexIndex];
+  // const vertexPosition = originalPositions[vertexIndex];
   let calculatedPosition = d.vec2f(vertexPosition);
-  const k = input.vertexIndex
 
   let color = yellow;
-  if (input.vertexIndex > 2) {
+  if (vertexIndex > 2) {
     color = green;
-    calculatedPosition = rotate(vertexPosition, time.$ )
+    const angle = std.mod(time.$, STEP_ROTATION_ANGLE);
+    const scaleFactor = interpolateToScaleFactor(angle);
+    calculatedPosition = rotate(vertexPosition, angle);
+    calculatedPosition = std.mul(calculatedPosition, scaleFactor);
   }
 
-  return {outPos: d.vec4f(calculatedPosition, 0, 1), color};
+  return { outPos: d.vec4f(calculatedPosition, 0, 1), color };
 });
 
 const maskVertex = tgpu['~unstable'].vertexFn({
-  in: { vertexIndex: d.builtin.vertexIndex},
-  out: { outPos: d.builtin.position},
-})((input) => {
-  const vertexPosition = trianglePositionBuffer.$.positions[input.vertexIndex + 3];
+  in: { vertexIndex: d.builtin.vertexIndex },
+  out: { position: d.builtin.position },
+})(({ vertexIndex }) => {
+  const originalPositions = PositionArray(3)([
+    d.vec2f(-0.5, -0.5),
+    d.vec2f(-0.5, 0.5),
+    d.vec2f(0.5, -0.5),
+  ]);
+  const vertexPosition = originalPositions[vertexIndex];
+  // const vertexPosition = trianglePositionBuffer.$.positions[vertexIndex];
 
-  return {outPos: d.vec4f(vertexPosition, 0, 1)};
+  return { position: d.vec4f(vertexPosition, 0, 1) };
 });
 
 const wgsl = tgpu.resolve([mainVertex]);
@@ -99,58 +144,56 @@ context.configure({
   alphaMode: 'premultiplied',
 });
 
-let depthTexture = root.device.createTexture({
-  size: [canvas.width, canvas.height, 1],
-  format: 'depth24plus',
+let stencilTexture = root.device.createTexture({
+  size: [canvas.width, canvas.height],
+  format: 'stencil8',
   usage: GPUTextureUsage.RENDER_ATTACHMENT,
 });
 
-const maskFragment = tgpu['~unstable'].fragmentFn({
-  out: d.builtin.fragDepth,
-})(() => {
-  return 1.0;
-});
-
-
 const maskPipeline = root['~unstable']
-  .withVertex(maskVertex, {})
-  .withFragment(maskFragment)
+  .withVertex(maskVertex)
   .withDepthStencil({
-    format: 'depth24plus',
-    depthWriteEnabled: true,
-    depthCompare: 'always',
+    format: 'stencil8',
+    stencilFront: {
+      compare: 'always',
+      passOp: 'replace',
+    },
   })
-  .createPipeline();
+  .createPipeline()
+  .withStencilReference(1);
 
 const pipeline = root['~unstable']
-  .withVertex(mainVertex, {})
+  .withVertex(mainVertex)
   .withFragment(mainFragment, { format: presentationFormat })
   .withDepthStencil({
-    format: 'depth24plus',
-    depthWriteEnabled: false,
-    depthCompare: 'less',
+    format: 'stencil8',
+    stencilFront: {
+      compare: 'equal',
+      passOp: 'keep',
+      failOp: 'keep',
+    },
   })
-  .createPipeline();
+  .createPipeline()
+  .withStencilReference(2);
 
 let isRunning = true;
 
 console.log(tgpu.resolve([maskPipeline]));
 
-
 function draw(timestamp: number) {
   if (!isRunning) return;
 
-  time.write((timestamp * 0.01) % 1000);
+  time.write((timestamp * 0.04) % 1000);
 
   maskPipeline
     .withDepthStencilAttachment({
-      view: depthTexture.createView(),
-      depthClearValue: 0,
-      depthLoadOp: 'clear',
-      depthStoreOp: 'store',
+      view: stencilTexture.createView(),
+      stencilClearValue: 0,
+      stencilLoadOp: 'clear',
+      stencilStoreOp: 'store',
     })
-    .draw(3);  
-  
+    .draw(3);
+
   pipeline
     .withColorAttachment({
       view: context.getCurrentTexture().createView(),
@@ -159,9 +202,9 @@ function draw(timestamp: number) {
       storeOp: 'store',
     })
     .withDepthStencilAttachment({
-      view: depthTexture.createView(),
-      depthLoadOp: 'load',
-      depthStoreOp: 'store',
+      view: stencilTexture.createView(),
+      stencilLoadOp: 'load',
+      stencilStoreOp: 'store',
     })
     .draw(6);
 
@@ -170,13 +213,22 @@ function draw(timestamp: number) {
 requestAnimationFrame(draw);
 
 const resizeObserver = new ResizeObserver(() => {
+  stencilTexture.destroy();
 
-  depthTexture.destroy();
-  depthTexture = root.device.createTexture({
-    size: [canvas.width, canvas.height, 1],
-    format: 'depth24plus',
+  stencilTexture = root.device.createTexture({
+    size: [canvas.width, canvas.height],
+    format: 'stencil8',
     usage: GPUTextureUsage.RENDER_ATTACHMENT,
   });
+
+  maskPipeline
+    .withDepthStencilAttachment({
+      view: stencilTexture.createView(),
+      stencilClearValue: 0,
+      stencilLoadOp: 'clear',
+      stencilStoreOp: 'store',
+    })
+    .draw(3);
 });
 resizeObserver.observe(canvas);
 
