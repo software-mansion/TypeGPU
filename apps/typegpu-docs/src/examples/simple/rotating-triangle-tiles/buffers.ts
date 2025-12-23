d88f4a8c1bf55858c851d0d68badbdc25ce5c4f8
Sergei Sharabai <sharabai.sergei@gmail.com>
2025-12-23 17:26:26 +0100

    Add instances

--- apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/buffers.ts
+++ apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/buffers.ts
@@ -1,9 +1,31 @@
 import * as d from 'typegpu/data';
 import { colors } from './geometry.ts';
 import { root } from './root.ts';
+import { triangleVertices } from './geometry.ts';
+import { instanceInfoArray, InstanceInfoArray } from './instanceInfo.ts';
 
-const animationProgress = root.createUniform(d.f32);
+const animationProgressUniform = root.createUniform(d.f32);
 
-const shiftedColors = root.createReadonly(d.arrayOf(d.vec4f, 3), [...colors]);
+const shiftedColorsBuffer = root.createReadonly(d.arrayOf(d.vec4f, 3), [
+  ...colors,
+]);
 
-export { animationProgress, shiftedColors };
+const TriangleVertices = d.struct({
+  positions: d.arrayOf(d.vec2f, 9),
+});
+
+const triangleVerticesBuffer = root.createReadonly(TriangleVertices, {
+  positions: triangleVertices,
+});
+
+const instanceInfoBuffer = root.createReadonly(
+  InstanceInfoArray,
+  instanceInfoArray,
+);
+
+export {
+  animationProgressUniform,
+  shiftedColorsBuffer,
+  instanceInfoBuffer,
+  triangleVerticesBuffer,
+};
