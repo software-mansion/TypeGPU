6d1912c5fd673efabd92ca87bd544accfefdcfcc
Sergei Sharabai <sharabai.sergei@gmail.com>
2026-01-13 14:38:58 +0100

    cleanup refactor

--- apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/params.ts
+++ apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/params.ts
@@ -12,6 +12,41 @@ const INITIAL_STEP_ROTATION = 60;
 const INITIAL_MIDDLE_SQUARE_SCALE = 2;
 const DEFAULT_ROTATION_TO_MIDDLE_SQUARE_SCALE_ARRAY = 2;
 
+let cubicBezierControlPoints: [number, number, number, number] = [
+  0.18,
+  0.7,
+  0.68,
+  1.03,
+];
+
+function getCubicBezierControlPoints() {
+  return cubicBezierControlPoints;
+}
+
+function getCubicBezierControlPointsString() {
+  return cubicBezierControlPoints.map((value) =>
+    String(value).replace(/^0(?=\.)/, '')
+  ).join(', ');
+}
+
+function parseControlPoints(value: string) {
+  const points = value.split(',');
+  return points.map((point, index) => parseOneControlPoint(point, index));
+}
+
+function parseOneControlPoint(value: string, index: number) {
+  const parsedNumber = Number(value);
+  console.log(parsedNumber);
+  if (isNaN(parsedNumber)) {
+    throw Error('Cubic Bezier control point must be a number');
+  }
+  if ((index % 2 === 0) && (parsedNumber < 0 || parsedNumber > 1)) {
+    throw Error('Cubic Bezier control point must be a value between 0 and 1');
+  }
+
+  return parsedNumber;
+}
+
 let aspectRatio = 1;
 
 function updateAspectRatio(width: number, height: number) {
@@ -89,11 +124,14 @@ function updateAnimationDuration(newValue: number) {
 export {
   createGridParams,
   getAnimationDuration,
+  getCubicBezierControlPoints,
+  getCubicBezierControlPointsString,
   GridParams,
   gridParams,
   INIT_TILE_DENSITY,
   INITIAL_MIDDLE_SQUARE_SCALE,
   INITIAL_STEP_ROTATION,
+  parseControlPoints,
   ROTATION_OPTIONS,
   updateAnimationDuration,
   updateAspectRatio,
