ff147917c22c0553c003dcec83d530bdaeb6b1e2
Sergei Sharabai <sharabai.sergei@gmail.com>
2026-01-13 14:38:58 +0100

    cleanup refactor

--- apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/buffers.ts
+++ apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/buffers.ts
@@ -2,13 +2,12 @@ import * as d from 'typegpu/data';
 import tgpu from 'typegpu';
 import { colors } from './geometry.ts';
 import { root } from './root.ts';
-import { triangleVertices } from './geometry.ts';
 import { createInstanceInfoArray, InstanceInfoArray } from './instanceInfo.ts';
 import {
   gridParams,
   INITIAL_MIDDLE_SQUARE_SCALE,
   INITIAL_STEP_ROTATION,
-} from './config.ts';
+} from './params.ts';
 
 const animationProgressUniform = root.createUniform(d.f32);
 
@@ -18,10 +17,6 @@ const shiftedColorsBuffer = root.createReadonly(d.arrayOf(d.vec4f, 3), [
 
 const TriangleVertices = d.struct({
   positions: d.arrayOf(d.vec2f, 9),
-});
-
-const triangleVerticesBuffer = root.createReadonly(TriangleVertices, {
-  positions: triangleVertices,
 });
 
 const instanceInfoLayout = tgpu.bindGroupLayout({
@@ -73,6 +68,5 @@ export {
   scaleBuffer,
   shiftedColorsBuffer,
   stepRotationBuffer,
-  triangleVerticesBuffer,
   updateInstanceInfoBufferAndBindGroup,
 };
--- apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/geometry.ts
+++ apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/geometry.ts
@@ -1,5 +1,6 @@
 import * as d from 'typegpu/data';
 import * as std from 'typegpu/std';
+import tgpu from 'typegpu';
 
 const green = d.vec4f(0.117, 0.839, 0.513, 1);
 const yellow = d.vec4f(0.839, 0.647, 0.117, 1);
@@ -7,29 +8,21 @@ const indigo = d.vec4f(0.38, 0.333, 0.96, 1);
 
 const colors = [green, yellow, indigo];
 
-const originalVertices = [
-  d.vec2f(std.sqrt(3) / 2, -1 / 2),
-  d.vec2f(0, 2 / 2),
-  d.vec2f(-std.sqrt(3) / 2, -1 / 2),
-];
+const originalVertices = tgpu.const(d.arrayOf(d.vec2f, 3), [
+  d.vec2f(std.sqrt(3) / 2, -0.5),
+  d.vec2f(0, 1),
+  d.vec2f(-std.sqrt(3) / 2, -0.5),
+]);
 
 const baseTriangleSide = 3 / 2;
 const baseTriangleCentroidToMidpointLength = 0.5;
 const baseTriangleHeight = std.sqrt(3);
 const baseTriangleHalfHeight = baseTriangleHeight * 0.5;
 
-const halvedVertices = originalVertices.map((pos) => std.mul(pos, 0.5));
-
-const triangleVertices = [
-  ...originalVertices, // the biggest outer static triangle
-  ...originalVertices, // the middle triangle growing from half the size to full size
-  ...halvedVertices, // the smallest triangle growing from zero to half the size
-];
-
 export {
   baseTriangleCentroidToMidpointLength,
   baseTriangleHalfHeight,
   baseTriangleSide,
   colors,
-  triangleVertices,
+  originalVertices,
 };
