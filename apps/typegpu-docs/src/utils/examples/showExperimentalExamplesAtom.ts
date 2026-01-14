deff251d323e2a0ff18c7f97f5acd34a0e8aaed5
Konrad Reczko <reczkok@gmail.com>
2026-01-14 17:04:18 +0100

    clean up state

--- apps/typegpu-docs/src/utils/examples/showExperimentalExamplesAtom.ts
+++
@@ -1,8 +0,0 @@
-import { atomWithStorage } from 'jotai/utils';
-
-export const experimentalExamplesShownAtom = atomWithStorage(
-  'experimental-examples-shown',
-  true,
-  undefined,
-  { getOnInit: true },
-);
