111f05bab3a0aa9879a9cd47b8ffc93316204a20
lursz <lukoszr@gmail.com>
2026-01-14 15:36:21 +0100

    gol resolution test

--- packages/typegpu/tests/examples/individual/game-of-life.test.ts
+++ packages/typegpu/tests/examples/individual/game-of-life.test.ts
@@ -17,72 +17,76 @@ describe('game of life example', () => {
     }, device);
 
     expect(shaderCodes).toMatchInlineSnapshot(`
-      "@group(0) @binding(0) var<storage, read> size: vec2u;
-
-      @group(0) @binding(1) var<storage, read> current: array<u32>;
-
-      @group(0) @binding(2) var<storage, read_write> next: array<u32>;
-      override blockSize = 8;
+      "@group(0) @binding(0) var<uniform> sizeUniform: vec3u;
 
       fn getIndex(x: u32, y: u32) -> u32 {
-        let h = size.y;
-        let w = size.x;
-
-        return (y % h) * w + (x % w);
+        return (((y % 64u) * 64u) + (x % 64u));
       }
+
+      @group(1) @binding(0) var<storage, read> current: array<u32>;
 
       fn getCell(x: u32, y: u32) -> u32 {
         return current[getIndex(x, y)];
       }
 
       fn countNeighbors(x: u32, y: u32) -> u32 {
-        return getCell(x - 1, y - 1) + getCell(x, y - 1) + getCell(x + 1, y - 1) +
-               getCell(x - 1, y) +                         getCell(x + 1, y) +
-               getCell(x - 1, y + 1) + getCell(x, y + 1) + getCell(x + 1, y + 1);
+        return (((((((getCell((x - 1u), (y - 1u)) + getCell(x, (y - 1u))) + getCell((x + 1u), (y - 1u))) + getCell((x - 1u), y)) + getCell((x + 1u), y)) + getCell((x - 1u), (y + 1u))) + getCell(x, (y + 1u))) + getCell((x + 1u), (y + 1u)));
       }
 
-      @compute @workgroup_size(blockSize, blockSize)
-      fn main(@builtin(global_invocation_id) grid: vec3u) {
-        let x = grid.x;
-        let y = grid.y;
+      @group(1) @binding(1) var<storage, read_write> next: array<u32>;
+
+      fn wrappedCallback(x: u32, y: u32, _arg_2: u32) {
         let n = countNeighbors(x, y);
-        next[getIndex(x, y)] = select(u32(n == 3u), u32(n == 2u || n == 3u), getCell(x, y) == 1u);
+        next[getIndex(x, y)] = u32(select((n == 3u), ((n == 2u) || (n == 3u)), (getCell(x, y) == 1u)));
       }
 
+      struct mainCompute_Input {
+        @builtin(global_invocation_id) id: vec3u,
+      }
 
-      @group(0) @binding(0) var<uniform> size: vec2u;
-      struct Out {
+      @compute @workgroup_size(16, 16, 1) fn mainCompute(in: mainCompute_Input)  {
+        if (any(in.id >= sizeUniform)) {
+          return;
+        }
+        wrappedCallback(in.id.x, in.id.y, in.id.z);
+      }
+
+      struct vertexFn_Output {
         @builtin(position) pos: vec4f,
-        @location(0) cell: f32,
+        @location(0) @interpolate(flat) cell: u32,
         @location(1) uv: vec2f,
       }
 
-      @vertex
-      fn vert(@builtin(instance_index) i: u32, @location(0) cell: u32, @location(1) pos: vec2u) -> Out {
-        let w = size.x;
-        let h = size.y;
-        let x = (f32(i % w + pos.x) / f32(w) - 0.5) * 2. * f32(w) / f32(max(w, h));
-        let y = (f32((i - (i % w)) / w + pos.y) / f32(h) - 0.5) * 2. * f32(h) / f32(max(w, h));
-
-        return Out(
-          vec4f(x, y, 0., 1.),
-          f32(cell),
-          vec2f((x + 1) / 2, (y + 1) / 2)
-        );
+      struct vertexFn_Input {
+        @builtin(instance_index) iid: u32,
+        @location(0) cell: u32,
+        @location(1) pos: vec2u,
       }
 
-      @fragment
-      fn frag(@location(0) cell: f32, @builtin(position) pos: vec4f, @location(1) uv: vec2f) -> @location(0) vec4f {
-        if (cell == 0.) {
-          discard;
-        }
+      @vertex fn vertexFn(_arg_0: vertexFn_Input) -> vertexFn_Output {
+        const w = 64u;
+        const h = 64u;
+        let col = (_arg_0.iid % w);
+        let row = u32((f32(_arg_0.iid) / f32(w)));
+        let gx = (col + _arg_0.pos.x);
+        let gy = (row + _arg_0.pos.y);
+        let maxWH = f32(max(w, h));
+        let x = (((f32(gx) * 2f) - f32(w)) / maxWH);
+        let y = (((f32(gy) * 2f) - f32(h)) / maxWH);
+        return vertexFn_Output(vec4f(x, y, 0f, 1f), _arg_0.cell, vec2f(((x + 1f) * 0.5f), ((y + 1f) * 0.5f)));
+      }
 
-        return vec4f(
-          uv.x / 1.5,
-          uv.y / 1.5,
-          1 - uv.x / 1.5,
-          0.8
-        );
+      struct fragmentFn_Input {
+        @location(0) @interpolate(flat) cell: u32,
+        @location(1) uv: vec2f,
+      }
+
+      @fragment fn fragmentFn(_arg_0: fragmentFn_Input) -> @location(0) vec4f {
+        if ((_arg_0.cell == 0u)) {
+          discard;;
+        }
+        var u = (_arg_0.uv / 1.5);
+        return vec4f(u.x, u.y, (1f - u.x), 0.8f);
       }"
     `);
   });
