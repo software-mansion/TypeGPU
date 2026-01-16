6cc683f1189bfcd9b2b039487728da6f9531d4dc
Iwo Plaza <iwoplaza@gmail.com>
2026-01-16 07:08:08 +0100

    Review fixes

--- packages/typegpu/src/core/slot/slotTypes.ts
+++ packages/typegpu/src/core/slot/slotTypes.ts
@@ -4,7 +4,7 @@ import type { TgpuNamable } from '../../shared/meta.ts';
 import type { GPUValueOf, Infer, InferGPU } from '../../shared/repr.ts';
 import { $gpuValueOf, $internal, $providing } from '../../shared/symbols.ts';
 import type { TgpuBufferShorthand } from '../buffer/bufferShorthand.ts';
-import { TgpuConst } from '../constant/tgpuConstant.ts';
+import type { TgpuConst } from '../constant/tgpuConstant.ts';
 import type { TgpuTextureView } from '../texture/texture.ts';
 import type { TgpuVar, VariableScope } from '../variable/tgpuVariable.ts';
 import type { TgpuBufferUsage } from './../buffer/bufferUsage.ts';
@@ -64,7 +64,6 @@ export type AccessorIn<TSchema extends AnyData> = TSchema extends
 export type MutableAccessorIn<TSchema extends AnyData> = TSchema extends
   WgslTexture | WgslStorageTexture ? (
     | (() => Infer<TSchema> | TgpuTextureView<TSchema>)
-    | Infer<TSchema>
     | TgpuTextureView<TSchema>
   )
   : (
