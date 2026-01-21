5ee25a35cb995fc80291337574fd5fb8a030f33c
Aleksander Katan <aleksander.katan@swmansion.com>
2026-01-21 12:30:12 +0100

    Use canvas.width in function visualizer

--- apps/typegpu-docs/src/examples/rendering/function-visualizer/index.ts
+++ apps/typegpu-docs/src/examples/rendering/function-visualizer/index.ts
@@ -190,10 +190,7 @@ const renderBackgroundPipeline = device.createRenderPipeline({
 });
 
 let msTexture = device.createTexture({
-  size: [
-    canvas.clientWidth * window.devicePixelRatio,
-    canvas.clientHeight * window.devicePixelRatio,
-  ],
+  size: [canvas.width, canvas.height],
   sampleCount: 4,
   format: presentationFormat,
   usage: GPUTextureUsage.RENDER_ATTACHMENT,
@@ -547,10 +544,7 @@ const resizeObserver = new ResizeObserver(() => {
 
   msTexture.destroy();
   msTexture = device.createTexture({
-    size: [
-      canvas.clientWidth * window.devicePixelRatio,
-      canvas.clientHeight * window.devicePixelRatio,
-    ],
+    size: [canvas.width, canvas.height],
     sampleCount: 4,
     format: presentationFormat,
     usage: GPUTextureUsage.RENDER_ATTACHMENT,
