adb679984ee65dd6fc6e1f7c305c298fffd7e779
Aleksander Katan <aleksander.katan@swmansion.com>
2026-01-23 12:57:23 +0100

    Add all rules

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,3 +1,35 @@
-import { plugin } from './plugin.ts';
+import fs from 'node:fs';
+import type { TSESLint } from '@typescript-eslint/utils';
+import { allRules, recommendedRules, rules } from './configs.ts';
 
-export default plugin;
+const pkg = JSON.parse(
+  fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8'),
+);
+
+const pluginBase: TSESLint.FlatConfig.Plugin = {
+  meta: {
+    name: pkg.name,
+    version: pkg.version,
+  },
+  rules,
+};
+
+const recommended: TSESLint.FlatConfig.Config = {
+  name: 'typegpu/recommended',
+  plugins: { typegpu: pluginBase },
+  rules: recommendedRules,
+};
+
+const all: TSESLint.FlatConfig.Config = {
+  name: 'typegpu/all',
+  plugins: { typegpu: pluginBase },
+  rules: allRules,
+};
+
+export const plugin: TSESLint.FlatConfig.Plugin = {
+  ...pluginBase,
+  configs: {
+    recommended,
+    all,
+  },
+};
