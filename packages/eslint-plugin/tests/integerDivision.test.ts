320563bbffe58af172fbe2a117cfa4cafe960366
Aleksander Katan <aleksander.katan@swmansion.com>
2026-01-23 10:21:59 +0100

    Add a describe to please vitest gods

--- packages/eslint-plugin/tests/integerDivision.test.ts
+++ packages/eslint-plugin/tests/integerDivision.test.ts
@@ -1,25 +1,28 @@
+import { describe } from 'vitest';
 import { integerDivision } from '../src/rules/integerDivision.ts';
 import { ruleTester } from './ruleTester.ts';
 
-ruleTester.run('integerDivision', integerDivision, {
-  valid: [
-    '1 / 2',
-    'd.u32(1) / 2',
-    '1 / d.u32(2)',
-    'd.u32(d.u32(1) / d.u32(2))',
-  ],
-  invalid: [
-    {
-      code: 'd.u32(1) / d.u32(2)',
-      errors: [
-        { messageId: 'intDiv', data: { snippet: 'd.u32(1) / d.u32(2)' } },
-      ],
-    },
-    {
-      code: 'd.i32(1) / d.i32(2)',
-      errors: [
-        { messageId: 'intDiv', data: { snippet: 'd.i32(1) / d.i32(2)' } },
-      ],
-    },
-  ],
+describe('integerDivision', () => {
+  ruleTester.run('integerDivision', integerDivision, {
+    valid: [
+      '1 / 2',
+      'd.u32(1) / 2',
+      '1 / d.u32(2)',
+      'd.u32(d.u32(1) / d.u32(2))',
+    ],
+    invalid: [
+      {
+        code: 'd.u32(1) / d.u32(2)',
+        errors: [
+          { messageId: 'intDiv', data: { snippet: 'd.u32(1) / d.u32(2)' } },
+        ],
+      },
+      {
+        code: 'd.i32(1) / d.i32(2)',
+        errors: [
+          { messageId: 'intDiv', data: { snippet: 'd.i32(1) / d.i32(2)' } },
+        ],
+      },
+    ],
+  });
 });
