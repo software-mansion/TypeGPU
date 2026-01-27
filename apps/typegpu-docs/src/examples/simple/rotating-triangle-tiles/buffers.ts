025a9fb2ad31f36ecb0e44a315b63c586ceeb213
Sergei Sharabai <sharabai.sergei@gmail.com>
2026-01-27 11:25:37 +0100

    refactor to create buffers once inside index, remove console.logs, recreate test file

--- apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/buffers.ts
+++ apps/typegpu-docs/src/examples/simple/rotating-triangle-tiles/buffers.ts
@@ -1,7 +1,6 @@
 import * as d from 'typegpu/data';
-import tgpu from 'typegpu';
+import tgpu, { type TgpuRoot } from 'typegpu';
 import { colors } from './geometry.ts';
-import { root } from './root.ts';
 import { createInstanceInfoArray, InstanceInfoArray } from './instanceInfo.ts';
 import {
   getGridParams,
@@ -9,60 +8,81 @@ import {
   INITIAL_STEP_ROTATION,
 } from './params.ts';
 
-const animationProgressUniform = root.createUniform(d.f32);
-
-const shiftedColorsBuffer = root.createReadonly(d.arrayOf(d.vec4f, 3), [
-  ...colors,
-]);
+const stepRotationAccess = tgpu['~unstable'].accessor(d.f32);
+const shiftedColorsAccess = tgpu['~unstable'].accessor(d.arrayOf(d.vec4f, 3));
+const animationProgressAccess = tgpu['~unstable'].accessor(d.f32);
+const middleSquareScaleAccess = tgpu['~unstable'].accessor(d.f32);
+const drawOverNeighborsAccess = tgpu['~unstable'].accessor(d.u32);
+const scaleAccess = tgpu['~unstable'].accessor(d.f32);
+const aspectRatioAccess = tgpu['~unstable'].accessor(d.f32);
 
 const instanceInfoLayout = tgpu.bindGroupLayout({
   instanceInfo: { storage: InstanceInfoArray },
 });
 
-let instanceInfoBindGroup = createInstanceInfoBufferAndBindGroup();
+function createBuffers(root: TgpuRoot) {
+  const animationProgressUniform = root.createUniform(d.f32);
 
-function getInstanceInfoBindGroup() {
-  return instanceInfoBindGroup;
-}
+  const shiftedColorsBuffer = root.createReadonly(d.arrayOf(d.vec4f, 3), [
+    ...colors,
+  ]);
 
-function createInstanceInfoBufferAndBindGroup() {
-  const instanceInfoBuffer = root.createReadonly(
-    InstanceInfoArray(getGridParams().triangleCount),
-    createInstanceInfoArray(),
+  let instanceInfoBindGroup = createInstanceInfoBufferAndBindGroup();
+
+  function getInstanceInfoBindGroup() {
+    return instanceInfoBindGroup;
+  }
+
+  function createInstanceInfoBufferAndBindGroup() {
+    const instanceInfoBuffer = root.createReadonly(
+      InstanceInfoArray(getGridParams().triangleCount),
+      createInstanceInfoArray(),
+    );
+
+    const instanceInfoBindGroup = root.createBindGroup(instanceInfoLayout, {
+      instanceInfo: instanceInfoBuffer.buffer,
+    });
+
+    return instanceInfoBindGroup;
+  }
+
+  function updateInstanceInfoBufferAndBindGroup() {
+    instanceInfoBindGroup = createInstanceInfoBufferAndBindGroup();
+  }
+
+  const scaleBuffer = root.createUniform(d.f32, getGridParams().tileDensity);
+  const aspectRatioBuffer = root.createUniform(d.f32, 1);
+
+  const stepRotationBuffer = root.createUniform(d.f32, INITIAL_STEP_ROTATION);
+
+  const middleSquareScaleBuffer = root.createUniform(
+    d.f32,
+    INITIAL_MIDDLE_SQUARE_SCALE,
   );
 
-  const instanceInfoBindGroup = root.createBindGroup(instanceInfoLayout, {
-    instanceInfo: instanceInfoBuffer.buffer,
-  });
+  const drawOverNeighborsBuffer = root.createUniform(d.u32, 0);
 
-  return instanceInfoBindGroup;
+  return {
+    animationProgressUniform,
+    aspectRatioBuffer,
+    drawOverNeighborsBuffer,
+    getInstanceInfoBindGroup,
+    middleSquareScaleBuffer,
+    scaleBuffer,
+    shiftedColorsBuffer,
+    stepRotationBuffer,
+    updateInstanceInfoBufferAndBindGroup,
+  };
 }
-
-function updateInstanceInfoBufferAndBindGroup() {
-  instanceInfoBindGroup = createInstanceInfoBufferAndBindGroup();
-}
-
-const scaleBuffer = root.createUniform(d.f32, getGridParams().tileDensity);
-const aspectRatioBuffer = root.createUniform(d.f32, 1);
-
-const stepRotationBuffer = root.createUniform(d.f32, INITIAL_STEP_ROTATION);
-
-const middleSquareScaleBuffer = root.createUniform(
-  d.f32,
-  INITIAL_MIDDLE_SQUARE_SCALE,
-);
-
-const drawOverNeighborsBuffer = root.createUniform(d.u32, 0);
 
 export {
-  animationProgressUniform,
-  aspectRatioBuffer,
-  drawOverNeighborsBuffer,
-  getInstanceInfoBindGroup,
+  animationProgressAccess,
+  aspectRatioAccess,
+  createBuffers,
+  drawOverNeighborsAccess,
   instanceInfoLayout,
-  middleSquareScaleBuffer,
-  scaleBuffer,
-  shiftedColorsBuffer,
-  stepRotationBuffer,
-  updateInstanceInfoBufferAndBindGroup,
+  middleSquareScaleAccess,
+  scaleAccess,
+  shiftedColorsAccess,
+  stepRotationAccess,
 };
