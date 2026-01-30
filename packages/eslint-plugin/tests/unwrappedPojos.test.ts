f5a83743879a08707746b83a2b2b41958e204424
Aleksander Katan <aleksander.katan@swmansion.com>
2026-01-30 11:35:29 +0100

    Add a base for the rule

---
+++ packages/eslint-plugin/tests/unwrappedPojos.test.ts
@@ -0,0 +1,23 @@
+import { describe } from 'vitest';
+import { ruleTester } from './ruleTester.ts';
+import { unwrappedPojos } from '../src/rules/unwrappedPojos.ts';
+
+describe('unwrappedPojos', () => {
+  ruleTester.run('unwrappedPojos', unwrappedPojos, {
+    valid: [
+      'const pojo = { a: 1 };',
+      "() => { 'use gpu'; const wrapped = Schema({ a: 1 }); }",
+    ],
+    invalid: [
+      {
+        code: "() => { 'use gpu'; const pojo = { a: 1 }; }",
+        errors: [
+          {
+            messageId: 'unwrappedPojo',
+            data: { snippet: '{ a: 1 }' },
+          },
+        ],
+      },
+    ],
+  });
+});
