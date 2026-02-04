b88d41cdb8c2e1427bca0c247998ced9d7adcea4
Aleksander Katan <aleksander.katan@swmansion.com>
2026-02-04 14:42:10 +0100

    Add types.ts to common

---
+++ apps/typegpu-docs/src/examples/common/types.ts
@@ -0,0 +1,55 @@
+type SelectControlParam<T> = {
+  initial?: T;
+  options: readonly T[];
+  onSelectChange: (newValue: T) => void;
+};
+
+type ToggleControlParam = {
+  initial?: boolean;
+  onToggleChange: (newValue: boolean) => void;
+};
+
+type SliderControlParam = {
+  initial?: number;
+  min?: number;
+  max?: number;
+  step?: number;
+  onSliderChange: (newValue: number) => void;
+};
+
+type VectorSliderControlParam = {
+  initial?: readonly number[];
+  min: readonly number[];
+  max: readonly number[];
+  step: readonly number[];
+  onVectorSliderChange: (newValue: number[]) => void;
+};
+
+type ColorPickerControlParam = {
+  initial?: readonly number[];
+  onColorChange: (newValue: readonly [number, number, number]) => void;
+};
+
+type ButtonControlParam = {
+  onButtonClick: (() => void) | (() => Promise<void>);
+};
+
+type TextAreaControlParam = {
+  initial?: string;
+  onTextChange: (newValue: string) => void;
+};
+
+export function defineControls<T extends Record<string, unknown>>(
+  controls: {
+    [Key in keyof T]:
+      | SelectControlParam<T[Key]>
+      | ToggleControlParam
+      | SliderControlParam
+      | VectorSliderControlParam
+      | ColorPickerControlParam
+      | ButtonControlParam
+      | TextAreaControlParam;
+  },
+) {
+  return controls;
+}
