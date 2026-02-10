9ccb7d4ab9c09c789e0b20198782874416c03451
Aleksander Katan <aleksander.katan@swmansion.com>
2026-02-10 16:11:39 +0100

    Add math rule

---
+++ packages/eslint-plugin/src/rules/math.ts
@@ -0,0 +1,41 @@
+import { createRule } from '../ruleCreator.ts';
+import { enhanceRule } from '../enhanceRule.ts';
+import { directiveTracking } from '../enhancers/directiveTracking.ts';
+
+export const math = createRule({
+  name: 'math',
+  meta: {
+    type: 'suggestion',
+    docs: { description: `Avoid dividing numbers wrapped in 'u32' and 'i32'.` },
+    messages: {
+      math:
+        "Using Math methods, such as '{{snippet}}', is not advised, and may not work. Use 'std' instead.",
+    },
+    schema: [],
+  },
+  defaultOptions: [],
+
+  create: enhanceRule({ directives: directiveTracking }, (context, state) => {
+    const { directives } = state;
+
+    return {
+      CallExpression(node) {
+        if (!directives.insideUseGpu()) {
+          return;
+        }
+
+        if (
+          node.callee.type === 'MemberExpression' &&
+          node.callee.object.type === 'Identifier' &&
+          node.callee.object.name === 'Math'
+        ) {
+          context.report({
+            node,
+            messageId: 'math',
+            data: { snippet: context.sourceCode.getText(node) },
+          });
+        }
+      },
+    };
+  }),
+});
