d14a742c1ad9e35b8cdd8f1f038f21624a090f1b
Konrad Reczko <reczkok@gmail.com>
2026-02-13 21:22:34 +0100

    update test

--- packages/typegpu/tests/examples/individual/bitonic-sort.test.ts
+++ packages/typegpu/tests/examples/individual/bitonic-sort.test.ts
@@ -21,71 +21,7 @@ describe('bitonic sort example', () => {
     );
 
     expect(shaderCodes).toMatchInlineSnapshot(`
-      "struct fullScreenTriangle_Input {
-        @builtin(vertex_index) vertexIndex: u32,
-      }
-
-      struct fullScreenTriangle_Output {
-        @builtin(position) pos: vec4f,
-        @location(0) uv: vec2f,
-      }
-
-      @vertex fn fullScreenTriangle(in: fullScreenTriangle_Input) -> fullScreenTriangle_Output {
-        const pos = array<vec2f, 3>(vec2f(-1, -1), vec2f(3, -1), vec2f(-1, 3));
-        const uv = array<vec2f, 3>(vec2f(0, 1), vec2f(2, 1), vec2f(0, -1));
-
-        return fullScreenTriangle_Output(vec4f(pos[in.vertexIndex], 0, 1), uv[in.vertexIndex]);
-      }
-
-      @group(0) @binding(0) var<storage, read> data: array<u32>;
-
-      struct fragmentFn_Input {
-        @location(0) uv: vec2f,
-      }
-
-      @fragment fn fragmentFn(input: fragmentFn_Input) -> @location(0) vec4f {
-        let arrayLength_1 = arrayLength(&data);
-        const maxValue = 255u;
-        let cols = u32(ceil(sqrt(f32(arrayLength_1))));
-        let rows = u32(ceil((f32(arrayLength_1) / f32(cols))));
-        let col = u32(floor((input.uv.x * f32(cols))));
-        let row = u32(floor((input.uv.y * f32(rows))));
-        let idx = ((row * cols) + col);
-        if ((idx >= arrayLength_1)) {
-          return vec4f(0.10000000149011612, 0.10000000149011612, 0.10000000149011612, 1);
-        }
-        let value = data[idx];
-        let normalized = (f32(value) / f32(maxValue));
-        return vec4f(normalized, normalized, normalized, 1f);
-      }
-
-      struct copyParamsType {
-        srcLength: u32,
-        dstLength: u32,
-        paddingValue: u32,
-      }
-
-      @group(0) @binding(2) var<uniform> params: copyParamsType;
-
-      @group(0) @binding(1) var<storage, read_write> dst: array<u32>;
-
-      @group(0) @binding(0) var<storage, read> src: array<u32>;
-
-      struct copyPadKernel_Input {
-        @builtin(global_invocation_id) gid: vec3u,
-      }
-
-      @compute @workgroup_size(256) fn copyPadKernel(input: copyPadKernel_Input) {
-        let idx = input.gid.x;
-        let dstLength = params.dstLength;
-        let srcLength = params.srcLength;
-        if ((idx >= dstLength)) {
-          return;
-        }
-        dst[idx] = select(params.paddingValue, src[idx], (idx < srcLength));
-      }
-
-      struct sortUniformsType {
+      "struct sortUniformsType {
         k: u32,
         jShift: u32,
       }
@@ -127,27 +63,42 @@ describe('bitonic sort example', () => {
         }
       }
 
-      struct copyParamsType {
-        srcLength: u32,
-        dstLength: u32,
-        paddingValue: u32,
+      struct fullScreenTriangle_Input {
+        @builtin(vertex_index) vertexIndex: u32,
       }
 
-      @group(0) @binding(2) var<uniform> params: copyParamsType;
-
-      @group(0) @binding(1) var<storage, read_write> dst: array<u32>;
-
-      @group(0) @binding(0) var<storage, read> src: array<u32>;
-
-      struct copyBackKernel_Input {
-        @builtin(global_invocation_id) gid: vec3u,
+      struct fullScreenTriangle_Output {
+        @builtin(position) pos: vec4f,
+        @location(0) uv: vec2f,
       }
 
-      @compute @workgroup_size(256) fn copyBackKernel(input: copyBackKernel_Input) {
-        let idx = input.gid.x;
-        if ((idx < params.srcLength)) {
-          dst[idx] = src[idx];
+      @vertex fn fullScreenTriangle(in: fullScreenTriangle_Input) -> fullScreenTriangle_Output {
+        const pos = array<vec2f, 3>(vec2f(-1, -1), vec2f(3, -1), vec2f(-1, 3));
+        const uv = array<vec2f, 3>(vec2f(0, 1), vec2f(2, 1), vec2f(0, -1));
+
+        return fullScreenTriangle_Output(vec4f(pos[in.vertexIndex], 0, 1), uv[in.vertexIndex]);
+      }
+
+      @group(0) @binding(0) var<storage, read> data: array<u32>;
+
+      struct fragmentFn_Input {
+        @location(0) uv: vec2f,
+      }
+
+      @fragment fn fragmentFn(input: fragmentFn_Input) -> @location(0) vec4f {
+        let arrayLength_1 = arrayLength(&data);
+        const maxValue = 255u;
+        let cols = u32(ceil(sqrt(f32(arrayLength_1))));
+        let rows = u32(ceil((f32(arrayLength_1) / f32(cols))));
+        let col = u32(floor((input.uv.x * f32(cols))));
+        let row = u32(floor((input.uv.y * f32(rows))));
+        let idx = ((row * cols) + col);
+        if ((idx >= arrayLength_1)) {
+          return vec4f(0.10000000149011612, 0.10000000149011612, 0.10000000149011612, 1);
         }
+        let value = data[idx];
+        let normalized = (f32(value) / f32(maxValue));
+        return vec4f(normalized, normalized, normalized, 1f);
       }"
     `);
   });
