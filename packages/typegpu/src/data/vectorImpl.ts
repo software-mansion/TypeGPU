4795fd401ada99659ea03c33823bf5ded12d685e
Iwo Plaza <iwoplaza@gmail.com>
2026-02-13 20:29:15 +0100

    feat: `tsover` support

--- packages/typegpu/src/data/vectorImpl.ts
+++ packages/typegpu/src/data/vectorImpl.ts
@@ -1,3 +1,4 @@
+import { Operator } from 'tsover-runtime';
 import { $internal, $resolve } from '../shared/symbols.ts';
 import type { SelfResolvable } from '../types.ts';
 import { bool, f16, f32, i32, u32 } from './numeric.ts';
@@ -176,6 +177,13 @@ abstract class Vec2<S> extends VecBase<S> implements Tuple2<S> {
 
   set g(value: S) {
     this[1] = this.castElement()(value);
+  }
+
+  [Operator.plus](lhs: Vec2<S>, rhs: Vec2<S>) {
+    return new this._Vec2(
+      (lhs[0] as number) + (rhs[0] as number) as S,
+      (lhs[1] as number) + (rhs[1] as number) as S,
+    );
   }
 }
 
