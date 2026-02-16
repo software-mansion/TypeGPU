025d1bc32971488d6b69f2e55f49a5ede4644856
Konrad Reczko <66403540+reczkok@users.noreply.github.com>
2026-02-16 14:53:47 +0100

    simplify (#2169)

--- apps/typegpu-docs/src/examples/algorithms/concurrent-chart/calculator.ts
+++ apps/typegpu-docs/src/examples/algorithms/concurrent-chart/calculator.ts
@@ -1,15 +1,36 @@
 import { initCache, prefixScan } from '@typegpu/concurrent-scan';
-import { compareArrayWithBuffer, concurrentSumOnJS } from './utils.ts';
 import * as d from 'typegpu/data';
 import type { TgpuRoot } from 'typegpu';
 import * as std from 'typegpu/std';
 
 type SumResult = {
   success: boolean;
-  jsTime?: number;
-  gpuTime?: number;
-  gpuShaderTime?: number;
+  jsTime: number;
+  gpuTime: number;
+  gpuShaderTime: number;
 };
+
+function prefixSumOnJS(arr: number[]) {
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] += arr[i - 1];
+  }
+  // In Blelloch scan, the result starts with identity element
+  arr.unshift(0);
+  arr.pop();
+  return arr;
+}
+
+function arraysEqual(a: number[], b: number[]): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 export async function performCalculationsWithTime(
   root: TgpuRoot,
@@ -17,18 +38,16 @@ export async function performCalculationsWithTime(
 ): Promise<SumResult> {
   const arraySize = inputArray.length;
   const inputBuffer = root
-    .createBuffer(
-      d.arrayOf(d.f32, arraySize),
-    )
+    .createBuffer(d.arrayOf(d.f32, arraySize))
     .$usage('storage');
   inputBuffer.write(inputArray);
 
-  // JS Version
+  // JS version
   const jsStartTime = performance.now();
-  const jsResult = concurrentSumOnJS(inputArray);
+  const jsResult = prefixSumOnJS(inputArray);
   const jsTime = performance.now() - jsStartTime;
 
-  // GPU Version
+  // GPU version
   initCache(root, { operation: std.add, identityElement: 0 });
   const querySet = root.createQuerySet('timestamp', 2);
   const gpuStartTime = performance.now();
@@ -45,13 +64,13 @@ export async function performCalculationsWithTime(
   querySet.resolve();
   await root.device.queue.onSubmittedWorkDone();
   const gpuTime = performance.now() - gpuStartTime;
-  // Compare results
+
   const gpuResult = await calcResult.read();
   const timestamps = await querySet.read();
   const gpuShaderTime = Number(timestamps[1] - timestamps[0]) / 1_000_000;
-  const isEqual = compareArrayWithBuffer(jsResult, gpuResult);
+
   return {
-    success: isEqual,
+    success: arraysEqual(jsResult, gpuResult),
     jsTime,
     gpuTime,
     gpuShaderTime,
