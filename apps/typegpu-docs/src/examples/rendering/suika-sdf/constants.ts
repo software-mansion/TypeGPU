9adce8ebb8cbe85b6ec636c796980eb37bbf7434
Konrad Reczko <reczkok@gmail.com>
2026-02-19 00:53:48 +0100

    move to proper atlas (tex array)

--- apps/typegpu-docs/src/examples/rendering/suika-sdf/constants.ts
+++ apps/typegpu-docs/src/examples/rendering/suika-sdf/constants.ts
@@ -3,7 +3,6 @@ import { d } from 'typegpu';
 export const LEVEL_RADII = [0.12, 0.16, 0.2, 0.24, 0.28, 0.32, 0.36, 0.4, 0.44, 0.48]
   .map((x) => x * 1.3);
 export const LEVEL_COUNT = LEVEL_RADII.length;
-export const LEVEL_SCALE = 1 / LEVEL_COUNT;
 export const MAX_LEVEL_RADIUS = LEVEL_RADII[LEVEL_COUNT - 1];
 
 export const WALL_DEFS = [
