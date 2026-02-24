3f0efa303daf9ec795921b9359aeb3bd15792724
Iwo Plaza <iwoplaza@gmail.com>
2026-02-24 18:14:37 +0100

    More maintainable version

--- packages/typegpu/src/index.js
+++ packages/typegpu/src/index.js
@@ -8,36 +8,4 @@ import * as tgpu from './tgpu.ts';
 export * as tgpu from './tgpu.ts';
 export default tgpu;
 
-export * as d from './data/index.ts';
-export * as std from './std/index.ts';
-export * as common from './common/index.ts';
-
-export {
-  MissingBindGroupsError,
-  MissingLinksError,
-  MissingSlotValueError,
-  MissingVertexBuffersError,
-  NotUniformError,
-  ResolutionError,
-} from './errors.ts';
-export { isBuffer, isUsableAsVertex } from './core/buffer/buffer.ts';
-export {
-  isAccessor,
-  isLazy,
-  isMutableAccessor,
-  isSlot,
-} from './core/slot/slotTypes.ts';
-export { isComparisonSampler, isSampler } from './core/sampler/sampler.ts';
-export { isTexture } from './core/texture/texture.ts';
-export {
-  isUsableAsRender,
-  isUsableAsSampled,
-} from './core/texture/usageExtension.ts';
-export { isUsableAsStorage } from './extension.ts';
-export { isUsableAsUniform } from './core/buffer/bufferUsage.ts';
-export { isBufferShorthand } from './core/buffer/bufferShorthand.ts';
-export { isTgpuFn } from './core/function/tgpuFn.ts';
-export { isTgpuFragmentFn } from './core/function/tgpuFragmentFn.ts';
-export { isTgpuVertexFn } from './core/function/tgpuVertexFn.ts';
-export { isTgpuComputeFn } from './core/function/tgpuComputeFn.ts';
-export { isVariable } from './core/variable/tgpuVariable.ts';
+export * from './indexNamedExports.ts';
