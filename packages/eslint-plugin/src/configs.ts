77f352437014fc09105886d426265046dac6a3db
Aleksander Katan <aleksander.katan@swmansion.com>
2026-02-27 17:26:39 +0100

    Add rule to config

--- packages/eslint-plugin/src/configs.ts
+++ packages/eslint-plugin/src/configs.ts
@@ -1,10 +1,12 @@
 import type { TSESLint } from '@typescript-eslint/utils';
 import { integerDivision } from './rules/integerDivision.ts';
 import { unwrappedPojos } from './rules/unwrappedPojos.ts';
+import { invalidAssignment } from './rules/invalidAssignment.ts';
 
 export const rules = {
   'integer-division': integerDivision,
   'unwrapped-pojo': unwrappedPojos,
+  'invalid-assignment': invalidAssignment,
 } as const;
 
 type Rules = Record<
@@ -15,9 +17,11 @@ type Rules = Record<
 export const recommendedRules: Rules = {
   'typegpu/integer-division': 'warn',
   'typegpu/unwrapped-pojo': 'warn',
+  'typegpu/invalid-assignment': 'warn',
 };
 
 export const allRules: Rules = {
   'typegpu/integer-division': 'error',
   'typegpu/unwrapped-pojo': 'error',
+  'typegpu/invalid-assignment': 'error',
 };
