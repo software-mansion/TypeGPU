397b9cbc995484698fe67664449b5555acc5c29b
Aleksander Katan <aleksander.katan@swmansion.com>
2026-03-02 11:50:47 +0100

    Handle JS assignment

--- packages/eslint-plugin/src/rules/invalidAssignment.ts
+++ packages/eslint-plugin/src/rules/invalidAssignment.ts
@@ -13,6 +13,8 @@ export const invalidAssignment = createRule({
     messages: {
       parameterAssignment:
         "Cannot assign to '{{snippet}}' since WGSL parameters are immutable. If you're using d.ref, please either use '.$' or disable this rule.",
+      jsAssignment:
+        "Cannot assign to '{{snippet}}' since it is a JS variable defined outside of the current TypeGPU function's scope. Use buffers, workgroup variables or local variables instead.",
     },
     schema: [],
   },
@@ -66,7 +68,8 @@ export const invalidAssignment = createRule({
       },
 
       AssignmentExpression(node) {
-        if (!directives.getEnclosingTypegpuFunction()) {
+        const enclosingFn = directives.getEnclosingTypegpuFunction();
+        if (!enclosingFn) {
           return;
         }
 
@@ -87,17 +90,30 @@ export const invalidAssignment = createRule({
         }
 
         // look for a scope that contains at least one
-        const defs = ASTUtils.findVariable(
+        const variable = ASTUtils.findVariable(
           context.sourceCode.getScope(assignee),
           assignee.name,
-        )?.defs;
+        );
 
-        if (defs && defs.length > 0) {
+        // TODO: handle variables with no defs (globalThis, Math etc.)
+        if (variable?.defs && variable.defs[0]) {
           // def[0] points to the correct scope
           // defs is an array because there may be multiple definitions with `var`
-          const def = defs[0];
+          const def = variable.defs[0];
 
-          if (def?.type === 'Parameter') {
+          // we check if it was defined outside of current function by checking ranges
+          if (
+            def.node.range[1] < enclosingFn.range[0] ||
+            enclosingFn.range[1] < def.node.range[0]
+          ) {
+            context.report({
+              messageId: 'jsAssignment',
+              node,
+              data: { snippet: context.sourceCode.getText(node.left) },
+            });
+          }
+
+          if (def.type === 'Parameter') {
             // either 'use gpu' or other parameter, either way invalid
             context.report({
               messageId: 'parameterAssignment',
