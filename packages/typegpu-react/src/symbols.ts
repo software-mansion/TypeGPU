e01251a0fe49fbcf8e37d5570ab3cd24f9c0c584
Iwo Plaza <iwoplaza@gmail.com>
2026-03-12 13:02:32 +0100

    useBindGroup

---
+++ packages/typegpu-react/src/symbols.ts
@@ -0,0 +1,6 @@
+// The version is inlined during build-time 🎉
+// It helps us identify problems when two versions of
+// @typegpu/react are used at the same time.
+import { version } from '../package.json';
+
+export const $buffer = Symbol(`@typegpu/react:${version}:$buffer`);
