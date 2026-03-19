e22c128e694b556d4f8946da46c5464dd06c4243
Aleksander Katan <aleksander.katan@swmansion.com>
2026-03-19 11:43:40 +0100

    Move the setup to editor component

--- apps/typegpu-docs/src/components/ExamplePage.tsx
+++ apps/typegpu-docs/src/components/ExamplePage.tsx
@@ -5,42 +5,6 @@ import { common, examples } from '../examples/exampleContent.ts';
 import { ExampleNotFound } from './ExampleNotFound.tsx';
 import { ExampleView } from './ExampleView.tsx';
 
-// This setup is required for tsover to work, because monaco-react won't use custom monaco without `loader.config()`
-// Config docs: https://www.npmjs.com/package/@monaco-editor/react?activeTab=readme#loader-config
-// Integration docs: https://github.com/microsoft/monaco-editor/blob/main/docs/integrate-esm.md
-import { loader } from '@monaco-editor/react';
-import * as monaco from 'tsover-monaco-editor';
-// oxlint-disable-next-line import/default
-import editorWorker from 'tsover-monaco-editor/esm/vs/editor/editor.worker?worker';
-// oxlint-disable-next-line import/default
-import jsonWorker from 'tsover-monaco-editor/esm/vs/language/json/json.worker?worker';
-// oxlint-disable-next-line import/default
-import cssWorker from 'tsover-monaco-editor/esm/vs/language/css/css.worker?worker';
-// oxlint-disable-next-line import/default
-import htmlWorker from 'tsover-monaco-editor/esm/vs/language/html/html.worker?worker';
-// oxlint-disable-next-line import/default
-import tsWorker from 'tsover-monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-
-self.MonacoEnvironment = {
-  getWorker(_, label) {
-    switch (label) {
-      case 'json':
-        return new jsonWorker();
-      case 'css':
-        return new cssWorker();
-      case 'html':
-        return new htmlWorker();
-      case 'typescript':
-      case 'javascript':
-        return new tsWorker();
-      default:
-        return new editorWorker();
-    }
-  },
-};
-
-loader.config({ monaco });
-
 const getRandomExampleKey = () => {
   const keys = Object.keys(examples);
   const randomIdx = Math.floor(Math.random() * keys.length);
