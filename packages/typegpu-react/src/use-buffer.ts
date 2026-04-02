fe444a3cd8546a289fa9f1e0d3ff07e57b48da35
Iwo Plaza <iwoplaza@gmail.com>
2026-04-02 14:41:04 +0200

    Recreating buffer when schema or root changes

--- packages/typegpu-react/src/use-buffer.ts
+++ packages/typegpu-react/src/use-buffer.ts
@@ -1,8 +1,7 @@
-import type * as d from 'typegpu/data';
-import { useRoot } from './root-context.tsx';
 import { useState } from 'react';
-import type { TgpuBuffer, ValidateBufferSchema } from 'typegpu';
-import { useDeferredCleanup } from './helper-hooks.ts';
+import type { d, TgpuBuffer, ValidateBufferSchema } from 'typegpu';
+import { useChangeDetection, useDeferredCleanup, useStableSchema } from './helper-hooks.ts';
+import { useRoot } from './root-context.tsx';
 
 export interface UseBufferOptions<TSchema extends d.AnyData> {
   initial?:
@@ -11,23 +10,30 @@ export interface UseBufferOptions<TSchema extends d.AnyData> {
   onInit?: (buffer: TgpuBuffer<TSchema>) => void;
 }
 
-// TODO: Recreate the buffer when the schema changes
 export function useBuffer<TSchema extends d.AnyData>(
-  schema: ValidateBufferSchema<TSchema>,
+  _schema: ValidateBufferSchema<TSchema>,
   options?: UseBufferOptions<TSchema>,
 ): TgpuBuffer<TSchema> {
   const { initial, onInit } = options ?? {};
   const root = useRoot();
 
   const [fakeState] = useState(() => {
-    // TODO: The cast to any should not be necessary
-    const buffer = root.createBuffer(schema, initial);
+    const buffer = root.createBuffer(_schema, initial);
     onInit?.(buffer);
 
     return {
       buffer,
     };
   });
+
+  const [schema, schemaChanged] = useStableSchema(_schema);
+  const rootChanged = useChangeDetection(root);
+
+  if (schemaChanged || rootChanged) {
+    fakeState.buffer.destroy();
+    fakeState.buffer = root.createBuffer(schema, initial);
+    onInit?.(fakeState.buffer);
+  }
 
   useDeferredCleanup(() => {
     fakeState.buffer.destroy();
