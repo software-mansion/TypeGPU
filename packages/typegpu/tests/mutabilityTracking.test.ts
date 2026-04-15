8f27d44688c71869ed8e7e25340ce1d2e70ff17e
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-04-15 11:54:39 +0200

    Add reference tests

--- packages/typegpu/tests/mutabilityTracking.test.ts
+++ packages/typegpu/tests/mutabilityTracking.test.ts
@@ -19,7 +19,7 @@ describe('mutability tracking', () => {
       expect(resolved).toMatchInlineSnapshot(`
         "fn item(arg: vec4u) -> u32 {
           var a = arg.x;
-          a += 1u;
+          a = 1u;
           return a;
         }"
       `);
@@ -40,7 +40,9 @@ describe('mutability tracking', () => {
       expect(resolved).toMatchInlineSnapshot(`
         "fn item(arg: vec4u) -> u32 {
           var a = arg.x;
-          a += 1u;
+          if ((a < 1u)) {
+            a = 1u;
+          }
           return a;
         }"
       `);
@@ -78,7 +80,7 @@ describe('mutability tracking', () => {
       expect(resolved).toMatchInlineSnapshot(`
         "fn item(arg: vec4u) -> u32 {
           var a = arg.x;
-          a += 1u;
+          a++;
           return a;
         }"
       `);
@@ -116,7 +118,7 @@ describe('mutability tracking', () => {
       expect(resolved).toMatchInlineSnapshot(`
         "fn item(arg: vec4u) -> u32 {
           var a = arg;
-          a += 1;
+          a.x = 1u;
           return a.x;
         }"
       `);
@@ -212,5 +214,85 @@ describe('mutability tracking', () => {
     expect(resolved).toContain('let a = arg');
     expect(resolved).toContain('var a = arg');
     expect(resolved).toMatchInlineSnapshot();
+  });
+
+  describe('references', () => {
+    it('resolves a primitive variable used with d.ref to var', () => {
+      const fn = fnShell((arg) => {
+        'use gpu';
+        const a = d.ref(0);
+        return a.$;
+      });
+
+      const resolved = tgpu.resolve([fn]);
+      expect(resolved).toContain('var a = 0');
+      expect(resolved).toMatchInlineSnapshot(`
+        "fn item(arg: vec4u) -> u32 {
+          var a = 0;
+          return u32(a);
+        }"
+      `);
+    });
+
+    it('resolves a complex variable used with d.ref to var', () => {
+      const fn = () => {
+        'use gpu';
+        const a = d.ref(d.vec4u());
+        return a.$.x;
+      };
+
+      const resolved = tgpu.resolve([fn]);
+      expect(resolved).toContain('var a = vec4u()');
+      expect(resolved).toMatchInlineSnapshot(`
+        "fn fn_1() -> u32 {
+          var a = vec4u();
+          return a.x;
+        }"
+      `);
+    });
+
+    it('resolves a struct to var when its prop is referenced', () => {
+      const Struct = d.struct({ prop: d.vec4f });
+      const fn = () => {
+        'use gpu';
+        const struct = Struct();
+        const prop = struct.prop;
+      };
+
+      const resolved = tgpu.resolve([fn]);
+      expect(resolved).toContain('var struct_1 = Struct()');
+      expect(resolved).toMatchInlineSnapshot(`
+        "struct Struct {
+          prop: vec4f,
+        }
+
+        fn fn_1() {
+          var struct_1 = Struct();
+          let prop = (&struct_1.prop);
+        }"
+      `);
+    });
+
+    it('resolves a struct to let when its prop is only read', () => {
+      const Struct = d.struct({ prop: d.vec4f });
+      const fn = () => {
+        'use gpu';
+        const struct = Struct();
+        return struct.prop;
+      };
+
+      const resolved = tgpu.resolve([fn]);
+      expect(resolved).toContain('let struct_1 = Struct()');
+      expect(resolved).toMatchInlineSnapshot(`
+        "struct Struct {
+          prop: vec4f,
+        }
+
+        fn fn_1() -> vec4f {
+          var struct_1 = Struct();
+          return struct_1.prop;
+        }"
+      `);
+    });
   });
 });
