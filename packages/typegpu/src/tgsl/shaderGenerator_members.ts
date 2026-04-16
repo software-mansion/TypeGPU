c7e9ac4d2212ac439aac40503a7f0e112e145554
Iwo Plaza <iwoplaza@gmail.com>
2026-04-16 12:56:34 +0200

    chore: Use public API in test utilities (extractSnippet, ...)

--- packages/typegpu/src/tgsl/shaderGenerator_members.ts
+++ packages/typegpu/src/tgsl/shaderGenerator_members.ts
@@ -2,3 +2,5 @@ export { UnknownData } from '../data/dataTypes.ts';
 
 // types
 export type { ResolutionCtx } from '../types.ts';
+export type { Snippet } from '../data/snippet.ts';
+export type { Origin } from '../data/snippet.ts';
