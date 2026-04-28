6f470356f699d5fc9667f9a936ebaf622c0f1a36
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-04-28 16:02:16 +0200

    Add rollup tests

--- packages/unplugin-typegpu/test/nested-externals.test.ts
+++ packages/unplugin-typegpu/test/nested-externals.test.ts
@@ -10,25 +10,71 @@ function extractExternals(code: string | undefined | null) {
   return code.slice(startIndex, endIndex).trim();
 }
 
+const codes = {
+  'allows multiple usages of one external': `\
+    const ext = {
+      value: 7,
+      config: {
+        multiplier: 1,
+        zero: 0,
+      }
+    };
+    const foo = () => {
+      'use gpu';
+      const a = ext.value;
+      const b = ext.config.multiplier;
+      const c = ext.config.zero;
+      const d = ext.config.multiplier;
+    };
+    console.log(foo);`,
+  // ---
+  'treats dereference like a regular external': `\
+    import tgpu, { d } from 'typegpu';
+
+    const root = await tgpu.init();
+    const buffer = root.createMutable(d.vec2u);
+    const foo = () => {
+      'use gpu';
+      const a = buffer.$.x;
+    };
+    console.log(foo);`,
+  // ---
+  'skips computed prop access': `\
+    const ext = {
+      n: 1,
+    };
+
+    const fn = () => {
+      'use gpu';
+      const a = ext['n'];
+    };
+    console.log(fn);`,
+  // ---
+  'skips calls': `\
+    import tgpu, { d } from 'typegpu';
+
+    const ext = {
+      comptime: tgpu.comptime(() => {
+        return d.vec4f();
+      }),
+      runtime: () => {
+        'use gpu';
+        return d.vec4f();
+      },
+    };
+
+    const fn = () => {
+      'use gpu';
+      const a = ext.comptime().x;
+      const b = ext.runtime().y;
+    };
+    console.log(fn);`,
+};
+
 describe('externals gathering', () => {
   describe('BABEL', () => {
     it('allows multiple usages of one external', () => {
-      const code = `\
-        const ext = {
-          value: 7,
-          config: {
-            multiplier: 1,
-            zero: 0,
-          }
-        };
-        const foo = () => {
-          'use gpu';
-          const a = ext.value;
-          const b = ext.config.multiplier;
-          const c = ext.config.zero;
-          const d = ext.config.multiplier;
-        };
-        console.log(foo);`;
+      const code = codes['allows multiple usages of one external'];
 
       expect(extractExternals(babelTransform(code))).toMatchInlineSnapshot(`
         "() => {
@@ -40,16 +86,7 @@ describe('externals gathering', () => {
     });
 
     it('treats dereference like a regular external', () => {
-      const code = `\
-        import tgpu, { d } from 'typegpu';
-
-        const root = await tgpu.init();
-        const buffer = root.createMutable(d.vec2u);
-        const foo = () => {
-          'use gpu';
-          const a = buffer.$.x;
-        };
-        console.log(foo);`;
+      const code = codes['treats dereference like a regular external'];
 
       expect(extractExternals(babelTransform(code))).toMatchInlineSnapshot(`
         "() => {
@@ -61,16 +98,7 @@ describe('externals gathering', () => {
     });
 
     it('skips computed prop access', () => {
-      const code = `\
-        const ext = {
-          n: 1,
-        };
-
-        const fn = () => {
-          'use gpu';
-          const a = ext['n'];
-        };
-        console.log(fn);`;
+      const code = codes['skips computed prop access'];
 
       expect(extractExternals(babelTransform(code))).toMatchInlineSnapshot(`
         "() => {
@@ -82,24 +110,7 @@ describe('externals gathering', () => {
     });
 
     it('skips calls', () => {
-      const code = `\
-        import tgpu, { d } from 'typegpu';
-
-        const ext = {
-          comptime: tgpu.comptime(() => {
-            return d.vec4f();
-          }),
-          runtime: () => {
-            'use gpu';
-            return d.vec4f();
-          },
-        };
-
-        const fn = () => {
-          'use gpu';
-          const a = ext.comptime().x;
-          const b = ext.runtime().y;
-        };`;
+      const code = codes['skips calls'];
 
       expect(extractExternals(babelTransform(code))).toMatchInlineSnapshot(`
         "() => {
@@ -113,9 +124,33 @@ describe('externals gathering', () => {
 
   describe('ROLLUP', () => {
     it('allows multiple usages of one external', async () => {
-      const code = ``;
+      const code = codes['allows multiple usages of one external'];
 
-      expect(extractExternals(await rollupTransform(code))).toMatchInlineSnapshot(`""`);
+      expect(extractExternals(await rollupTransform(code))).toMatchInlineSnapshot(
+        `"() => ({ext}),"`,
+      );
+    });
+
+    it('treats dereference like a regular external', async () => {
+      const code = codes['treats dereference like a regular external'];
+
+      expect(extractExternals(await rollupTransform(code))).toMatchInlineSnapshot(
+        `"() => ({buffer}),"`,
+      );
+    });
+
+    it('skips computed prop access', async () => {
+      const code = codes['skips computed prop access'];
+
+      expect(extractExternals(await rollupTransform(code))).toMatchInlineSnapshot(
+        `"() => ({ext}),"`,
+      );
+    });
+
+    it('skips calls', async () => {
+      const code = codes['skips calls'];
+
+      expect(extractExternals(await rollupTransform(code))).toMatchInlineSnapshot(`"() => ({d}),"`);
     });
   });
 });
