c1a9ea3a6085e92688dfce62d7dfc80edebd01fd
Konrad Reczko <reczkok@gmail.com>
2026-05-06 12:51:40 +0200

    simplify

--- apps/typegpu-docs/src/examples/rendering/area-light/ltcConfig.ts
+++ apps/typegpu-docs/src/examples/rendering/area-light/ltcConfig.ts
@@ -1,9 +1,8 @@
-let configuredCanFilterFloat32Ltc = false;
+let canFilter = false;
 
-export function configureLtcFiltering(enabled: boolean) {
-  configuredCanFilterFloat32Ltc = enabled;
-}
-
-export function getCanFilterFloat32Ltc() {
-  return configuredCanFilterFloat32Ltc;
+export function ltcFiltering(enabled?: boolean) {
+  if (enabled !== undefined) {
+    canFilter = enabled;
+  }
+  return canFilter;
 }
