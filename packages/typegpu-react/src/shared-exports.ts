50eaf78dfb6885d676653974c956b7db49407f7b
Iwo Plaza <iwoplaza@gmail.com>
2026-05-08 10:32:17 +0200

    Delete deprecated useUniformValue API

--- packages/typegpu-react/src/shared-exports.ts
+++ packages/typegpu-react/src/shared-exports.ts
@@ -3,7 +3,6 @@ export { useFrame } from './core/use-frame.ts';
 export { useMutable } from './core/use-mutable.ts';
 export { useReadonly } from './core/use-readonly.ts';
 export { useUniform } from './core/use-uniform.ts';
-export { useUniformValue } from './core/use-uniform-value.ts';
 export { useMirroredUniform } from './core/use-mirrored-uniform.ts';
 export { useBuffer } from './core/use-buffer.ts';
 export { useBindGroup } from './core/use-bind-group.ts';
@@ -12,4 +11,3 @@ export type {
   UseConfigureContextOptions,
   UseConfigureContextResult,
 } from './core/use-configure-context.ts';
-export type { UniformValue } from './core/use-uniform-value.ts';
