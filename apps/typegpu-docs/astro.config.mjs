ae148488a99e6e05efa77131dae0ad3ed475291d
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-05-08 15:09:34 +0200

    nr fix

--- apps/typegpu-docs/astro.config.mjs
+++ apps/typegpu-docs/astro.config.mjs
@@ -122,7 +122,7 @@ export default defineConfig({
             {
               label: 'Vertices and fragments',
               slug: 'fundamentals/vertices-and-fragments',
-            }
+            },
           ]),
         },
         {
