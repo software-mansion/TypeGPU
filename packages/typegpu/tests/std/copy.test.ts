68765aa3fc77254803d4cc6fea12e67a749d34ee
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-05-12 12:23:05 +0200

    Add more tests

--- packages/typegpu/tests/std/copy.test.ts
+++ packages/typegpu/tests/std/copy.test.ts
@@ -34,6 +34,14 @@ describe('std.copy', () => {
       expect(copy).toStrictEqual(boid);
       expect(copy).not.toBe(boid);
     });
+
+    it('copies arrays', () => {
+      const arr = [1, 2, 3];
+      const copy = std.copy(arr);
+
+      expect(copy).toStrictEqual(arr);
+      expect(copy).not.toBe(arr);
+    });
   });
 
   describe('on the GPU', () => {
@@ -82,6 +90,42 @@ describe('std.copy', () => {
       `);
     });
 
+    it('works for structs', () => {
+      const Boid = d.struct({ pos: d.vec2u });
+
+      const fn = () => {
+        'use gpu';
+        const boid = Boid({ pos: d.vec2u(1, 2) });
+        const copy = std.copy(boid);
+      };
+
+      expect(tgpu.resolve([fn])).toMatchInlineSnapshot(`
+        "struct Boid {
+          pos: vec2u,
+        }
+
+        fn fn_1() {
+          var boid = Boid(vec2u(1, 2));
+          var copy = boid;
+        }"
+      `);
+    });
+
+    it('works for arrays', () => {
+      const fn = () => {
+        'use gpu';
+        const arr = [1, 2, 3];
+        const copy = std.copy(arr);
+      };
+
+      expect(tgpu.resolve([fn])).toMatchInlineSnapshot(`
+        "fn fn_1() {
+          var arr = array<i32, 3>(1, 2, 3);
+          var copy = arr;
+        }"
+      `);
+    });
+
     it('is not allowed on LHS of assignment', () => {
       const fn = () => {
         'use gpu';
@@ -90,6 +134,53 @@ describe('std.copy', () => {
       };
 
       expect(() => tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+    });
+
+    it('can be used to copy a buffer', ({ root }) => {
+      const uniform = root.createUniform(d.vec2u, [1, 2]);
+
+      const fn = () => {
+        'use gpu';
+        const v = std.copy(uniform.$);
+      };
+
+      expect(tgpu.resolve([fn])).toMatchInlineSnapshot(`
+        "@group(0) @binding(0) var<uniform> uniform_1: vec2u;
+
+        fn fn_1() {
+          var v = uniform_1;
+        }"
+      `);
+    });
+
+    it('cannot be used to copy d.ref', () => {
+      const fn = () => {
+        'use gpu';
+        const a = d.vec2u(1, 2);
+        std.copy(d.ref(a));
+      };
+
+      expect(() => tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+    });
+
+    it('cannot be used in d.ref', () => {
+      const modify = (v: d.ref<d.v2u>) => {
+        'use gpu';
+        v.$.x++;
+      };
+
+      const fn = () => {
+        'use gpu';
+        const a = d.vec2u(1, 2);
+        const b = modify(d.ref(std.copy(a)));
+      };
+
+      expect(() => tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot(`
+        [Error: Resolution of the following tree failed:
+        - <root>
+        - fn*:fn
+        - fn*:fn(): d.ref() created with primitive types must be stored in a variable before use]
+      `);
     });
   });
 });
