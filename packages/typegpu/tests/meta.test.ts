524ebfd6eab136883464aa0deaad04032ca3face
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-05-15 12:47:52 +0200

    Remove directives from tests, add new test

--- packages/typegpu/tests/meta.test.ts
+++ packages/typegpu/tests/meta.test.ts
@@ -2,10 +2,11 @@ import { describe, expect } from 'vitest';
 import * as d from '../src/data/index.ts';
 import * as tinyest from 'tinyest';
 import { it } from 'typegpu-testing-utility';
-import type { INTERNAL_GlobalExt } from '../src/shared/meta.ts';
+import { getName, setName, type INTERNAL_GlobalExt } from '../src/shared/meta.ts';
 import { tgpu } from '../src/index.js';
 import type { RawMetadata, RawMetadataV1, RawMetadataV2 } from '../src/shared/normalizeMetadata.ts';
 
+// functions in this file intentionally have no 'use gpu' directive
 describe('meta', () => {
   function assignMetadata(fn: object, meta: RawMetadata) {
     ((globalThis as INTERNAL_GlobalExt).__TYPEGPU_META__ ??= new WeakMap()).set(fn, meta);
@@ -13,23 +14,18 @@ describe('meta', () => {
   const NODE = tinyest.NodeTypeCatalog;
 
   it('throws a readable error when metadata is missing', () => {
-    const fn = () => {
-      'use gpu';
-    };
+    const fn = () => {};
     assignMetadata(fn, { v: 1 });
 
     expect(() => tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot(`
       [Error: Resolution of the following tree failed:
-      - <root>: Value () => {
-      			"use gpu";
-      		} is not resolvable]
+      - <root>: Value () => {} is not resolvable]
     `);
   });
 
   it('correctly parses externals v1', () => {
     const EXT = { N: tgpu.const(d.u32, 1) };
     const fn = () => {
-      'use gpu';
       let a = EXT.N.$;
     };
     const meta: RawMetadataV1 = {
@@ -38,7 +34,10 @@ describe('meta', () => {
       externals: { EXT },
       ast: {
         params: [],
-        body: [NODE.block, [[NODE.let, 'a', [NODE.memberAccess, [NODE.memberAccess, 'EXT', 'N'], '$']]]],
+        body: [
+          NODE.block,
+          [[NODE.let, 'a', [NODE.memberAccess, [NODE.memberAccess, 'EXT', 'N'], '$']]],
+        ],
         externalNames: ['EXT'],
       },
     };
@@ -56,7 +55,6 @@ describe('meta', () => {
   it('correctly parses deferred externals v1', () => {
     const EXT = { N: tgpu.const(d.u32, 1) };
     const fn = () => {
-      'use gpu';
       let a = EXT.N.$;
     };
     const meta: RawMetadataV1 = {
@@ -65,7 +63,10 @@ describe('meta', () => {
       externals: () => ({ EXT }),
       ast: {
         params: [],
-        body: [NODE.block, [[NODE.let, 'a', [NODE.memberAccess, [NODE.memberAccess, 'EXT', 'N'], '$']]]],
+        body: [
+          NODE.block,
+          [[NODE.let, 'a', [NODE.memberAccess, [NODE.memberAccess, 'EXT', 'N'], '$']]],
+        ],
         externalNames: ['EXT'],
       },
     };
@@ -83,7 +84,6 @@ describe('meta', () => {
   it('correctly parses deferred externals v2', () => {
     const EXT = { N: tgpu.const(d.u32, 1) };
     const fn = () => {
-      'use gpu';
       let a = EXT.N.$;
     };
     const meta: RawMetadataV2 = {
@@ -92,7 +92,10 @@ describe('meta', () => {
       externals: { EXT: { N: { $: () => EXT.N.$ } } },
       ast: {
         params: [],
-        body: [NODE.block, [[NODE.let, 'a', [NODE.memberAccess, [NODE.memberAccess, 'EXT', 'N'], '$']]]],
+        body: [
+          NODE.block,
+          [[NODE.let, 'a', [NODE.memberAccess, [NODE.memberAccess, 'EXT', 'N'], '$']]],
+        ],
         externalNames: ['EXT'],
       },
     };
@@ -108,14 +111,31 @@ describe('meta', () => {
   });
 
   it('throws a readable error when metadata version is not recognized', () => {
-    const fn = () => {
-      'use gpu';
-    };
+    const fn = () => {};
     assignMetadata(fn, {} as RawMetadata);
 
     expect(() => tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot(`
       [Error: Resolution of the following tree failed:
       - <root>: Unrecognized TypeGPU metadata format: {}]
     `);
+  });
+
+  it('prefers assigned name over metadata', () => {
+    const fn = () => {};
+
+    setName(fn, 'newName');
+    const meta: RawMetadataV1 = {
+      v: 1,
+      name: 'oldName',
+      externals: () => ({}),
+      ast: {
+        params: [],
+        body: [NODE.block, []],
+        externalNames: [],
+      },
+    };
+    assignMetadata(fn, meta);
+
+    expect(getName(fn)).toBe('newName');
   });
 });
