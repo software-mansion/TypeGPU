864d42fa904c09833d2d97585fba1e2b7ce070b8
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-05-19 15:10:53 +0200

    Remove internal from InfixDispatch

--- packages/typegpu/src/tgsl/infixDispatch.ts
+++ packages/typegpu/src/tgsl/infixDispatch.ts
@@ -1,12 +1,12 @@
 import { type Snippet } from '../data/snippet.ts';
 import type { AnyMatInstance, AnyNumericVecInstance } from '../data/wgslTypes.ts';
-import { $internal, isMarkedInternal } from '../shared/symbols.ts';
 import type { InfixOperator } from './accessProp.ts';
 
 type Numeric = number | AnyNumericVecInstance | AnyMatInstance;
 
 /**
- * InfixDispatch is recognized by the $internal symbol, and lhs may either be Numeric or Snippet.
+ * `lhs` may either be Numeric or Snippet.
+ * JS infix dispatches are handled differently.
  * @example
  * const external = d.vec2u(1);
  * const fn = () => {
@@ -16,9 +16,6 @@ type Numeric = number | AnyNumericVecInstance | AnyMatInstance;
  * }
  */
 export class InfixDispatch {
-  [$internal] = true;
-  type = 'infix-dispatch';
-
   readonly lhs: Snippet | Numeric;
   readonly operator: InfixOperator;
 
@@ -29,5 +26,5 @@ export class InfixDispatch {
 }
 
 export function isInfixDispatch(o: unknown): o is InfixDispatch {
-  return isMarkedInternal(o) && (o as InfixDispatch)?.type === 'infix-dispatch';
+  return o instanceof InfixDispatch;
 }
