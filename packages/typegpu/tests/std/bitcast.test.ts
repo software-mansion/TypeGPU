545f41b852d0f72b202f5b633062bbbd7ae08731
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2026-05-19 16:50:53 +0200

    bitcastF32toU32

--- packages/typegpu/tests/std/bitcast.test.ts
+++ packages/typegpu/tests/std/bitcast.test.ts
@@ -12,6 +12,9 @@ import {
 } from '../../src/data/vector.ts';
 import tgpu, { d, std } from '../../src/index.js';
 
+// remember to pad with zeros to 8 hex symbols
+const floatFromHex = (hex: string) => Buffer.from(hex, 'hex').readFloatBE(0);
+
 describe('bitcast', () => {
   it('bitcastU32toF32', () => {
     // 1.0 in f32
@@ -35,6 +38,14 @@ describe('bitcast', () => {
     // 10000000000000000000000000000000
     const i2 = std.bitcastU32toI32(2147483648);
     expect(i2).toBe(-2147483648);
+  });
+
+  it('bitcastF32toU32', () => {
+    const i1 = std.bitcastF32toU32(floatFromHex('00000001'));
+    expect(i1).toBe(1);
+
+    const i2 = std.bitcastF32toU32(floatFromHex('7f800000'));
+    expect(i2).toBe(2139095040);
   });
 
   it('bitcastU32toF32 vectors', () => {
@@ -63,6 +74,25 @@ describe('bitcast', () => {
     const v4 = vec4u(0, 1, 4294967295, 2147483648);
     const cast4 = std.bitcastU32toI32(v4);
     expect(cast4).toEqual(vec4i(0, 1, -1, -2147483648));
+  });
+
+  it('bitcastF32toU32 vectors', () => {
+    const v2 = vec2f(floatFromHex('7f800000'), floatFromHex('7fc00000')); // +inf, quiet nan
+    const cast2 = std.bitcastF32toU32(v2);
+    expect(cast2).toStrictEqual(vec2u(2139095040, 2143289344));
+
+    const v3 = vec3f(floatFromHex('ff800000'), floatFromHex('00000001'), floatFromHex('80000001'));
+    const cast3 = std.bitcastF32toU32(v3);
+    expect(cast3).toStrictEqual(vec3u(4286578688, 1, 2147483649));
+
+    const v4 = vec4f(
+      floatFromHex('84220925'),
+      floatFromHex('68800000'),
+      floatFromHex('48980780'),
+      floatFromHex('0000075a'),
+    );
+    const cast4 = std.bitcastF32toU32(v4);
+    expect(cast4).toStrictEqual(vec4u(2216823077, 1753219072, 1217922944, 1882));
   });
 
   it('bitcastU32toF32 specials (NaN, infinities etc)', () => {
@@ -128,6 +158,7 @@ describe('bitcast in shaders', () => {
   it('works for primitives', () => {
     const fnf32 = tgpu.fn([], d.f32)(() => std.bitcastU32toF32(1234));
     const fni32 = tgpu.fn([], d.i32)(() => std.bitcastU32toI32(d.u32(2 ** 31)));
+    const fnu32 = tgpu.fn([d.f32], d.u32)((v) => std.bitcastF32toU32(v));
 
     expect(tgpu.resolve([fnf32])).toMatchInlineSnapshot(`
       "fn fnf32() -> f32 {
@@ -139,11 +170,17 @@ describe('bitcast in shaders', () => {
         return -2147483648i;
       }"
     `);
+    expect(tgpu.resolve([fnu32])).toMatchInlineSnapshot(`
+      "fn fnu32(v: f32) -> u32 {
+        return bitcast<u32>(v);
+      }"
+    `);
   });
 
   it('works for vectors', () => {
     const fnvec4i = tgpu.fn([], d.vec4i)(() => std.bitcastU32toI32(vec4u(1, 2, 3, 4)));
     const fnvec4f = tgpu.fn([], d.vec4f)(() => std.bitcastU32toF32(vec4u(1, 2, 3, 4)));
+    const fnvec4u = tgpu.fn([d.vec4f], d.vec4u)((v) => std.bitcastF32toU32(v));
 
     expect(tgpu.resolve([fnvec4i])).toMatchInlineSnapshot(`
       "fn fnvec4i() -> vec4i {
@@ -153,6 +190,11 @@ describe('bitcast in shaders', () => {
     expect(tgpu.resolve([fnvec4f])).toMatchInlineSnapshot(`
       "fn fnvec4f() -> vec4f {
         return vec4f(1.401298464324817e-45, 2.802596928649634e-45, 4.203895392974451e-45, 5.605193857299268e-45);
+      }"
+    `);
+    expect(tgpu.resolve([fnvec4u])).toMatchInlineSnapshot(`
+      "fn fnvec4u(v: vec4f) -> vec4u {
+        return bitcast<vec4u>(v);
       }"
     `);
   });
