d083c7754e6d62f533d58df90d1eb5f35dd6ed4d
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-05-25 17:35:35 +0200

    Add remaining packages

--- packages/typegpu-cli/src/utils/pkg.ts
+++ packages/typegpu-cli/src/utils/pkg.ts
@@ -8,6 +8,16 @@ export function hasDependency(pkg: PackageJsonWithDeps, name: string) {
 }
 
 export const typegpuPkgs = [
-  { value: '@typegpu/noise', hint: 'randomness' },
-  { value: '@typegpu/sdf', hint: 'sdfs' },
+  { value: '@typegpu/color', hint: 'helpers for converting color spaces' },
+  // { value: '@typegpu/geometry', hint: 'helpers for drawing points and lines' },
+  { value: '@typegpu/gl', hint: 'WebGL utilities for TypeGPU integration' },
+  {
+    value: '@typegpu/noise',
+    hint: 'helpers for Perlin noise and general purpose random number generation.',
+  },
+  { value: '@typegpu/radiance-cascades', hint: 'implementation of Radiance Cascades algorithm' },
+  { value: '@typegpu/react', hint: 'React hooks for TypeGPU integration' },
+  { value: '@typegpu/sdf', hint: 'helpers for creating Signed Distance Fields' },
+  // { value: '@typegpu/sort', hint: 'implementations of scanning and sorting algorithms' },
+  { value: '@typegpu/three', hint: 'Three.js utilities for TypeGPU integration' },
 ] as const satisfies { value: string; hint: string }[];
