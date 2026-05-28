13647a93d4b8236be83a2343b31020fc156b027e
Iwo Plaza <iwoplaza@gmail.com>
2026-05-28 07:17:04 +0200

    Cleanup

--- packages/typegpu-cli/templates/template-vite-complex/src/main.ts
+++ packages/typegpu-cli/templates/template-vite-complex/src/main.ts
@@ -1,31 +1,33 @@
+import tgpu, { common, d, std } from 'typegpu';
+import { hexToOklab, oklabToRgb } from '@typegpu/color';
+import { perlin2d } from '@typegpu/noise';
+
+// Wiring up the speed slider
 const slider = document.querySelector<HTMLInputElement>('#slider') as HTMLInputElement;
 let speed = Number(slider.value);
 slider.addEventListener('input', () => {
   speed = Number(slider.value);
 });
 
-import tgpu, { common, d, std } from 'typegpu';
-import { hexToOklab, oklabToRgb } from '@typegpu/color';
-import { perlin2d } from '@typegpu/noise';
-
+// Initializing TypeGPU
 const root = await tgpu.init();
+
 const time = root.createUniform(d.f32);
 
 const canvas = document.querySelector<HTMLCanvasElement>('#canvas') as HTMLCanvasElement;
-
 const context = root.configureContext({ canvas });
 
-const noise = (v: d.v2f) => {
+function noise(v: d.v2f) {
   'use gpu';
   return perlin2d.sample(v);
-};
+}
 
 const octavesAccessor = tgpu.accessor(d.u32);
 const standardizeNoiseAccessor = tgpu.accessor(d.bool);
 const getMaxValue = tgpu.comptime((octaves: number) => 1 - 2 ** -octaves);
 const rotation = d.mat2x2f(0.8, 0.6, -0.6, 0.8);
 
-const fbm = tgpu.fn((v: d.v2f) => {
+function fbm(v: d.v2f) {
   'use gpu';
   let u = d.vec2f(v);
   let f = d.f32();
@@ -54,17 +56,17 @@ const fbm = tgpu.fn((v: d.v2f) => {
   }
 
   return f / getMaxValue(octavesAccessor.$);
-});
+}
 
-const fbm4 = fbm.with(octavesAccessor, 4).with(standardizeNoiseAccessor, false);
-const fbm6 = fbm.with(octavesAccessor, 6).with(standardizeNoiseAccessor, true);
+const fbm4 = tgpu.fn(fbm).with(octavesAccessor, 4).with(standardizeNoiseAccessor, false);
+const fbm6 = tgpu.fn(fbm).with(octavesAccessor, 6).with(standardizeNoiseAccessor, true);
 
-const domainWarp = (v: d.v2f) => {
+function domainWarp(v: d.v2f) {
   'use gpu';
   return fbm6(v + fbm4(v + fbm4(v)));
-};
+}
 
-const palette = (t: number) => {
+function palette(t: number) {
   'use gpu';
   const purple = hexToOklab('#c04bf2');
   const blue = hexToOklab('#4e65f6');
@@ -77,11 +79,12 @@ const palette = (t: number) => {
   const mixed = std.mix(std.mix(dark, blue, factor1), purple, factor2);
 
   return oklabToRgb(mixed);
-};
+}
 
 const VIRTUAL_GRID_SIZE = 4;
 const EPS = 0.04;
 const DIFF_SCALE = 0.4;
+
 const pipeline = root.createRenderPipeline({
   vertex: common.fullScreenTriangle,
   fragment: ({ uv }) => {
@@ -115,10 +118,9 @@ function render(timestamp: number) {
   if (lastTimestamp !== null) {
     elapsed += ((timestamp - lastTimestamp) / 1000.0) * speed;
   }
-
   lastTimestamp = timestamp;
-  time.write(elapsed);
 
+  time.write(elapsed);
   pipeline.withColorAttachment({ view: context }).draw(3);
 
   requestAnimationFrame(render);
