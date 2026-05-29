cf0b27f37bb5ee8164bb66b2f8bcf1aae0faeecf
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2026-05-29 18:38:39 +0200

    no deprecated api

--- apps/typegpu-docs/src/examples/rendering/smoky-triangle/index.ts
+++ apps/typegpu-docs/src/examples/rendering/smoky-triangle/index.ts
@@ -1,5 +1,5 @@
 import { perlin3d } from '@typegpu/noise';
-import tgpu, { d, std } from 'typegpu';
+import tgpu, { d, std, type TgpuFragmentFn, type TgpuVertexFn } from 'typegpu';
 
 import { defineControls } from '../../common/defineControls.ts';
 
@@ -30,15 +30,13 @@ try {
   rgba16floatAvailable = false;
 }
 
-const onChromium = navigator.vendor?.includes('Google');
-const hdr = rgba16floatAvailable && onChromium;
-
-const presentationFormat = hdr ? 'rgba16float' : navigator.gpu.getPreferredCanvasFormat();
+const hdr = rgba16floatAvailable && window.matchMedia('(dynamic-range: high)').matches;
+const preferredFormat = navigator.gpu.getPreferredCanvasFormat();
 
 let context = root.configureContext({
   canvas,
   alphaMode: 'premultiplied',
-  format: presentationFormat,
+  format: preferredFormat,
 });
 
 const getGradientColor = (ratio: number) => {
@@ -73,39 +71,52 @@ const positions = tgpu.const(d.arrayOf(d.vec2f, 3), [
 
 const uvs = tgpu.const(d.arrayOf(d.vec2f, 3), [d.vec2f(0.5, 1), d.vec2f(0, 0), d.vec2f(1, 0)]);
 
-const perlinCache = perlin3d.staticCache({ root, size: d.vec3u(32, 32, 32) });
-const pipeline = root.pipe(perlinCache.inject()).createRenderPipeline({
-  vertex: ({ $vertexIndex }) => {
-    'use gpu';
-    return {
-      $position: d.vec4f(positions.$[$vertexIndex], 0, 1),
-      uv: uvs.$[$vertexIndex],
-    };
-  },
-  fragment: ({ uv }) => {
-    'use gpu';
-    const params = paramsUniform.$;
-    const t = params.time * 0.1;
-    const ouv = uv.mul(5).add(d.vec2f(0, -t));
-    let off = d
-      .vec2f(perlin3d.sample(d.vec3f(ouv, t)), perlin3d.sample(d.vec3f(ouv.mul(2), t + 10)) * 0.5)
-      .add(-0.1);
-    // Sharpening the offset
-    off = tanhVec(off.mul(params.sharpness));
-    // Offsetting the sample point by the distortion
-    const p = uv.add(off.mul(params.distortion));
+const vertex = ({ $vertexIndex }: TgpuVertexFn.AutoInEmpty) => {
+  'use gpu';
+  return {
+    $position: d.vec4f(positions.$[$vertexIndex], 0, 1),
+    uv: uvs.$[$vertexIndex],
+  } satisfies TgpuVertexFn.AutoOut;
+};
 
-    // const factor = (p.x - p.y + 0.7) * 0.7; // How far along the diagonal we are
-    let factor = d.f32(0);
-    if (params.polarCoords === 1) {
-      factor = std.length(p.sub(d.vec2f(0.5, 0.3)).mul(2));
-    } else {
-      factor = (p.x + p.y) * 0.7; // How far along the diagonal we are
-    }
-    return d.vec4f(grain(getGradientColor(factor), uv), 1);
-  },
-  targets: { format: presentationFormat },
+const fragment = ({ uv }: TgpuFragmentFn.AutoIn<{ uv: d.v2f }>) => {
+  'use gpu';
+  const params = paramsUniform.$;
+  const t = params.time * 0.1;
+  const ouv = uv.mul(5).add(d.vec2f(0, -t));
+  let off = d
+    .vec2f(perlin3d.sample(d.vec3f(ouv, t)), perlin3d.sample(d.vec3f(ouv.mul(2), t + 10)) * 0.5)
+    .add(-0.1);
+  // Sharpening the offset
+  off = tanhVec(off.mul(params.sharpness));
+  // Offsetting the sample point by the distortion
+  const p = uv.add(off.mul(params.distortion));
+
+  // const factor = (p.x - p.y + 0.7) * 0.7; // How far along the diagonal we are
+  let factor = d.f32(0);
+  if (params.polarCoords === 1) {
+    factor = std.length(p.sub(d.vec2f(0.5, 0.3)).mul(2));
+  } else {
+    factor = (p.x + p.y) * 0.7; // How far along the diagonal we are
+  }
+  return d.vec4f(grain(getGradientColor(factor), uv), 1);
+};
+
+const perlinCache = perlin3d.staticCache({ root, size: d.vec3u(32, 32, 32) });
+
+const hdrPipeline = root.pipe(perlinCache.inject()).createRenderPipeline({
+  vertex,
+  fragment,
+  targets: { format: 'rgba16float' },
 });
+
+const basePipeline = root.pipe(perlinCache.inject()).createRenderPipeline({
+  vertex,
+  fragment,
+  targets: { format: preferredFormat },
+});
+
+let pipeline = basePipeline;
 
 let frameId: number;
 function frame(timestamp: number) {
@@ -128,9 +139,11 @@ const controls = hdr
           context = root.configureContext({
             canvas,
             alphaMode: 'premultiplied',
-            format: 'rgba16float',
+            format: value ? 'rgba16float' : preferredFormat,
             toneMapping: { mode: value ? 'extended' : 'standard' },
           });
+
+          pipeline = value ? hdrPipeline : basePipeline;
         },
       },
     })
