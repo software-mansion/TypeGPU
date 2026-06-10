8877ba1cc621ca573d88a27b71f826bbc5d3a87f
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-06-10 13:31:43 +0200

    Update unplugin and parsers tests

--- packages/unplugin-typegpu/test/use-gpu-directive.test.ts
+++ packages/unplugin-typegpu/test/use-gpu-directive.test.ts
@@ -432,13 +432,11 @@ describe('marked object methods', () => {
             type: "i",
             name: "n"
           }],
-          body: [0, [[11, [1, "n", "<=", [5, "1"]], [0, [[10, false]]]], [14, [12, "i", [5, "2"]], [1, "i", "<", "n"], [102, "++", "i"], [0, [[11, [1, [6, [7, "obj", "mod"], ["n", "i"]], "===", [5, "0"]], [0, [[10, false]]]]]]], [10, true]]],
+          body: [0, [[11, [1, "n", "<=", [5, "1"]], [0, [[10, false]]]], [14, [12, "i", [5, "2"]], [1, "i", "<", "n"], [102, "++", "i"], [0, [[11, [1, [6, "obj.mod", ["n", "i"]], "===", [5, "0"]], [0, [[10, false]]]]]]], [10, true]]],
           externalNames: ["obj"]
         },
         externals: {
-          obj: {
-            mod: () => obj.mod
-          }
+          "obj.mod": () => obj.mod
         }
       }) && $.f)({});
       console.log(obj, isPrime);"
@@ -476,8 +474,8 @@ describe('marked object methods', () => {
           }), {
           v: 2,
           name: "isPrime",
-          ast: {"params":[{"type":"i","name":"n"}],"body":[0,[[11,[1,"n","<=",[5,"1"]],[0,[[10,false]]]],[14,[12,"i",[5,"2"]],[1,"i","<","n"],[102,"++","i"],[0,[[11,[1,[6,[7,"obj","mod"],["n","i"]],"===",[5,"0"]],[0,[[10,false]]]]]]],[10,true]]],"externalNames":["obj"]},
-          externals: { obj: { mod: () => obj.mod } }
+          ast: {"params":[{"type":"i","name":"n"}],"body":[0,[[11,[1,"n","<=",[5,"1"]],[0,[[10,false]]]],[14,[12,"i",[5,"2"]],[1,"i","<","n"],[102,"++","i"],[0,[[11,[1,[6,"obj.mod",["n","i"]],"===",[5,"0"]],[0,[[10,false]]]]]]],[10,true]]],"externalNames":["obj"]},
+          externals: { "obj.mod": () => obj.mod }
         }) && $.f)({}));
 
           console.log(obj, isPrime);
@@ -530,13 +528,11 @@ describe('transforms numeric operations', () => {
             type: "i",
             name: "b"
           }],
-          body: [0, [[12, "c", [1, [1, "a", "+", "b"], "+", [5, "2"]]], [2, "c", "+=", [1, [5, "2"], "*", "b"]], [2, [7, "countMutable", "$"], "+=", [5, "3"]]]],
+          body: [0, [[12, "c", [1, [1, "a", "+", "b"], "+", [5, "2"]]], [2, "c", "+=", [1, [5, "2"], "*", "b"]], [2, "countMutable.$", "+=", [5, "3"]]]],
           externalNames: ["countMutable"]
         },
         externals: {
-          countMutable: {
-            $: () => countMutable.$
-          }
+          "countMutable.$": () => countMutable.$
         }
       }) && $.f)({});
       console.log(main);"
@@ -560,8 +556,8 @@ describe('transforms numeric operations', () => {
           }), {
           v: 2,
           name: "main",
-          ast: {"params":[{"type":"i","name":"a"},{"type":"i","name":"b"}],"body":[0,[[12,"c",[1,[1,"a","+","b"],"+",[5,"2"]]],[2,"c","+=",[1,[5,"2"],"*","b"]],[2,[7,"countMutable","$"],"+=",[5,"3"]]]],"externalNames":["countMutable"]},
-          externals: { countMutable: { $: () => countMutable.$ } }
+          ast: {"params":[{"type":"i","name":"a"},{"type":"i","name":"b"}],"body":[0,[[12,"c",[1,[1,"a","+","b"],"+",[5,"2"]]],[2,"c","+=",[1,[5,"2"],"*","b"]],[2,"countMutable.$","+=",[5,"3"]]]],"externalNames":["countMutable"]},
+          externals: { "countMutable.$": () => countMutable.$ }
         }) && $.f)({}));
 
           console.log(main);
@@ -953,7 +949,7 @@ describe('hoists function statements marked with "use gpu", scoped inside an if 
             externalNames: ["c"]
           },
           externals: {
-            c: () => c
+            "c": () => c
           }
         }) && $.f)({});
         /** ADD */
@@ -977,7 +973,7 @@ describe('hoists function statements marked with "use gpu", scoped inside an if 
             externalNames: ["c"]
           },
           externals: {
-            c: () => c
+            "c": () => c
           }
         }) && $.f)({});
         console.log(add, mul);
@@ -998,7 +994,7 @@ describe('hoists function statements marked with "use gpu", scoped inside an if 
           v: 2,
           name: "mul",
           ast: {"params":[{"type":"i","name":"a"},{"type":"i","name":"b"}],"body":[0,[[10,[1,[1,"a","*","b"],"*","c"]]]],"externalNames":["c"]},
-          externals: { c: () => c }
+          externals: { "c": () => c }
         }) && $.f)({}));
 
       /** ADD */
@@ -1010,7 +1006,7 @@ describe('hoists function statements marked with "use gpu", scoped inside an if 
           v: 2,
           name: "add",
           ast: {"params":[{"type":"i","name":"a"},{"type":"i","name":"b"}],"body":[0,[[10,[1,[1,"a","+","b"],"+","c"]]]],"externalNames":["c"]},
-          externals: { c: () => c }
+          externals: { "c": () => c }
         }) && $.f)({}));
 
       console.log(add, mul);
@@ -1081,7 +1077,7 @@ describe('replaces function statements marked with "use gpu" in place when condi
               externalNames: ["c"]
             },
             externals: {
-              c: () => c
+              "c": () => c
             }
           }) && $.f)({});
           break;
@@ -1107,7 +1103,7 @@ describe('replaces function statements marked with "use gpu" in place when condi
               externalNames: ["c"]
             },
             externals: {
-              c: () => c
+              "c": () => c
             }
           }) && $.f)({});
           break;
@@ -1131,7 +1127,7 @@ describe('replaces function statements marked with "use gpu" in place when condi
           v: 2,
           name: "add",
           ast: {"params":[{"type":"i","name":"a"},{"type":"i","name":"b"}],"body":[0,[[10,[1,[1,"a","+","b"],"+","c"]]]],"externalNames":["c"]},
-          externals: { c: () => c }
+          externals: { "c": () => c }
         }) && $.f)({}));
 
 
@@ -1146,7 +1142,7 @@ describe('replaces function statements marked with "use gpu" in place when condi
           v: 2,
           name: "mul",
           ast: {"params":[{"type":"i","name":"a"},{"type":"i","name":"b"}],"body":[0,[[10,[1,[1,"a","*","b"],"*","c"]]]],"externalNames":["c"]},
-          externals: { c: () => c }
+          externals: { "c": () => c }
         }) && $.f)({}));
 
 
