b0da4e0bf1b491560426ddd66e6cafa150077b3d
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2026-06-11 18:18:59 +0200

    waku bare template

---
+++ packages/typegpu-cli/templates/template-waku-bare/src/components/Shader.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { useMemo } from 'react';
+import { useConfigureContext, useFrame, useRoot } from '@typegpu/react';
+import { common, d } from 'typegpu';
+
+export function Shader({ className }: { className?: string }) {
+  const { ref, ctxRef } = useConfigureContext({ autoResize: true, alphaMode: 'premultiplied' });
+
+  const root = useRoot();
+  const renderPipeline = useMemo(
+    () =>
+      root.createRenderPipeline({
+        vertex: common.fullScreenTriangle,
+        fragment: ({ uv }) => {
+          'use gpu';
+          return d.vec4f(0.55, uv, 1);
+        },
+      }),
+    [root],
+  );
+
+  useFrame(() => {
+    if (!ctxRef.current) return;
+    renderPipeline.withColorAttachment({ view: ctxRef.current }).draw(3);
+  });
+
+  return <canvas ref={ref} className={className} />;
+}
+
+export const getConfig = async () => {
+  return {
+    render: 'dynamic',
+  } as const;
+};
