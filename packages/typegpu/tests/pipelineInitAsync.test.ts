e5168a66414377695ebe956161617eddc35aa8e3
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-06-12 13:06:21 +0200

    Add tests

---
+++ packages/typegpu/tests/pipelineInitAsync.test.ts
@@ -0,0 +1,51 @@
+import { describe, expect } from 'vitest';
+import tgpu from '../src/index.js';
+import { it } from 'typegpu-testing-utility';
+
+describe('initAsync', () => {
+  const computeFn = tgpu.computeFn({ workgroupSize: [1, 1, 1] })(() => {});
+
+  describe('compute pipeline', () => {
+    it('resolves and creates a pipeline', async ({ root, device }) => {
+      const pipeline = root.createComputePipeline({ compute: computeFn });
+
+      await pipeline.initAsync();
+
+      expect(device.mock.createComputePipelineAsync).toHaveBeenCalled();
+      expect(() => root.unwrap(pipeline)).not.toThrow(); // this means that memo already exists
+      expect(tgpu.resolve([pipeline])).toMatchInlineSnapshot(`
+        "@compute @workgroup_size(1, 1, 1) fn computeFn() {
+
+        }"
+      `);
+    });
+
+    it('throws when attempting to unwrap when not resolved', async ({ root, device }) => {
+      const pipeline = root.createComputePipeline({ compute: computeFn });
+
+      pipeline.initAsync();
+
+      expect(() => root.unwrap(pipeline)).toThrowErrorMatchingInlineSnapshot(
+        `[Error: 'pipeline.initAsync()' was called and is not yet resolved.]`,
+      );
+    });
+
+    it('returns the promise again if not resolved', async ({ root, device }) => {
+      const pipeline = root.createComputePipeline({ compute: computeFn });
+
+      const p1 = pipeline.initAsync();
+      const p2 = pipeline.initAsync();
+
+      expect(p1).toBe(p2);
+    });
+
+    it('returns a resolved promise if pipeline was already created', async ({ root, device }) => {
+      const pipeline = root.createComputePipeline({ compute: computeFn });
+
+      root.unwrap(pipeline); // resolves & compiles the pipeline
+      await pipeline.initAsync(); // should not enqueue device operations
+
+      expect(device.mock.createComputePipelineAsync).not.toHaveBeenCalled();
+    });
+  });
+});
