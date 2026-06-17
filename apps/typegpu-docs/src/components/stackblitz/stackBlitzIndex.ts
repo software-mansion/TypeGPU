0de8633e429ffe0878651500ec7596169511242a
Iwo Plaza <iwoplaza@gmail.com>
2026-06-17 22:35:32 +0200

    Remove resizing code from stackblitz index

--- apps/typegpu-docs/src/components/stackblitz/stackBlitzIndex.ts
+++ apps/typegpu-docs/src/components/stackblitz/stackBlitzIndex.ts
@@ -10,52 +10,6 @@ body.style.margin = '0';
 body.style.boxSizing = 'border-box';
 body.style.padding = '1rem';
 
-// Resize canvases
-for (const canvas of document.querySelectorAll('canvas')) {
-  if ('width' in canvas.attributes || 'height' in canvas.attributes) {
-    continue; // custom canvas, not replacing with resizable
-  }
-
-  const container = document.createElement('div');
-  const frame = document.createElement('div');
-
-  canvas.parentElement?.replaceChild(container, canvas);
-
-  frame.appendChild(canvas);
-  container.appendChild(frame);
-
-  container.style.display = 'flex';
-  container.style.flex = '1';
-  container.style.justifyContent = 'center';
-  container.style.alignItems = 'top';
-  container.style.width = '100%';
-
-  container.style.containerType = 'size';
-
-  frame.style.position = 'relative';
-
-  if (canvas.dataset.fitToContainer !== undefined) {
-    frame.style.width = '100%';
-    frame.style.height = '100%';
-  } else {
-    const aspectRatio = canvas.dataset.aspectRatio ?? '1';
-    frame.style.aspectRatio = aspectRatio;
-    frame.style.height = `min(calc(min(100cqw, 100cqh)/(${aspectRatio})), min(100cqw, 100cqh))`;
-  }
-
-  canvas.style.position = 'absolute';
-  canvas.style.width = '100%';
-  canvas.style.height = '100%';
-
-  const onResize = () => {
-    canvas.width = frame.clientWidth * window.devicePixelRatio;
-    canvas.height = frame.clientHeight * window.devicePixelRatio;
-  };
-
-  onResize();
-  new ResizeObserver(onResize).observe(container);
-}
-
 const controlsPanel = document.createElement('div');
 controlsPanel.style.display = 'grid';
 controlsPanel.style.gridTemplateColumns = '1fr 1fr';
