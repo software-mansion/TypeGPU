46c1932286f10df6db6b4e8b429400d78c8f6fe2
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-06-18 12:19:38 +0200

    [RFC] chore: Don't name externals in mergeExternals (#2616)

--- packages/typegpu/src/resolutionCtx.ts
+++ packages/typegpu/src/resolutionCtx.ts
@@ -51,7 +51,7 @@ import type {
 } from './types.ts';
 import { CodegenState, isSelfResolvable, NormalState } from './types.ts';
 import type { WgslEnableExtension } from './wgslExtensions.ts';
-import { getName, hasTinyestMetadata, setName } from './shared/meta.ts';
+import { getName, hasTinyestMetadata, isNamable, setName } from './shared/meta.ts';
 import { FuncParameterType } from 'tinyest';
 import { accessProp } from './tgsl/accessProp.ts';
 import { createIoSchema } from './core/function/ioSchema.ts';
@@ -197,6 +197,9 @@ class ItemStateStackImpl implements ItemStateStack {
         }
 
         const external = layer.externalMap[id];
+        if (isNamable(external) && getName(external) === undefined) {
+          setName(external, id);
+        }
 
         if (external !== undefined && external !== null) {
           return coerceToSnippet(external);
