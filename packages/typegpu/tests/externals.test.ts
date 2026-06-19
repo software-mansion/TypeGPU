9f336913e5c405ad72ee9b300863937f3dd75871
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-06-19 16:57:26 +0200

    Add external name collision tests

--- packages/typegpu/tests/externals.test.ts
+++ packages/typegpu/tests/externals.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { addArgTypesToExternals, type ExternalMap } from '../src/core/resolve/externals.ts';
 import * as d from '../src/data/index.ts';
+import tgpu from '../src/index.js';
 
 // TODO: fix these tests
 describe('addArgTypesToExternals', () => {
@@ -58,5 +59,80 @@ describe('addArgTypesToExternals', () => {
     //     (result) => externals.push(result),
     //   );
     //   expect(externals).toStrictEqual([{ A: Particle, B: Particle, C: Particle }]);
+  });
+});
+
+describe('external name collisions', () => {
+  it("throws when rawWgsl fn has an 'Out' external", () => {
+    const vertexFn = tgpu.vertexFn({
+      out: { position: d.builtin.position },
+    })`{ return Out(); }`.$uses({ Out: d.struct({ prop: d.u32 }).$name('myOut') });
+
+    expect(() => tgpu.resolve([vertexFn])).toThrowErrorMatchingInlineSnapshot(`
+      [Error: Resolution of the following tree failed:
+      - <root>
+      - vertexFn:vertexFn: Key 'Out' appears in externals while being reserved for internals. Please rename this external.]
+    `);
+  });
+
+  it("allows an 'Out' external in TGSL implemented functions", () => {
+    const Out = d.struct({ prop: d.u32 });
+    const vertexFn = tgpu.vertexFn({
+      out: { position: d.builtin.position },
+    })(() => {
+      'use gpu';
+      const out = Out();
+      return { position: d.vec4f() };
+    });
+
+    expect(tgpu.resolve([vertexFn])).toMatchInlineSnapshot(`
+      "struct Out {
+        prop: u32,
+      }
+
+      struct vertexFn_Output {
+        @builtin(position) position: vec4f,
+      }
+
+      @vertex fn vertexFn() -> vertexFn_Output {
+        let out = Out();
+        return vertexFn_Output(vec4f());
+      }"
+    `);
+  });
+
+  it("throws when rawWgsl fn has an 'in' external", () => {
+    const vertexFn = tgpu.vertexFn({
+      in: { vId: d.builtin.vertexIndex },
+      out: { position: d.builtin.position },
+    })`{ return d.vec4f(in); }`.$uses({ in: 1 });
+
+    expect(() => tgpu.resolve([vertexFn])).toThrowErrorMatchingInlineSnapshot(`
+      [Error: Resolution of the following tree failed:
+      - <root>
+      - vertexFn:vertexFn: Key 'in' appears in externals while being reserved for internals. Please rename this external.]
+    `);
+  });
+
+  it("allows an 'in' external in TGSL implemented functions", () => {
+    const EXT = { in: 1 };
+    const vertexFn = tgpu.vertexFn({
+      out: { position: d.builtin.position },
+    })(() => {
+      'use gpu';
+      const x = EXT.in;
+      return { position: d.vec4f() };
+    });
+
+    expect(tgpu.resolve([vertexFn])).toMatchInlineSnapshot(`
+      "struct vertexFn_Output {
+        @builtin(position) position: vec4f,
+      }
+
+      @vertex fn vertexFn() -> vertexFn_Output {
+        const x = 1;
+        return vertexFn_Output(vec4f());
+      }"
+    `);
   });
 });
