5bcb3797d3d02a9ea0c693f7ccf5a91af9d8ef89
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-06-25 12:32:57 +0200

    Address linter issues

--- packages/typegpu/src/index.js
+++ packages/typegpu/src/index.js
@@ -4,6 +4,7 @@
 
 // NOTE: This is a barrel file, internal files should not import things from this file
 
+// oxlint-disable-next-line
 import * as tgpu from './tgpu.ts';
 export * as tgpu from './tgpu.ts';
 export default tgpu;
