f67d303eae4f1dd191df190907dc2cca294c7556
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2026-07-07 20:08:08 +0200

    review changes

--- packages/typegpu-cli/templates/template-waku-bare/waku.config.ts
+++ packages/typegpu-cli/templates/template-waku-bare/waku.config.ts
@@ -1,10 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import typegpu from 'unplugin-typegpu/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'waku/config';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss(), react(), typegpu()],
+    plugins: [tailwindcss(), typegpu()],
   },
 });
