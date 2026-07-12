2f06959c70c2cf2e96c1b6ff8c57babcd0a06e4d
Iwo Plaza <iwoplaza@gmail.com>
2026-07-12 16:30:36 +0200

    feat: Early return pruning

--- packages/typegpu/src/internal.ts
+++ packages/typegpu/src/internal.ts
@@ -29,6 +29,7 @@ export type { Snippet, ResolvedSnippet, Origin } from './data/snippet.ts';
 
 export type {
   ShaderGenerator,
+  ResolvedStatement,
   BinaryOperator,
   ShaderGeneratorClass,
   FunctionDefinitionOptions,
