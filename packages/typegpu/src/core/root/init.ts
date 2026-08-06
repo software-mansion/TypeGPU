24b54d87d40350d6ec868a440422808a57b1dddf
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-08-06 13:54:47 +0200

    Add minify options to types

--- packages/typegpu/src/core/root/init.ts
+++ packages/typegpu/src/core/root/init.ts
@@ -303,6 +303,7 @@ class TgpuRootImpl extends WithBindingImpl implements TgpuRoot, ExperimentalTgpu
 
   readonly device: GPUDevice;
   readonly nameRegistrySetting: 'random' | 'strict';
+  readonly minify: boolean;
   readonly shaderGenerator: ShaderGenerator | undefined;
 
   #unwrappedBindGroupLayouts = new WeakMemo((key: TgpuBindGroupLayout) => key.unwrap(this));
@@ -316,6 +317,7 @@ class TgpuRootImpl extends WithBindingImpl implements TgpuRoot, ExperimentalTgpu
   constructor(
     device: GPUDevice,
     nameRegistrySetting: 'random' | 'strict',
+    minify: boolean,
     ownDevice: boolean,
     logOptions: LogGeneratorOptions,
     shaderGenerator?: ShaderGenerator,
@@ -324,6 +326,7 @@ class TgpuRootImpl extends WithBindingImpl implements TgpuRoot, ExperimentalTgpu
 
     this.device = device;
     this.nameRegistrySetting = nameRegistrySetting;
+    this.minify = minify;
     this.#ownDevice = ownDevice;
     this.shaderGenerator = shaderGenerator;
 
@@ -583,6 +586,15 @@ export type InitOptions = {
   /** @default 'strict' */
   unstable_names?: 'random' | 'strict' | undefined;
   /**
+   * If set to true, rooted resolves will be stripped of all excessive spaces.
+   * Rooted resolves include pipelines and all their transitive dependencies,
+   * but not, for example, `tgpu.resolve([MyStruct])`,
+   * as there may be multiple roots with different settings in one program.
+   *
+   * @default false
+   */
+  unstable_minify?: boolean;
+  /**
    * A custom shader code generator, used when resolving TypeGPU functions.
    * If not provided, the default WGSL generator will be used.
    */
@@ -590,6 +602,7 @@ export type InitOptions = {
   unstable_logOptions?: LogGeneratorOptions;
 };
 
+// TODO: merge these types
 /**
  * Options passed into {@link initFromDevice}.
  */
@@ -597,6 +610,15 @@ export type InitFromDeviceOptions = {
   device: GPUDevice;
   /** @default 'strict' */
   unstable_names?: 'random' | 'strict' | undefined;
+  /**
+   * If set to true, rooted resolves will be stripped of all excessive spaces.
+   * Rooted resolves include pipelines and all their transitive dependencies,
+   * but not, for example, `tgpu.resolve([MyStruct])`,
+   * as there may be multiple roots with different settings in one program.
+   *
+   * @default false
+   */
+  unstable_minify?: boolean;
   /**
    * A custom shader code generator, used when resolving TypeGPU functions.
    * If not provided, the default WGSL generator will be used.
@@ -628,6 +650,7 @@ export async function init(options?: InitOptions): Promise<TgpuRoot> {
     adapter: adapterOpt,
     device: deviceOpt,
     unstable_names: names = 'strict',
+    unstable_minify: minify = false,
     unstable_logOptions: logOptions,
     unstable_shaderGenerator: shaderGenerator,
   } = options ?? {};
@@ -665,7 +688,7 @@ export async function init(options?: InitOptions): Promise<TgpuRoot> {
     requiredFeatures: availableFeatures,
   });
 
-  return new TgpuRootImpl(device, names, true, logOptions ?? {}, shaderGenerator);
+  return new TgpuRootImpl(device, names, minify, true, logOptions ?? {}, shaderGenerator);
 }
 
 /**
@@ -681,9 +704,10 @@ export function initFromDevice(options: InitFromDeviceOptions): TgpuRoot {
   const {
     device,
     unstable_names: names = 'strict',
+    unstable_minify: minify = false,
     unstable_logOptions: logOptions,
     unstable_shaderGenerator: shaderGenerator,
   } = options ?? {};
 
-  return new TgpuRootImpl(device, names, false, logOptions ?? {}, shaderGenerator);
+  return new TgpuRootImpl(device, names, minify, false, logOptions ?? {}, shaderGenerator);
 }
