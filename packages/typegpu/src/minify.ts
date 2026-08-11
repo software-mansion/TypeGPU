f8906388ced195b87c92a61f199f0d9dff721f70
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-08-11 11:52:36 +0200

    Update docs

--- packages/typegpu/src/minify.ts
+++ packages/typegpu/src/minify.ts
@@ -2,9 +2,7 @@ import { blankSpaces, lineBreaks } from './core/whitespaces.ts';
 import { invariant } from './errors.ts';
 
 /**
- * Regex for splitting code into tokens.
  * We don't separate every WGSL token, for example `main()` already has no spaces, no need to further split it.
- * Split if whitespace is encountered, or if either of [:,;] is in lookahead.
  */
 const splitRegex = new RegExp(`[${[...blankSpaces].join('')}]+`, 'ug');
 
@@ -12,23 +10,20 @@ const splitRegex = new RegExp(`[${[...blankSpaces].join('')}]+`, 'ug');
  * Regex for detecting boundary characters that indicate whitespace may be needed between tokens.
  * @example
  * "let", "variable" // "t" and "v" both match, a space is needed
- * "return" "a" // "n" and "t" both match, a space is needed
+ * "return" "a" // "n" and "a" both match, a space is needed
  * "return" "(a+b)" // "n" matches and "(" does not, no space is needed
  */
 const separatorNeededRegex = /[.\p{XID_Continue}]/u;
 
 /**
  * This function accepts a code string, and returns equivalent code
- * with unnecessary whitespace removed.
+ * with comments and unnecessary whitespace removed.
  */
 export function minify(code: string): string {
-  // Remove comments.
   const codeWithoutComments = stripWGSLComments(code);
 
-  // Split into tokens.
   const tokens = codeWithoutComments.split(splitRegex).filter((token) => token !== '');
 
-  // Join and separate if necessary.
   let result = '';
   for (let i = 0; i < tokens.length; i++) {
     const current = tokens[i];
