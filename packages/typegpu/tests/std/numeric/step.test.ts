9d5a33bd4df89c59a02bcc65513f7b248e450092
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-08-18 10:44:48 +0200

    Add missing std tests

---
+++ packages/typegpu/tests/std/numeric/step.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest';
+import { d, std } from 'typegpu';
+
+describe('step', () => {
+  it('compares scalars', () => {
+    expect(std.step(2, 3)).toBe(1);
+    expect(std.step(2, 2)).toBe(1);
+    expect(std.step(2, 1)).toBe(0);
+  });
+
+  it('throws on invalid arguments', () => {
+    // @ts-expect-error
+    expect(() => std.step(2, d.vec2f())).toThrowErrorMatchingInlineSnapshot(
+      `[Error: Unsupported signature. Expected the following kinds to be equal: 'number, vec2f']`,
+    );
+  });
+});
