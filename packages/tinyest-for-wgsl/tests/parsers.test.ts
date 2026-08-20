60810de3deac9ed639b5f1a794d3484d374da735
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-08-20 13:34:46 +0200

    Fix parsers test

--- packages/tinyest-for-wgsl/tests/parsers.test.ts
+++ packages/tinyest-for-wgsl/tests/parsers.test.ts
@@ -27,20 +27,17 @@ describe('transpileFn', () => {
       `);
   });
 
-  it(
-    'parses null',
-    dualTest((p) => {
-      const { params, body, externalNames } = transpileFn(
-        p(`() => {
-          const a = null;
-        }`),
-      );
+  it('parses null', () => {
+    const { params, body, externalNames } = transpileFn(
+      parseBabel(`() => {
+        const a = null;
+      }`),
+    );
 
-      expect(params).toStrictEqual([]);
-      expect(JSON.stringify(body)).toMatchInlineSnapshot(`"[0,[[13,"a",[106]]]]"`);
-      expect(externalNames).toMatchInlineSnapshot(`Map {}`);
-    }),
-  );
+    expect(params).toStrictEqual([]);
+    expect(JSON.stringify(body)).toMatchInlineSnapshot(`"[0,[[13,"a",[106]]]]"`);
+    expect(externalNames).toMatchInlineSnapshot(`Map {}`);
+  });
 
   it('fails when the input is not a function', () => {
     expect(() => transpileFn(parseBabel('1 + 2'))).toThrow();
