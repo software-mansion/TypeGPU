36275c8021bf350f27b91ab399e818c5064d7330
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2026-08-25 13:48:38 +0200

    external chain

--- packages/tinyest-for-wgsl/src/externals.ts
+++ packages/tinyest-for-wgsl/src/externals.ts
@@ -1,8 +1,6 @@
+import * as acorn from 'acorn';
+import * as babel from '@babel/types';
 import type { Context, JsNode } from './types.ts';
-
-function isDeclared(ctx: Context, name: string) {
-  return ctx.stack.some((scope) => scope.declaredNames.includes(name));
-}
 
 /**
  * Checks if the provided node is an external chain access.
@@ -43,3 +41,89 @@ export function tryFindExternalChain(ctx: Context, node: JsNode): string | undef
     }
   }
 }
+
+function isDeclared(ctx: Context, name: string) {
+  return ctx.stack.some((scope) => scope.declaredNames.includes(name));
+}
+
+type PrivateNameGetter<TMemberExpression> = (node: TMemberExpression) => string | undefined;
+
+type ExternalChainFinder<TNode> = (ctx: Context, node: TNode) => string | undefined;
+
+function createExternalChainFinder(
+  getPrivatePropertyName: PrivateNameGetter<acorn.MemberExpression>,
+): ExternalChainFinder<acorn.AnyNode>;
+function createExternalChainFinder(
+  getPrivatePropertyName: PrivateNameGetter<babel.MemberExpression>,
+): ExternalChainFinder<babel.Node>;
+function createExternalChainFinder(
+  getPrivatePropertyName:
+    | PrivateNameGetter<acorn.MemberExpression>
+    | PrivateNameGetter<babel.MemberExpression>,
+) {
+  const find: ExternalChainFinder<JsNode> = (ctx, node) => {
+    if (node.type === 'Identifier' && !isDeclared(ctx, node.name)) {
+      return node.name;
+    }
+    if (node.type === 'ThisExpression') {
+      return 'this';
+    }
+
+    if (node.type === 'MemberExpression' && !node.computed) {
+      if (ctx.visitedNodes.has(node)) {
+        return;
+      }
+      ctx.visitedNodes.add(node);
+
+      const property =
+        node.property.type === 'Identifier' && node.property.name !== '$'
+          ? node.property.name
+          : (
+              getPrivatePropertyName as PrivateNameGetter<
+                acorn.MemberExpression | babel.MemberExpression
+              >
+            )(node);
+
+      if (!property) {
+        return;
+      }
+
+      const lhs = find(ctx, node.object);
+      if (lhs) {
+        return `${lhs}.${property}`;
+      }
+    }
+  };
+
+  return find;
+}
+
+function getPrivatePropertyNameAcorn(node: acorn.MemberExpression): string | undefined {
+  return node.property.type === 'PrivateIdentifier' ? `#${node.property.name}` : undefined;
+}
+
+function getPrivatePropertyNameBabel(node: babel.MemberExpression): string | undefined {
+  return node.property.type === 'PrivateName' ? `#${node.property.id.name}` : undefined;
+}
+
+/**
+ * Checks if the provided node is an external chain access.
+ * @example
+ * tryFindExternalChainAcorn(ctx, node`ext`); // 'ext'
+ * tryFindExternalChainAcorn(ctx, node`ext.p.q`); // 'ext.p.q'
+ * tryFindExternalChainAcorn(ctx, node`ext.p.q().r`); // undefined
+ * tryFindExternalChainAcorn(ctx, node`local.p.q`); // undefined
+ * tryFindExternalChainAcorn(ctx, node`ext.$.q`); // undefined
+ */
+export const tryFindExternalChainAcorn = createExternalChainFinder(getPrivatePropertyNameAcorn);
+
+/**
+ * Checks if the provided node is an external chain access.
+ * @example
+ * tryFindExternalChainBabel(ctx, node`ext`); // 'ext'
+ * tryFindExternalChainBabel(ctx, node`ext.p.q`); // 'ext.p.q'
+ * tryFindExternalChainBabel(ctx, node`ext.p.q().r`); // undefined
+ * tryFindExternalChainBabel(ctx, node`local.p.q`); // undefined
+ * tryFindExternalChainBabel(ctx, node`ext.$.q`); // undefined
+ */
+export const tryFindExternalChainBabel = createExternalChainFinder(getPrivatePropertyNameBabel);
