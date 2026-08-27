c35932d51a21f4163f3a2bd2e1beed7c0309e5c6
Iwo Plaza <iwoplaza@gmail.com>
2026-08-27 16:05:24 +0200

    fix: `tgpu` default export tree shakeability

--- packages/typegpu/src/index.js
+++ packages/typegpu/src/index.js
@@ -4,8 +4,7 @@
 
 // NOTE: This is a barrel file, internal files should not import things from this file
 
-import * as tgpu from './tgpu.ts';
 export * as tgpu from './tgpu.ts';
-export default tgpu;
+export * as default from './tgpu.ts';
 
 export * from './indexNamedExports.ts';
