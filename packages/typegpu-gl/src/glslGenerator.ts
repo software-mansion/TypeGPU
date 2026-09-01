52cd4c8de17f3a94f7fd7beea747c92fab5c22c1
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2026-09-01 13:08:07 +0200

    warn when prop omitted

--- packages/typegpu-gl/src/glslGenerator.ts
+++ packages/typegpu-gl/src/glslGenerator.ts
@@ -1100,6 +1100,10 @@ export class GlslGenerator extends WgslGenerator {
         d.isWgslStruct(expectedReturnType) &&
         expectedReturnType.propTypes[key] === undefined
       ) {
+        console.warn(
+          `Object property '${key}' in '${stringifyNode(exprNode)}' is not part of ` +
+            `'${String(expectedReturnType)}'. Its runtime side effects will be omitted.`,
+        );
         continue;
       }
 
