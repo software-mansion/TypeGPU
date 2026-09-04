55cf1ac8e14719997bc293365ee6588773a2fe52
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-09-04 10:40:46 +0200

    Add identifier node

--- packages/unplugin-typegpu/src/core/obfuscate.ts
+++ packages/unplugin-typegpu/src/core/obfuscate.ts
@@ -189,6 +189,12 @@ const visitors = {
     }
     return [NODE.booleanLiteral, node[1]];
   },
+  identifier(ctx: Context, node: tinyest.Identifier) {
+    if (typeof node === 'string') {
+      return obf(ctx, node);
+    }
+    return [NODE.identifier, obf(ctx, node[1])];
+  },
 } as const satisfies {
   [N in keyof typeof NODE]: (
     ctx: Context,
