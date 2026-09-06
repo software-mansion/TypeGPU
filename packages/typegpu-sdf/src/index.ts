e0a16823b4f25e82fcb324c146a0e1a100b05498
Iwo Plaza <iwoplaza@gmail.com>
2026-09-06 19:37:25 +0200

    feat(@typegpu/sdf): Add arc, capped cylinder, rhombus, and triangle shapes

--- packages/typegpu-sdf/src/index.ts
+++ packages/typegpu-sdf/src/index.ts
@@ -1,13 +1,25 @@
-export { sdBezier, sdBezierApprox, sdBox2d, sdDisk, sdLine, sdPie, sdRoundedBox2d } from './2d.ts';
+export {
+  sdArc,
+  sdBezier,
+  sdBezierApprox,
+  sdBox2d,
+  sdDisk,
+  sdLine,
+  sdPie,
+  sdRoundedBox2d,
+} from './2d.ts';
 
 export {
   sdBox3d,
   sdBoxFrame3d,
+  sdCappedCylinder,
   sdCapsule,
   sdLine3d,
   sdPlane,
+  sdRhombus,
   sdRoundedBox3d,
   sdSphere,
+  sdTriangle3d,
 } from './3d.ts';
 
 export {
