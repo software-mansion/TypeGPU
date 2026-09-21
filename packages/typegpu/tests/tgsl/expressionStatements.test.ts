016b5840e4a081f831fdca0da7594d7b1606b6b0
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2026-09-21 17:07:05 +0200

    Add tests

---
+++ packages/typegpu/tests/tgsl/expressionStatements.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, it } from 'vitest';
+import { tgpu, d } from 'typegpu';
+
+describe('expression statements', () => {
+  it('forbids numeric expression statements', () => {
+    const fn = () => {
+      'use gpu';
+      1;
+    };
+
+    expect(tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+  });
+
+  it('forbids boolean expression statements', () => {
+    const fn = () => {
+      'use gpu';
+      true;
+    };
+
+    expect(tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+  });
+
+  it('forbids identifier expression statements', () => {
+    const fn = () => {
+      'use gpu';
+      const a = 1;
+      a;
+    };
+
+    expect(tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+  });
+
+  it('forbids external identifier expression statements', () => {
+    const a = 1;
+    const fn = () => {
+      'use gpu';
+      a;
+    };
+
+    expect(tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+  });
+
+  it('forbids member access expression statements', () => {
+    const Struct = d.struct({ p: d.u32 });
+    const fn = () => {
+      'use gpu';
+      const a = Struct();
+      a.p;
+    };
+
+    expect(tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+  });
+
+  it('forbids complex expression statements', () => {
+    const fn = () => {
+      'use gpu';
+      1 + 1;
+    };
+
+    expect(tgpu.resolve([fn])).toThrowErrorMatchingInlineSnapshot();
+  });
+});
